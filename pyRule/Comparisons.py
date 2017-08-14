ac670a9c757d4a05ae4549d75243f5db83ad9a06
John Grey <jgrey@ucsc.edu>
2017-08-14 17:33:43 +0100

    added reverse lookup and a dedicated comparison class

--- pyRule/Comparisons.py
+++ pyRule/Comparisons.py
@@ -1,7 +1,11 @@
 """ Simple comparison functions to be used in rules """
-from .utils import COMP
 import logging as root_logger
+from enum import Enum 
 logging = root_logger.getLogger(__name__)
+
+#comparison operators:
+COMP = Enum('Comp_ops','LT GT NE EQ')
+
 
 def EQ(a,b):
     return a == b
@@ -28,3 +32,34 @@ COMP_LOOKUP = {
     COMP.NE : NEQ,
     COMP.EQ : EQ
 }
+
+COMP_REVERSE_LOOKUP = {
+    COMP.LT : "<",
+    COMP.GT : ">",
+    COMP.NE : "!=",
+    COMP.EQ : "=="
+}
+
+class Comparison:
+
+    def __init__(self, op, value=None, bind=None):
+        assert(isinstance(op, COMP))
+        assert(value is not None or bind is not None)
+        assert(not (value is not None and bind is not None))
+        self.op = op
+        self.value = value
+        self.bind = bind
+
+    def copy(self):
+        return Comparison(self.op, self.value, self.bind)
+        
+    def is_alpha_test(self):
+        return self.value is not None
+        
+    def __repr__(self):
+        if self.value is not None:
+            return "{} {}".format(COMP_REVERSE_LOOKUP[self.op],
+                                    self.value)
+        else:
+            return "{} {}".format(COMP_REVERSE_LOOKUP[self.op],
+                                    repr(self.bind))
