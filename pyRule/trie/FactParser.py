14ced73bfdb7162b6599fec7069033e2aec4f751
John Grey <jgrey@ucsc.edu>
2017-08-14 12:55:08 +0100

    cleaned up parser slightly, param_fact_strings deprecate fact_strings

--- pyRule/trie/FactParser.py
+++ pyRule/trie/FactParser.py
@@ -16,10 +16,10 @@ opLn = s(op(pp.LineEnd()))
 
 def construct_num(toks):
     #todo: add in fractions and underscores
-    if 'd' in toks[0]:
-        return float(toks[0].replace('d','.'))
+    if 'd' in toks:
+        return float(toks.replace('d','.'))
     else:
-        return int(tok[0])
+        return int(toks)
 
 #Base Defs
 DOT = pp.Keyword('.', identChars='!')
@@ -33,41 +33,35 @@ NUM = pp.Word(pp.nums + '-d').setParseAction(construct_num)
 STRING = pp.dblQuotedString
 VALUE = pp.Or([NAME, NUM, STRING])
 
-#Core = .a | !b
-CORE = OP + VALUE
-
-#fact string
-fact_string = pp.OneOrMore(CORE)
-fact_strings = fact_string + pp.ZeroOrMore(COMMA + fact_string)
-
 #alt for actions, PARAM_CORE
 BIND = DOLLAR + VALUE
 VALBIND = pp.Or([VALUE, BIND])
 
+#Core = .a | !b | .$a | !$b
 PARAM_CORE = OP + VALBIND
 
 param_fact_string = pp.OneOrMore(PARAM_CORE)
-param_fact_strings = param_fact_string + pp.ZeroOrMore(COMMA + fact_string)
+param_fact_strings = param_fact_string + pp.ZeroOrMore(COMMA + param_fact_string)
 
 #Actions
 BIND.setParseAction(lambda toks: Bind(toks[1]))
 STRING.setParseAction(lambda t: t[0].replace('"',''))
 DOT.setParseAction(lambda t: EXOP.DOT)
 EX.setParseAction(lambda t: EXOP.EX)
-NUM.setParseAction(lambda t: int(t[0]))
-CORE.setParseAction(lambda toks: Node(toks[1], toks[0]))
-fact_string.setParseAction(lambda toks: [toks[:]])
+NUM.setParseAction(lambda t: construct_num(t[0]))
 
+PARAM_CORE.setParseAction(lambda toks: Node(toks[1], toks[0]))
+param_fact_string.setParseAction(lambda toks: [toks[:]])
 
 # MAIN PARSER:
 def parseString(s):
     """ str -> [Node] """
-    parsed = fact_string.parseString(s)[0]
+    parsed = param_fact_string.parseString(s)[0]
     return [Node.Root()] + parsed
 
 def parseStrings(s):
     """ str -> [[Node]] """
-    parsed = fact_strings.parseString(s)[:]
+    parsed = param_fact_strings.parseString(s)[:]
     with_roots = [[Node.Root()] + x for x in parsed]
     return with_roots
 
