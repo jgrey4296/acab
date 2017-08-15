86e68a6ee1ae6dc7bf6549f375134c7c178b413d
John Grey <jgrey@ucsc.edu>
2017-08-15 11:10:04 +0100

    fleshed out stub engine

--- pyRule/trie/Engine.py
+++ pyRule/trie/Engine.py
@@ -4,10 +4,12 @@ with support for transforms and actions
 """
 import logging as root_logger
 from pyRule.trie.Trie import Trie
-from pyRule.trie import TransformParser as TP
-from pyRule.trie import ActionParser as AP
-from pyRule.trie import QueryParser as QP
-from pyRule.trie import FactParser as FP
+from . import TransformParser as TP
+from . import ActionParser as AP
+from . import QueryParser as QP
+from . import FactParser as FP
+from . import RuleParser as RP
+from . import Contexts
 import  pyRule.utils as util
 
 logging = root_logger.getLogger(__name__)
@@ -16,15 +18,20 @@ class Engine:
     
     def __init__(self, init=None):
         self._trie = Trie(init)
+        self._rules = {}
+        #to be updated with printed representations of the trie state after each action
         self._prior_states = []
+        #named recall states of past tries
+        self._recall_states = []
         self._custom_actions = {}
         
     def _save_state(self):
         self._prior_states.append(str(self._trie))
 
-    def register_action(self, name, func):
+    def register_action(self, name, func, related_facts):
         return None
-        
+
+    #todo: be able to assert retract or query from tries instead of strings
     def add(self, s):
         assert(isinstance(s, str))
         self._trie.assertSMulti(s)
@@ -35,25 +42,56 @@ class Engine:
 
     def query(self, s):
         assert(isinstance(s, str))
-        self._trie.queryS(s)
+        return self._trie.queryS(s)
 
     def registerRules(self, s):
         #rules are strings in the factbase too
-        return None
+        assert(isinstance(s, str))
+        rules = RP.parseStrings(s)
+        for x in rules:
+            assert(isinstance(x, util.Rule))
+            assert(x.is_coherent())
+            self._rules[x._name] = x
 
-    def _run_rules(self, s):
-        #for all child rules at parsed(s),
-        #run the rule, run the transform, run the action
-        return None
+    def _run_rules(self):
+        #todo: make it parse the names of rules to run
+        #for now, run all rules in random order
+        results = []
+        for r in self._rules.values():
+            results.append(self.run_rule(r))
 
-    def _run_transform(self, ctx, transform):
-        return None
-
-    def _run_action(self, ctx, action):
-        #lookup custom actions
-        return None
+    def run_rule(self, rule):
+        assert(isinstance(rule, util.Rule))
+        assert(rule.is_coherent())
+        logging.info("Running Rule: {}".format(rule._name))
+        result = self.query(rule._query)
+        if not bool(result):
+            logging.warning("Rule {} Failed".format(rule._name))
+            return False
+        transformed = self._run_transform(result, rule._transform)
+        output = self._run_actions(transformed, rule._actions)
+        return output
     
-    def _verify_rule(self, s):
-        #parse a rule, then verify that
-        #query -> transform -> action bindings are aligned
-        return None
+    
+    def _run_transform(self, ctx, transform):
+        assert(isinstance(ctx, Contexts))
+        #todo: detect min max bounds of transform
+        chosen_ctx = ctx.select()
+        for x in transform.components:
+            continue
+            #lookup op
+            #get source
+            #get second source
+            #perform
+            #rebind or readd
+        return chosen_ctx
+
+    def _run_actions(self, data, actions):
+        results = []
+        for x in actions:
+            continue
+            #lookup op
+            #get values from data
+            #perform action op with data
+            #push results to results
+        return results
