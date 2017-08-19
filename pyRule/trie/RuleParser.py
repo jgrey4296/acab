68532b14c23829623c811d176bfae918181e044d
John Grey <jgrey@ucsc.edu>
2017-08-19 18:18:26 +0100

    implemented the initial rule parser

--- pyRule/trie/RuleParser.py
+++ pyRule/trie/RuleParser.py
@@ -11,10 +11,43 @@ logging = root_logger.getLogger(__name__)
 
 s = pp.Suppress
 op = pp.Optional
+sLn = s(pp.White(ws='\n',exact=1))
 opLn = s(op(pp.LineEnd()))
+COLON = s(pp.Literal(':'))
+emptyLine = s(pp.lineEnd + pp.lineEnd)
+ruleEnd = s(pp.Literal('end'))
+
+ruleName = FP.param_fact_string.copy().setResultsName('ruleName')
+
+conditions = (QP.clauses + emptyLine).setResultsName('conditions')
+transforms = (TP.transforms + emptyLine).setResultsName('transforms')
+actions = (AP.actions + emptyLine).setResultsName('actions')
+
+rule = ruleName + COLON + sLn \
+       + op(conditions) + op(transforms) + op(actions) \
+       + ruleEnd
+
 
 #todo: a parser to combine { query -> transform -> action }
+"""
+.rule.test:
+	 .a.b.c.d!20
+	 .a.d.e.$t(>30)
 
+         ($t + 20 -> $y)
+
+         +.a.b.c.d
+          +.a.b.e!$t
+          -.a.b.e.f
+          @"Blah"
+          something("else")
+end
+
+meta rules:
+add/remove/replace penumbra conditions
+modify penumbra transforms
+add/remove/replace penumbra actions
+"""
 
 def parseString(s):
     return s
