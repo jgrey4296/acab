dcf3258d781f84929af67d1b19181709cb77e75a
John Grey <jgrey@ucsc.edu>
2017-08-24 13:07:57 +0100

    implement additional transform operators, improved the repr of transform

--- pyRule/Transforms.py
+++ pyRule/Transforms.py
@@ -1,10 +1,13 @@
 """ Simple Transform funtions to be used in rules """
 from enum import Enum 
 import logging as root_logger
-logging = root_logger.getLogger(__name__)
 from pyRule.utils import Bind
+from random import uniform, sample, randint
+from math import floor
 
-TROP = Enum("Transform_ops", "ADD SUB MUL DIV RAND RANGE REMAIN ROUND")
+logging = root_logger.getLogger(__name__)
+
+TROP = Enum("Transform_ops", "ADD SUB MUL DIV RAND REMAIN ROUND NEG REGEX FORMAT SELECT")
 #todo: add regex transform
 
 def ADD(a, b):
@@ -19,11 +22,44 @@ def MUL(a, b):
 def DIV(a, b):
     return a / b
 
+def RAND(a, b):
+    #Rand between 0 and 1
+    return uniform(a, b)
+
+def REMAIN(a, b):
+    #divde and get remainder?
+    raise Exception("Not implemented yet")
+
+def ROUND(a):
+    #round to integer
+    return floor(a)
+
+def NEG(a):
+    #invert the number
+    return -a
+
+def REGEX(a, b, data):
+    #substitute a pattern with a value from passed in data
+    raise Exception("Not implemented yet")
+
+def FORMAT(a, data):
+    #use str.format variant with a data dictionary
+    return a.format(**data)
+
+def SELECT(alts, min, max):
+    return sample(alts, randomint(min, max))
+
+
 TROP_LOOKUP = {
     TROP.ADD : ADD,
     TROP.SUB : SUB,
     TROP.MUL : MUL,
-    TROP.DIV : DIV
+    TROP.DIV : DIV,
+    TROP.RAND : RAND,
+    TROP.REMAIN : REMAIN,
+    TROP.ROUND : ROUND,
+    TROP.NEG : NEG,
+    TROP.REGEX : REGEX
 }
 
 TROP_REVERSE_LOOKUP = {
@@ -31,16 +67,29 @@ TROP_REVERSE_LOOKUP = {
     TROP.SUB : "-",
     TROP.MUL : "*",
     TROP.DIV : "/",
-    TROP.RAND : "RAND",
-    TROP.RANGE: "RANGE",
+    TROP.RAND : "<->",
     TROP.REMAIN : "%",
-    TROP.ROUND: "_"
+    TROP.ROUND: "_",
+    TROP.NEG : "-",
+    TROP.REGEX : "~="    
 }
+
+TROP_PARAM_LENGTHS = {
+    TROP.ADD : 2,
+    TROP.SUB : 2,
+    TROP.MUL : 2,
+    TROP.DIV : 2,
+    TROP.RAND : 2,
+    TROP.REMAIN : 2,
+    TROP.ROUND: 1,
+    TROP.NEG : 1,
+    TROP.REGEX : 3
+}
+
 
 class TransformComponent:
 
     def __init__(self, op, source, value=None, bind=None, rebind=None):
-        assert(not(value is None and bind is None))
         assert(isinstance(op, TROP))
         if bind is not None:
             assert(isinstance(source, Bind))
@@ -57,17 +106,41 @@ class TransformComponent:
     def verify_op(self):
         if self.op not in TROP_LOOKUP:
             raise Exception("Unknown Op: {}".format(self.op))
+
+    def setRebind(self, bind):
+        assert(isinstance(bind, Bind))
+        assert(self.rebind is None)
+        self.rebind = bind
         
     def __repr__(self):
-        if self.val is not None:
-            rhs = self.val
+        op = TROP_REVERSE_LOOKUP[self.op]
+        source = repr(self.source)
+        if self.rebind is not None:
+            rebind = " -> {}".format(repr(self.rebind))
         else:
-            rhs = self.bind
-        if self.rebind is None:
-            return "{} {} {}".format(self.source, TROP_REVERSE_LOOKUP[self.op], rhs)
+            rebind = ""
+        if self.val is not None and isinstance(self.val, float):
+            value = str(self.val)
+            value = value.replace(".","d")
+        elif self.val is not None:
+            value = str(self.val)
         else:
-            return "{} {} {} -> {}".format(self.source, TROP_REVERSE_LOOKUP[self.op],
-                                           rhs, self.rebind)
+            value = ""
+        if self.bind is not None:
+            bind = repr(self.bind)
+        else:
+            bind = ""
+            
+        assert(self.op in TROP_PARAM_LENGTHS)
+        param_length = TROP_PARAM_LENGTHS[self.op]
+        if param_length == 1:
+            return "{}{}{}".format(op, source, rebind)
+        elif param_length == 2:
+            return "{} {} {}{}{}".format(source, op, value, bind, rebind)
+        elif param_length == 3:
+            return "{} {} /{}/ {}{}".format(source, op, self.val,
+                                          repr(self.bind), rebind)
+
         
 class Transform:
     #todo: add ability to select n contexts, sort contexts by parameters,
