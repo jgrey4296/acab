eb280337db8275b6c29960be580c5345e92a72f5
John Grey <jgrey@ucsc.edu>
2017-08-24 21:38:58 +0100

    added select operator, split transform components into separate classes

--- pyRule/Transforms.py
+++ pyRule/Transforms.py
@@ -8,7 +8,7 @@ from re import sub
 import IPython
 logging = root_logger.getLogger(__name__)
 
-TROP = Enum("Transform_ops", "ADD SUB MUL DIV RAND REMAIN ROUND NEG REGEX FORMAT SELECT")
+TROP = Enum("Transform_ops", "ADD SUB MUL DIV RAND REMAIN ROUND NEG REGEX FORMAT SELECT SELECT_ALL")
 #todo: add regex transform
 
 def ADD(a, b):
@@ -61,7 +61,8 @@ TROP_LOOKUP = {
     TROP.ROUND : ROUND,
     TROP.NEG : NEG,
     TROP.REGEX : REGEX,
-    TROP.FORMAT: FORMAT
+    TROP.FORMAT: FORMAT,
+    TROP.SELECT : SELECT    
 }
 
 TROP_REVERSE_LOOKUP = {
@@ -74,7 +75,8 @@ TROP_REVERSE_LOOKUP = {
     TROP.ROUND: "_",
     TROP.NEG : "-",
     TROP.REGEX : "~="  ,
-    TROP.FORMAT : "~{}"
+    TROP.FORMAT : "~{}",
+    TROP.SELECT : "select"
 }
 
 TROP_PARAM_LENGTHS = {
@@ -87,22 +89,50 @@ TROP_PARAM_LENGTHS = {
     TROP.ROUND: 1,
     TROP.NEG : 1,
     TROP.REGEX : 3,
-    TROP.FORMAT: 1
-    
+    TROP.FORMAT: 1,
+    TROP.SELECT : 2
 }
 
 
 class TransformComponent:
-
-    def __init__(self, op, source, value=None, bind=None, rebind=None):
+    """ Superclass of Transforms """
+    def __init__(self, op):
         assert(isinstance(op, TROP))
+        self.op = op
+    
+class SelectionTransform(TransformComponent):
+    def __init__(self, lBound, uBound, op=TROP.SELECT):
+        super().__init__(op)
+        self.lBound = lBound
+        self.uBound = uBound
+
+    def __repr__(self):
+        if self.lBound is TROP.SELECT_ALL:
+            lbound = "_"
+        elif isinstance(self.lBound, Bind):
+            lbound = repr(self.lBound)
+        else:
+            lbound = str(self.lBound)
+            
+        if self.uBound is TROP.SELECT_ALL:
+            ubound = "_"
+        elif isinstance(self.uBound, Bind):
+            ubound = repr(self.uBound)
+        else:
+            ubound = str(self.uBound)
+            
+        return "select {} - {}".format(lbound, ubound)
+
+        
+class OperatorTransform(TransformComponent):
+    def __init__(self, op, source, value=None, bind=None, rebind=None):
+        super().__init__(op)
         if bind is not None:
             assert(isinstance(source, Bind))
         if rebind is not None:
             assert(isinstance(rebind, Bind))
         assert(isinstance(op, TROP) or isinstance(op, str))
             
-        self.op = op
         self.source = source
         self.val = value
         self.bind = bind
@@ -153,15 +183,32 @@ class Transform:
     #have min and max bounds
     def __init__(self, components):
         assert(all([isinstance(x, TransformComponent) for x in components]))
-        self.components = components
+        selection = [x for x in components if isinstance(x, SelectionTransform)]
+        if len(selection) == 1:
+            self.selection = selection[0]
+        else:
+            self.selection = None
+        self.components = [x for x in components if not isinstance(x, SelectionTransform)]
+
 
     def __repr__(self):
-        return "{}".format(", ".join([repr(x) for x in self.components]))
+        if self.selection is not None:
+            sel = repr(self.selection)
+        else:
+            sel = ""
+        
+        return "{}{}".format(sel,", ".join([repr(x) for x in self.components]))
 
     def verify_ops(self):
         for x in self.components:
             x.verify_op()
-        
+
+    def getSelectionBounds(self):
+        if self.selection is None:
+            return (None, None)
+        else:
+            return (self.selection.lBound, self.selection.uBound)            
+            
     def get_input_requirements(self):
         #return the set of input bound names
         return set([])
