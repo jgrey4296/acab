7479daf6cf33fa3be962448c72d141cffe965bee
John Grey <jgrey@ucsc.edu>
2017-08-30 10:55:34 +0100

    added rule expand binding method, and some stubs

--- pyRule/trie/Rule.py
+++ pyRule/trie/Rule.py
@@ -1,9 +1,10 @@
 import logging as root_logger
 from .Query import Query
+from .Node import Node
 import pyRule.utils as util
 import pyRule.Transforms as T
 import pyRule.Actions as A
-
+import IPython
 logging = root_logger.getLogger(__name__)
 
 class Rule:
@@ -17,22 +18,21 @@ class Rule:
         assert(transform is None or isinstance(transform, T.Transform))
         assert(tags is None or all([isinstance(x, str) for x in tags]))
         if name is None:
-            #todo: make this a node based anon name
-            self._name = "anon_{}".format(Rule.__count)
+            #todo: convert this to a fact string
+            self._name = ".rule.anon_{}".format(Rule.__count)
             Rule.__count += 1
         else:
+            assert(isinstance(name, list))
             self._name = name
         self._query = query
         self._transform = transform
         self._actions = actions
-        #todo: fill set
         if tags is not None:
             self._tags = set(tags)
         else:
             self._tags = set()
             
     def __repr__(self):
-        #todo: turn tags back into #tag
         nameStr = "".join([repr(x) for x in self._name[1:]])
         tagsStr = ", ".join(["#{}".format(x) for x in self._tags])
         if self._query is not None:
@@ -64,3 +64,25 @@ class Rule:
         #if nothing raises an exception:
         return True
 
+
+    def to_node_lists(self):
+        """ Convert a rule to a list of node lists  """
+        return []
+
+    @staticmethod
+    def from_trie(self, node):
+        """ given a root node of a trie, create a rule from it """
+        return Rule()
+
+    def expandBindings(self, bindings):
+        #expand the name
+        newName = util.expandFact(self._name, bindings)
+        #expand the query
+        newQuery = self._query.expandBindings(bindings)
+        #expand the actions
+        newActions = [x.expandBindings(bindings) for x in self._actions]
+        return Rule(newQuery,
+                    newActions,
+                    transform=self._transform,
+                    name=newName,
+                    tags=self._tags)
