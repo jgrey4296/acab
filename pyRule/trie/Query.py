5a7582dbafc95ba85daa7c8f133f76c24d19e350
John Grey <jgrey@ucsc.edu>
2017-08-30 10:54:59 +0100

    added expandBindings to Query, which triggers clause expandbindings, simplified arguments

--- pyRule/trie/Query.py
+++ pyRule/trie/Query.py
@@ -1,14 +1,15 @@
 from .Clause import Clause
+import IPython
 
 class Query:
     """ A Query for the Trie Knowledge base """
 
-    def __init__(self, *args, negated=False):
+    def __init__(self, clauses):
         #ATTENTION: List of clauses, not List of tuples
         #[Clause]
         #Each clause is a list of tests and bindings
         self._clauses = []
-        for clause in args:
+        for clause in clauses:
             assert(isinstance(clause, Clause))
             self._clauses.append(clause)
 
@@ -28,3 +29,9 @@ class Query:
     def __repr__(self):
         clauseStrs = [repr(x) for x in self._clauses]
         return "\n\t".join(clauseStrs)
+
+    def expandBindings(self, bindings):
+        newClauses = []
+        for x in self._clauses:
+            newClauses.append(x.expandBindings(bindings))
+        return Query(newClauses)
