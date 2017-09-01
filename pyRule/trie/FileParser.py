fca996bcad55e117e74c48779b603b3733112bb3
John Grey <jgrey@ucsc.edu>
2017-09-01 12:00:30 +0100

    added action macros, and related parse post processing, to file parser

--- pyRule/trie/FileParser.py
+++ pyRule/trie/FileParser.py
@@ -10,19 +10,34 @@ import IPython
 
 from . import FactParser as FP
 from . import RuleParser as RP
+from . import ActionParser as AP
 from .Rule import Rule
+from pyRule.Actions import ActionMacro
+
 
 parseBindings = {}
 
-def split_into_rules_and_assertions(toks):
+def final_pass(toks):
+    """ The final action of the file parser.
+    splits out rules and assertions, 
+    and expands action macros in rules into the actual
+    action sequences """
     rules = []
     assertions = []
+    action_macros = {}
     for x in toks:
         if isinstance(x, Rule):
             rules.append(x)
+        elif isinstance(x, ActionMacro):
+            action_macros[x._name] = x
         else:
             assertions.append(x)
-    return (rules, assertions)
+    #everything has been parsed,
+    #clear the parse bindings as a guard:
+    parseBindings = {}
+    #and expand rulemacros into rule sequences:
+    expandedActMacroRules = [x.expandActionMacros(action_macros) for x in rules]    
+    return (expandedActMacroRules, assertions)
 
 def add_file_binding(toks):
     """ Store the string of in the binding """
@@ -34,7 +49,7 @@ def add_file_binding(toks):
     parseBindings[binding.value] = string
     return []
 
-def expandBinding(toks):
+def expansion_pass(toks):
     """ Expand any bindings that are stored in the parse """
     if len(toks) == 0:
         return toks
@@ -65,28 +80,29 @@ bindArrow = s(pp.Literal('<-'))
 clear = s(pp.Literal('clear'))
 
 fileBind = FP.BIND + bindArrow + FP.param_fact_string
-clearBind = clear + pp.OneOrMore(FP.BIND)
+clearBind = clear + orm(FP.BIND)
 
 
 
 
-rule_or_assertion = pp.MatchFirst([s(comment),
-                                   s(fileBind),
-                                   s(clearBind),
-                                   RP.rule,
-                                   FP.param_fact_string])
+file_component = pp.MatchFirst([s(comment),
+                                s(fileBind),
+                                s(clearBind),
+                                AP.action_definition,
+                                RP.rule,
+                                FP.param_fact_string])
 
-manyRorA = rule_or_assertion + pp.ZeroOrMore(s(orm(pp.lineEnd)) + rule_or_assertion)
+file_total = file_component + pp.ZeroOrMore(s(orm(pp.lineEnd)) + file_component)
 
 #Parse Actions
-manyRorA.setParseAction(split_into_rules_and_assertions)
+file_total.setParseAction(final_pass)
 fileBind.setParseAction(add_file_binding)
 clearBind.setParseAction(clearBinding)
-rule_or_assertion.setParseAction(expandBinding)
+file_component.setParseAction(expansion_pass)
 
 def parseString(s):
     assert(isinstance(s, str))
-    return manyRorA.parseString(s)[0]
+    return file_total.parseString(s)[0]
  
 
 
