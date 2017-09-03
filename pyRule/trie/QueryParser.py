3c0c231857fd72e4b0fc07f2829a1e58e116f3ca
John Grey <jgrey@ucsc.edu>
2017-09-03 20:57:37 +0100

    implemented clause fallback parsing

--- pyRule/trie/QueryParser.py
+++ pyRule/trie/QueryParser.py
@@ -2,19 +2,30 @@ import logging as root_logger
 import pyparsing as pp
 from pyRule.utils import Bind,META_OP
 from pyRule.Comparisons import COMP, Comparison, COMP_REVERSE_LOOKUP
-from .FactParser import COMMA, VALBIND, PARAM_CORE, BIND
+from .FactParser import COMMA, VALBIND, PARAM_CORE, BIND, COLON, VALUE
 from .Query import Query
 from .Clause import Clause
 
 pp.ParserElement.setDefaultWhitespaceChars(' \t\r')
+
 logging = root_logger.getLogger(__name__)
+
+FALLBACK_IDEN = 'fallback'
+MAIN_IDEN = 'main'
+NOT_IDEN = 'NOT'
 
 def buildClause(toks):
     #detect negation and annotate the clause with it
-    if 'NOT' in toks:
-        return Clause(toks[1:], negated=True)
+    if FALLBACK_IDEN in toks:
+        fallback_toks = toks.fallback[:]
     else:
-        return Clause(toks)
+        fallback_toks = None
+    if NOT_IDEN in toks:
+        if fallback_toks is not None:
+            raise Exception("Fallback bindings in negated clauses don't make sense")
+        return Clause(toks.main[:], negated=True)
+    else:
+        return Clause(toks.main[:], fallback=fallback_toks)
 
 def buildComparison(op, value):
     if isinstance(value, Bind):
@@ -42,10 +53,11 @@ GT = pp.Literal(COMP_REVERSE_LOOKUP[COMP.GT])
 NE = pp.Literal(COMP_REVERSE_LOOKUP[COMP.NE])
 EQ = pp.Literal(COMP_REVERSE_LOOKUP[COMP.EQ])
 REGMATCH = pp.Literal(COMP_REVERSE_LOOKUP[COMP.RE])
-NOT = pp.Literal('~').setResultsName('NOT')
+NOT = pp.Literal('~').setResultsName(NOT_IDEN)
 SLASH = s(pp.Literal('/'))
 REGEX = pp.Regex(r'/.+?/')
 COMP_OP = pp.Or([LT, GT, NE, EQ, REGMATCH])
+DOUBLEBAR = pp.Literal('||')
 
 COMP_Internal = COMP_OP + pp.Or([VALBIND, REGEX])
 
@@ -53,11 +65,16 @@ comparison = OPAR + COMP_Internal \
              + op(pp.OneOrMore(COMMA + COMP_Internal))\
              + CPAR
 
+assignment = BIND + COLON + VALUE
+assignmentList = assignment + pp.ZeroOrMore(COMMA + assignment)
+fallback = s(DOUBLEBAR) + assignmentList
+
 #core component of a query, a modified param_fact_string
 QueryCore = PARAM_CORE + op(comparison)
 
 #Core Query Chain
-clause = op(NOT) + op(BIND) + pp.OneOrMore(QueryCore) + s(QMARK)
+clause = op(NOT) + (op(BIND) + pp.OneOrMore(QueryCore)).setResultsName(MAIN_IDEN) \
+         + s(QMARK) + op(fallback).setResultsName(FALLBACK_IDEN)
 
 clauses = clause + pp.ZeroOrMore(COMMA + clause)
 
@@ -76,8 +93,11 @@ QueryCore.setParseAction(buildQueryComponent)
 clause.setParseAction(buildClause)
 clauses.setParseAction(lambda toks: Query(toks[:]))
 
+#assignment:
+assignment.setParseAction(lambda toks: (toks[0], toks[1]))
+
+
 #Main parser:
 def parseString(s):
     """ .a.b(>20)!d.$X, ... -> Query """
     return clauses.parseString(s)[0]
-
