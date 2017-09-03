1848084f439a23d3f0ca64c094dd53b9f844b39c
John Grey <jgrey@ucsc.edu>
2017-09-03 12:24:57 +0100

    implemented initial basic working bdi agent engine

--- pyRule/bdi_engine.py
+++ pyRule/bdi_engine.py
@@ -6,72 +6,84 @@ from time import sleep
 import pyRule.trie as T
 import pyRule.utils as util
 
-class BDI_ENGINE:
+class Agent:
       """ A Simple Agent  """
 
-    def __init__(self, name, startup_files,
-                 rule_sequence,
-                 action_policy
-                 wait_time=0.5):
-        logging.info("Initialising BDI: {}, {}".format(name, startup_file))
-        assert(isinstance(rule_sequence, list))
-        #Agent Data
-        self._name = name
-        self._wait_time = wait_time
-        self._rule_sequence = rule_sequence
-        self._action_policy = action_policy        
+      def __init__(self, name,
+                   startup_files,
+                   rule_seq_pol,
+                   wait_time=0.5,
+                   actions=None):
 
-        #Agent Structure
-        self._state_query = ".agent.{}.state.finished?".format(name)
-        self._state_temp_data = ".agent.{}.state.temp.$x".format(name)
-        self._state_busy_on_action = ".agent.{}.state.busy".format(name)
-        self._engine = T.Engine()
+            assert(isinstance(startup_files, list))
+            assert(isinstance(rule_seq_pol, list))
+            assert(all([isinstance(x, tuple) for x in rule_seq_pol]))
+            assert(all([x[0] is None or isinstance(x[0], str) for x in rule_seq_pol]))
+            assert(all([x[1] is None or callable(x[1]) for x in rule_seq_pol]))
+            logging.info("Initialising BDI: {}, {}".format(name, ", ".join(startup_files)))
+            #Agent Data
+            self._name = name
+            self._wait_time = wait_time
+            #pairings of rule sequence and selection policies:
+            self._rule_seq_pols = rule_seq_pol
 
-        #Run data
-        self._assertion_queue = []
+            #Agent Structure
+            self._state_query = "~.agent.{}.state.finished?".format(name)
+            self._state_temp_data = ".agent.{}.state.temp.$x".format(name)
+            self._state_busy_on_action = ".agent.{}.state.busy".format(name)
+            self._engine = T.Engine()
 
-        self.load_files(startup_files)
-        #todo: register actions
+            #Runtime data
+            self._assertion_queue = []
+
+            #registration of custom actions
+            if actions is not None:
+                  assert(isinstance(actions, dict))
+                  for k,v in actions.items():
+                        self._engine.register_action(k, v)
+
+            #initialisation of startup data
+            for filename in startup_files:
+                  self._engine.load_file(filename)
+
+            #initialisation of agent data
+            self._engine.add(".agent.{}.state".format(name))
+                  
+      def add_to_assertion_queue(self, s):
+            assert(isinstance(s, (str, list)))
+            self._assertion_queue.append(s)
+
+      def _assert_queue(self):
+            queue = self._assertion_queue
+            while len(queue) > 0:
+                  next = queue.pop(0)
+                  self._engine.add(next)
+            self._assertion_queue = []
+
+      def _run_rule_sequence(self):
+            logging.debug("Running rule sequence")
+            for rule_spec, policy in self._rule_seq_pols:
+                  logging.debug("Running rule_spec: {} with policy: {}".format(rule_spec,
+                                                                               policy))
+                  self._engine._run_rules(rule_tags=rule_spec.split(" "), policy=policy)
+
+      def _clear_temp_data(self):
+            self._engine.retract(self._state_temp_data)
+            self._engine.clear_proposed_actions()
         
-    def load_files(self, files):
-        assert(isinstance(files, list))
-        for f in files:
-            self._engine.load_file(f)
-        
-    def add_to_assertion_queue(self, string):
-        assert(isinstance(s, str) or isinstance(s, list))
-        self._assertion_queue.append(string)
+      def run(self):
+            logging.info("Running Agent: {}".format(self._name))
+            while self._engine.query(self._state_query):
+                  #assert anything in the queue
+                  self._assert_queue()
+                  #run rules
+                  self._run_rule_sequence()
+                  #clear temporary data
+                  self._clear_temp_data()
+                  #Wait
+                  sleep(self._wait_time)
+            logging.info("Shutting Down Agent: {}".format(self._name))
 
-    def _assert_queue(self):
-        queue = self._assertion_queue
-        while len(queue) > 0:
-            next = queue.pop(0)
-            self._engine.add(next)
-        self._assertion_queue = []
 
-    def _run_rule_sequence(self):
-        for rule_spec in self._rule_sequence:
-            self._engine.run_rules(rule_spec)
-
-    def _select_and_perform(self):
-        #if not busy or interrupt actions are specified:
-        #run policy and perform
-        return
-
-    def _clear_temp_data(self):
-        self._engine.retract(self._state_temp_data)
-        
-    def run(self):
-        logging.info("Running Agent: {}".format(self._name))
-        while not self._engine.query(self._state_query):
-            #assert anything in the queue
-            self._assert_queue()
-            #run rules
-            self.run_rule_sequence()
-            #select action and perform
-            self._select_and_perform()
-            #clear temporary data
-            self._clear_temp_data()
-            #Wait
-            sleep(self._wait_time)
-        logging.info("Shutting Down Agent: {}".format(self._name))
+      def num_rules(self):
+            return len(self._engine)
