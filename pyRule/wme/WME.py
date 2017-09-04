d8f63bd14a88b4bd469e1da18332d355b7109199
John Grey <jgrey@ucsc.edu>
2017-09-04 11:22:17 +0100

    added a repr for wme

--- pyRule/wme/WME.py
+++ pyRule/wme/WME.py
@@ -6,6 +6,9 @@ class WME:
           self._data = data.copy()
           self._hash = hash(str(self._data) + str(self._assertTime))
 
+      def __repr__(self):
+            return repr(self._data)
+          
       def __hash__(self):
           return self._hash
 
