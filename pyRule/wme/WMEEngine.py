48de968d810a391c9b7499eaa5a5eebb7ca9c149
John Grey <jgrey@ucsc.edu>
2017-09-04 11:21:10 +0100

    added WMEClause subclass, and stub WMEEngine

---
+++ pyRule/wme/WMEEngine.py
@@ -0,0 +1,31 @@
+import logging as root_logger
+from pyRule.EngineBase import EngineBase
+from pyRule import utils as util
+from .FactBase import FactBase
+logging = root_logger.getLogger(__name__)
+
+
+class WMEEngine(EngineBase):
+    """ The base class that wme and trie versions implement the interface of """
+    
+    def __init__(self, path=None, init=None):
+        super().__init__(FactBase, path=path, init=init)
+
+        
+    def load_file(self, filename):
+        """ Load a file spec for the facts / rules for this engine """
+        raise Exception("Base Engine Stub")
+        return
+
+    def registerRules(self, s):
+        """ Register passed in rule specifications """
+        raise Exception("Base Engine Stub")
+        
+    def add(self, s):
+        """ Assert a new fact into the engine """
+        raise Exception("Base Engine Stub")
+
+    def retract(self, s):
+        """ Remove a fact from the engine """
+        raise Exception("Base Engine Stub")
+
