ddaa806191aa350da277d1471fff30b445390c96
John Grey <jgrey@ucsc.edu>
2017-09-27 12:50:19 +0100

    added some rule query parsing code

--- pyRule/trie/QueryParser.py
+++ pyRule/trie/QueryParser.py
@@ -6,7 +6,8 @@ from pyRule.Comparisons import COMP, Comparison, COMP_REVERSE_LOOKUP
 from pyRule.Query import Query
 from pyRule.Clause import Clause
 from .FactParser import COMMA, VALBIND, PARAM_CORE, BIND, COLON, VALUE
-
+from . import RuleQueryParser as RQP
+import IPython
 pp.ParserElement.setDefaultWhitespaceChars(' \t\r')
 
 logging = root_logger.getLogger(__name__)
@@ -14,6 +15,8 @@ logging = root_logger.getLogger(__name__)
 FALLBACK_IDEN = 'fallback'
 MAIN_IDEN = 'main'
 NOT_IDEN = 'NOT'
+COMP_IDEN = 'comparison'
+RULEBIND_IDEN = 'rulebind'
 
 def buildClause(toks):
     #detect negation and annotate the clause with it
@@ -36,8 +39,12 @@ def buildComparison(operator, value):
 
 def buildQueryComponent(toks):
     node = toks[0]
-    comparisons = toks[1:]
-    node.set_meta_eval(META_OP.COMP, comparisons)
+    if COMP_IDEN in toks:
+        comparisons = toks[COMP_IDEN][:]
+        node.set_meta_eval(META_OP.COMP, comparisons)
+    elif RULEBIND_IDEN in toks:
+        rulebind = toks[RULEBIND_IDEN][0]
+        node.set_meta_eval(META_OP.RULEBIND, rulebind)
     return node
 
 
@@ -47,7 +54,7 @@ opLn = s(op(pp.LineEnd()))
 
 OPAR = s(pp.Literal('('))
 CPAR = s(pp.Literal(')'))
-QMARK = pp.Literal('?')
+QMARK = s(pp.Literal('?'))
 
 LT = pp.Literal(COMP_REVERSE_LOOKUP[COMP.LT])
 GT = pp.Literal(COMP_REVERSE_LOOKUP[COMP.GT])
@@ -59,38 +66,47 @@ SLASH = s(pp.Literal('/'))
 REGEX = pp.Regex(r'/.+?/')
 COMP_OP = pp.Or([LT, GT, NE, EQ, REGMATCH])
 DOUBLEBAR = pp.Literal('||')
+META = s(pp.Literal('^'))
 
 COMP_Internal = COMP_OP + pp.Or([VALBIND, REGEX])
+
+#defined earlier to work with named copies
+LT.setParseAction(lambda toks: COMP.LT)
+GT.setParseAction(lambda toks: COMP.GT)
+NE.setParseAction(lambda toks: COMP.NE)
+EQ.setParseAction(lambda toks: COMP.EQ)
+REGMATCH.setParseAction(lambda toks: COMP.RE)
+COMP_Internal.setParseAction(lambda toks: buildComparison(toks[0], toks[1]))
+REGEX.setParseAction(lambda toks: toks[0][1:-1])
+
+
+#TODO: add comparison features for testing rule components
+#ie: has tag, has penumbral conditions,
+#has penumbral actions
+ruleBind = OPAR + META + BIND + CPAR
+ruleBind = ruleBind(RULEBIND_IDEN)
 
 comparison = OPAR + COMP_Internal \
              + op(pp.OneOrMore(COMMA + COMP_Internal))\
              + CPAR
+comparison = comparison(COMP_IDEN)
 
 assignment = BIND + COLON + VALUE
 assignmentList = assignment + pp.ZeroOrMore(COMMA + assignment)
 fallback = s(DOUBLEBAR) + assignmentList
 
 #core component of a query, a modified param_fact_string
-QueryCore = PARAM_CORE + op(comparison)
+QueryCore = PARAM_CORE + op(pp.Or([comparison,
+                                   ruleBind]))
 
 #Core Query Chain
 clause = op(NOT) + (op(BIND) + pp.OneOrMore(QueryCore)).setResultsName(MAIN_IDEN) \
-         + s(QMARK) + op(fallback).setResultsName(FALLBACK_IDEN)
+         + QMARK + op(fallback).setResultsName(FALLBACK_IDEN)
 
 clauses = clause + pp.ZeroOrMore(COMMA + clause)
 
 #Actions
-
-COMP_Internal.setParseAction(lambda toks: buildComparison(toks[0], toks[1]))
-LT.setParseAction(lambda toks: COMP.LT)
-GT.setParseAction(lambda toks: COMP.GT)
-NE.setParseAction(lambda toks: COMP.NE)
-EQ.setParseAction(lambda toks: COMP.EQ)
-REGMATCH.setParseAction(lambda toks: COMP.RE)
-
-REGEX.setParseAction(lambda toks: toks[0][1:-1])
 QueryCore.setParseAction(buildQueryComponent)
-#Clause, not negated:
 clause.setParseAction(buildClause)
 clauses.setParseAction(lambda toks: Query(toks[:]))
 
