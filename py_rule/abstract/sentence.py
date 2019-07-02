ed3217275bc50cdcd12a919e4b33832864cc1c21
John Grey <jgrey@ucsc.edu>
2019-07-02 23:34:02 +0100

    factored out clause into sentence

--- py_rule/abstract/sentence.py
+++ py_rule/abstract/sentence.py
@@ -2,15 +2,26 @@ import IPython
 
 class Sentence:
 
-    def __init__(self, words=None):
+    def __init__(self, words=None, negated=False, fallback=None):
         self._words = []
+        self._negated = negated
+        self._fallback = []
         if words is not None:
             self._words += words
+        if fallback is not None:
+            self._fallback += fallback[:]
 
     def __str__(self):
         result = "".join([str(x) for x in self._words[:-1]])
         result += self._words[-1].opless_print()
-        return result
+        negated_str = ""
+        fallback_str = ""
+        if self._fallback is not None:
+            fallback_str = " || " + ", ".join(["${}:{}".format(x[0], x[1]) for x in self._fallback])
+        if self._negated:
+            negated_str = "~"
+
+        return "{}{}{}".format(negated_str, result, fallback_str)
 
     def __repr__(self):
         return "Sentence({})".format(str(self))
