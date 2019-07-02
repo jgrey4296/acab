07e47083751f8560c10dc8e5eceead0eb4d3d853
John Grey <jgrey@ucsc.edu>
2019-07-02 23:37:04 +0100

    removed obsolete code from fact node

--- py_rule/trie/nodes/fact_node.py
+++ py_rule/trie/nodes/fact_node.py
@@ -1,7 +1,7 @@
 """ The Core Trie-Node, stores information, meta data """
 from .trie_node import TrieNode
 from math import floor
-from py_rule.utils import EXOP
+from py_rule.utils import EXOP, ROOT
 import IPython
 import logging as root_logger
 import py_rule.utils as util
@@ -18,49 +18,42 @@ class FactNode(TrieNode):
     @staticmethod
     def Root():
         """ Get a Root designated node """
-        #TODO: make this a singleton?
-        return FactNode("root", EXOP.ROOT)
+        return FactNode(ROOT, EXOP.DOT)
 
     def is_root(self):
         """ Query to check if this node is the Root Node """
         return self._op is EXOP.ROOT
 
 
-    def __init__(self, value, operator,
+    @staticmethod
+    def copy_fact(node):
+        if isinstance(node, FactNode):
+            return node.copy()
+        else:
+            assert(isinstance(node, TrieNode))
+            operator = EXOP.DOT
+            if 'op' in node._data:
+                operator = node._data['op']
+            new_node = FactNode(node._value,
+                                operator,
+                                data=node._data)
+            return new_node
+
+    def __init__(self,
+                 value,
+                 operator,
                  parent=None,
-                 meta_leaf=None,
-                 meta_eval=None):
+                 data=None):
         assert(isinstance(operator, EXOP))
         if parent is not None:
             parent = weakref.ref(parent)
 
-        bind = False
-        if isinstance(value, util.Bind):
-            value = value.value
-            bind = True
-
-        super().__init__(value)
+        super().__init__(value, data)
 
         self._parent = parent
         self._dirty = True
         self._cached = []
         self._op = operator
-
-        #meta field holds ordering for leaf pathways
-        #and eval details as a subtrie for other nodes
-        self._is_meta = False #todo: implmement _is_meta
-        self._meta_leaf = {}
-        self._meta_eval = {}
-
-        #after value setting to override based on passed in meta ops
-        self.set_meta_eval(util.META_OP.BIND, bind)
-        if meta_eval is not None:
-            for k, v in meta_eval.items():
-                self.set_meta_eval(k, v)
-        if meta_leaf is not None:
-            for k, v in meta_leaf.items():
-                self.set_meta_leaf(k, v)
-
 
     def __eq__(self, other):
         """ Main comparison routine: turn to strings, compare """
@@ -82,124 +75,29 @@ class FactNode(TrieNode):
         comp = all([self._children[x] == other._children[x] for x in self._children])
         return comp
 
-    def _print_node(self):
-        """ Return a representation of this particular node """
-        #operator stringify
-        op = util.EXOP_lookup[self._op]
-        if self._meta_eval[util.META_OP.BIND]:
-            bind = "$"
-        else:
-            bind = ""
-
-        #reconverting floats
-        if isinstance(self._value, float):
-            f = str(self._value)
-            val = f.replace(".", "d")
-            #value stringify
-        elif isinstance(self._value, int):
-            val = str(self._value)
-        elif ' ' in self._value:
-            val = '"' + str(self._value) + '"'
-        else:
-            val = str(self._value)
-        #Then convert any meta comparisons:
-        if util.META_OP.COMP in self._meta_eval \
-           and bool(self._meta_eval[util.META_OP.COMP]):
-            meta = "(" \
-                   + ", ".join([repr(x) for x in self._meta_eval[util.META_OP.COMP]]) \
-                   + ")"
-        elif util.META_OP.RULEBIND in self._meta_eval:
-            meta = "(^{})".format(self._meta_eval[util.META_OP.RULEBIND])
-        else:
-            meta = ""
-
-        final_val = "{}{}{}{}".format(op, bind, val, meta)
-        return final_val
-
-    def _print_sub_trie(self):
-        """ Create a multi line string of the entire sub trie """
-        #A list of each self->leaf path
-        leaf_list = self._reconstruct()
-
-        if not bool(leaf_list):
-            return self._print_node()
-        else:
-            finals = []
-            #for each path, convert to a repr of it
-            for l in leaf_list:
-                finals.append("".join([x._print_node() for x in l]))
-
-            return ",\n".join(finals)
-
     def __repr__(self):
-        return "FactNode: {}".format(self._print_node())
+        return "FactNode: {}".format(str(self))
 
+    def __hash__(self):
+        return hash(str(self))
 
-    def root_str(self):
-        """ Return a set of EL strings, treating this Node as the Root """
-        xs = [str(y) for x, y in sorted(self._children.items())]
-        return ",\n".join(xs)
 
     def copy(self):
         assert(not bool(self._children))
         #todo: deeper copy
-        meta_evals = self._meta_eval.copy()
-        meta_leaf = self._meta_leaf.copy()
-        return FactNode(self._value, self._op,
-                    meta_leaf=meta_leaf,
-                    meta_eval=meta_evals)
+        return FactNode(self._value, self._op)
 
     def set_parent(self, parent):
         assert(isinstance(parent, FactNode))
         self._parent = weakref.ref(parent)
 
 
-    def is_exclusive(self):
-        """ Checks for the exclusion operator in this node """
-        return self._op is util.EXOP.EX
-
-    def looks_exclusive(self):
-        """ Checks for implicit exclusivity by having 0 or 1 children """
-        return len(self) <= 1
-
-
-    def set_meta_leaf(self, mType, value):
-        """ Annotate this Node with a Meta-Leaf.
-        (A Meta-Leaf is a Meta-Node that sets its container to be a leaf)
-        """
-        logging.info("Setting Meta Leaf: {}, {}, {}".format(repr(self),
-                                                            str(mType),
-                                                            repr(value)))
-        assert(isinstance(mType, util.META_OP))
-        self._meta_leaf[mType] = value
-
-    def set_meta_eval(self, mType, values):
-        """
-        Set the Node to hold a Meta-Node which can be evaluated.
-        """
-        #todo
-        assert(isinstance(mType, util.META_OP))
-        if isinstance(values, list):
-            self._meta_eval[mType] = [x.copy() for x in values]
-        else:
-            self._meta_eval[mType] = values
-
-    def get_meta_eval(self, mType):
-        """ Get any meta-eval nodes from this Node """
-        if mType in self._meta_eval:
-            return self._meta_eval[mType]
-        else:
-            return []
-
-
     def insert(self, fact):
         """ Insert A Node as a Child of this Node """
-        assert(isinstance(fact, FactNode))
         self._set_dirty_chain()
-        copied = fact.copy()
+        copied = FactNode.copy_fact(fact)
         copied.set_parent(self)
-        if copied._op is EXOP.EX \
-           and bool(self._children):
+        if copied._op is EXOP.EX and bool(self._children):
             if copied in self:
                 temp = self.get_child(copied)
             else:
@@ -216,7 +114,7 @@ class FactNode(TrieNode):
 
     def get(self, fact):
         """ Retrieve a Node from this Node """
-        assert(isinstance(fact, FactNode))
+        assert(isinstance(fact, TrieNode))
         if fact in self:
             return self.get_child(fact)
 
@@ -224,13 +122,14 @@ class FactNode(TrieNode):
 
     def delete_node(self, fact):
         """ Remove a Node from this Node """
-        assert(isinstance(fact, FactNode))
+        assert(isinstance(fact, TrieNode))
         if self.remove_child(fact):
             self._set_dirty_chain()
 
+
     def bind(self, data):
         """ Annotate the Node with a Meta-Bind Evaluation """
-        if self.get_meta_eval(util.META_OP.BIND) is False:
+        if not self._data['bind']:
             return self.copy()
         else:
             copied = self.copy()
@@ -238,25 +137,9 @@ class FactNode(TrieNode):
             return copied
 
 
-    def split_tests(self):
-        """ Split tests into (alphas, betas, regexs) """
-        comps = self.get_meta_eval(util.META_OP.COMP)
-        assert(isinstance(comps, list))
-        alphas = []
-        betas = []
-        regexs = []
-        for c in comps:
-            if c.is_regex_test():
-                regexs.append(c)
-            elif c.is_alpha_test():
-                alphas.append(c)
-            else:
-                betas.append(c)
-        return (alphas, betas, regexs)
-
     def search_regex(self, regex):
         """ Test a regex on the Nodes value """
-        result = re.search(regex.value, self._value)
+        result = re.search(regex._value, self._value)
         if result is not None:
             return result.groupdict()
         else:
@@ -300,30 +183,9 @@ class FactNode(TrieNode):
         # { bindNode : [ options ] }
         raise Exception("Unimplemented")
 
-    def _reconstruct(self):
-        """ Internal method for DFS reconstructing min fact/leaf list """
-        if not self._dirty and bool(self._cached):
-            return self._cached
-        #todo: improve caching
-        queue = [([], self)]
-        leaves = []
-        while bool(queue):
-            path, node = queue.pop()
-            newpath = path.copy()
-            newpath.append(node)
-            if bool(node._children):
-                queue += [(newpath, x) for x in node._children.values()]
-            else:
-                leaves.append(newpath)
-
-        self._dirty = False
-        self._cached = leaves
-        return leaves
-
     def _bind_to_value(self, data):
         """ Set the Nodes value to be one retrieved from passed in bindings """
         assert(self._value in data)
         self._value = data[self._value]
 
 
-#todo: add breadth and depth traversal
