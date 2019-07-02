2ae7a0f854c11e32a3d8d528baa87fe65e8a664c
John Grey <jgrey@ucsc.edu>
2019-07-02 23:34:50 +0100

    updated matching to new design

--- py_rule/trie/Matching.py
+++ py_rule/trie/Matching.py
@@ -2,54 +2,55 @@
 Functions to facilitate matching Trie-Nodes to queries
 """
 import logging as root_logger
+import IPython
 from py_rule import utils as util
-from py_rule.abstract.Comparisons import COMP_LOOKUP
-from .nodes.fact_node import FactNode
+import py_rule.abstract.comparisons as Comp
+from .nodes.trie_node import TrieNode
 logging = root_logger.getLogger(__name__)
 
 
 def test_alphas(node, comps):
     """ Run alpha tests against a retrieved value """
-    return all([COMP_LOOKUP[x.op](node._value, x.value) for x in comps])
+    return all([x._op(node._value, x._value._value) for x in comps])
 
 def test_betas(node, comps, data):
     """ Run a beta tests against a retrieved value, with supplied bindings """
-    return all([COMP_LOOKUP[x.op](node._value, data[x.bind.value]) for x in comps])
+    return all([x._op(node._value, data[x._value._value]) for x in comps])
 
 def exclusion_matches(a, b):
     """ Compare the EXOP of a node, with whether that exop
     is in the children of the other node/parent """
-    logging.info("Running exclusion match test")
-    assert(isinstance(a, FactNode))
-    assert(isinstance(b, FactNode))
-    result = a.is_exclusive() and not b.has_exclusive()
+    logging.info("Running exclusion match test: {} {}".format(str(a), str(b)))
+    assert(isinstance(a, TrieNode))
+    assert(isinstance(b, TrieNode))
+    result = a.is_exclusive() and not b.looks_exclusive()
     if result:
-        logging.debug("C: {}, Node: {}, {}".format(a.is_exclusive(),
+        logging.info("C: {}, Node: {}, {}".format(a.is_exclusive(),
                                                    b.is_exclusive(), str(b)))
-        logging.debug("Mismatch EX num")
+        logging.info("Mismatch EX num")
     return result
 
 def non_bind_value_match(a, b, betas, regexs, data):
     """ Compare two values without caring about binding """
     logging.info("Running non bind value match: {}{}".format(repr(b), repr(a)))
-    assert(isinstance(a, FactNode))
-    assert(isinstance(b, FactNode))
+    assert(isinstance(a, TrieNode))
+    assert(isinstance(b, TrieNode))
     tested = False
     newNode, newData = (None, None)
-    if not a.get_meta_eval(util.META_OP.BIND):
-        logging.debug("Not Bind: {}|{}".format(a._value, b._children.keys()))
+    if not a._data['bind']:
+        logging.info("Not Bind: {}|{}".format(str(a) , b._children.keys()))
         tested = True
         if a in b:# and test_betas(a._value, betas,data):
-            logging.debug("Suitable value")
-            newData, newNode = b._children[a._value].test_regexs_for_matching(regexs, data)
+            logging.info("Suitable value")
+            newData, newNode = b.get_child(a).test_regexs_for_matching(regexs, data)
 
     return (tested, newNode, newData)
 
 def existing_bind_match(a, b, betas, regexs, data):
     """ Compare an existing bound value to the children of b """
     logging.info("Running existing bind match: {}{}".format(repr(b), repr(a)))
-    assert(isinstance(a, FactNode))
-    assert(isinstance(b, FactNode))
+    assert(isinstance(a, TrieNode))
+    assert(isinstance(b, TrieNode))
     tested = False
     newNode, newData = (None, None)
     if a._value in data:
@@ -63,12 +64,15 @@ def create_new_bindings(a, b, alphas, betas, regexs, data):
     a: the clause component,
     b: the node
     """
-    logging.info("Creating new bindings: {}{}".format(repr(b), repr(a)))
-    assert(isinstance(a, FactNode))
-    assert(isinstance(b, FactNode))
+    logging.info("Creating new bindings: {} {} ({})".format(repr(b),
+                                                            repr(a),
+                                                            len(alphas)))
+    assert(isinstance(a, TrieNode))
+    assert(isinstance(b, TrieNode))
     output = []
     potentials = b._children.values()
     passing = [x for x in potentials if test_alphas(x, alphas) and test_betas(x, betas, data)]
+    logging.info("Passing: {}".format(len(passing)))
     for x in passing:
         output.append(x.test_regexs_for_matching(regexs,
                                                  data,
