058d40d132c19c2561ca6680fc11d43436cb64d0
John Grey <jgrey@ucsc.edu>
2019-07-03 20:59:21 +0100

    removed obsolete code

--- py_rule/utils.py
+++ py_rule/utils.py
@@ -1,9 +1,6 @@
 """ Cross-module utilities for the rule engines """
 from enum import Enum
-from collections import namedtuple
 from random import choice
-from py_rule.abstract.sentence import Sentence
-import IPython
 
 ROOT = "__root"
 
@@ -28,46 +25,12 @@ META_OP = Enum("Meta Ops", "BIND COMP RULE RULEBIND")
 #for use in query clauses, and actions
 MUTABLE = Enum('Mutablility', "CORE PENUMBRA")
 
-def expandFact(factString, bindings):
-    """ Given a list of fact components, and a dictionary of bindings,
-    reify the fact, using those bindings.
-    ie: .a.b.$x with {x: blah} => .a.b.blah
-    """
-    assert(isinstance(bindings, dict))
-    assert(isinstance(factString, Sentence))
-    output = []
-
-    for x in factString:
-        if x._data['bind'] and x._value in bindings:
-            retrieved = bindings[x._value]
-        else:
-            #early exit if a plain node
-            output.append(x)
-            continue
-
-        if isinstance(retrieved, Sentence) and len(retrieved) > 1:
-            output += retrieved._words
-            continue
-        else:
-            retrieved = retrieved._words[0]
-
-        if retrieved._data['bind']:
-            copyNode = x.copy()
-            copyNode._value = retrieved._value
-            output.append(copyNode)
-        else:
-            copyNode = x.copy()
-            copyNode._value = retrieved._value
-            copyNode._data['bind'] = False
-            output.append(copyNode)
-    return Sentence(output)
-
 def build_rebind_dict(formal, usage):
     """ Build a dictionary for action macro expansion,
     to swap internal formal params for provided usage params """
-    assert(all([isinstance(x, Bind) for x in formal]))
-    fVals = [x.value for x in formal]
-    return dict(zip(fVals, usage))
+    assert(all(['bind' in x._data for x in formal]))
+    fvals = [x.value for x in formal]
+    return dict(zip(fvals, usage))
 
 def default_action_policy(pairings):
     """ A Simple random selection policy """
