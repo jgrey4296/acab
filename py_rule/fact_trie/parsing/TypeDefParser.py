44df9ea12dcb7f1b1af1c9c7dfc65df2726408b2
John Grey <jgrey@ucsc.edu>
2019-07-04 17:26:02 +0100

    implemented type definition function in typedef parser

--- py_rule/fact_trie/parsing/TypeDefParser.py
+++ py_rule/fact_trie/parsing/TypeDefParser.py
@@ -1,27 +1,29 @@
+import IPython
 import pyparsing as pp
 import logging as root_logger
 from . import FactParser as FP
 from py_rule.typing.ex_types import TypeDefinition
 logging = root_logger.getLogger(__name__)
 
-def make_type_def(tokens):
-    #TODO
-    return TypeDefinition()
-
+def make_type_def(toks):
+    return TypeDefinition(toks.SEN[0][-1],
+                          toks.SEN[0],
+                          toks.Structure[:],
+                          toks.TVars[:])
 
 s = pp.Suppress
 op = pp.Optional
-opLn = s(op(pp.lineEnd))
 comment = s(pp.dblSlashComment)
-OPAR = s(pp.Literal('('))
-CPAR = s(pp.Litral(')')
+emptyLine = s(pp.OneOrMore(pp.lineEnd))
 
+VARLIST = FP.OPAR + pp.delimitedList(FP.VALBIND, ', ', False) + FP.CPAR
 
-VARLIST = OPAR + pp.delimtedList(FP.BIND, ', ', False) + CPAR
-
-TYPEDEF = DBLCOLON + N("SEN", FP.basic_fact_string) \
-    + N("TVars", op(VARLIST)) + COLON  + opLn \
-    + N("Structure", FP.param_fact_strings)
-    + s(pp.Keyword("END"))
+TYPEDEF = FP.DBLCOLON + FP.NG("SEN", FP.basic_fact_string) \
+    + FP.N("TVars", op(VARLIST)) + FP.COLON  + emptyLine \
+    + FP.N("Structure", FP.param_fact_strings) + emptyLine \
+    + s(pp.Literal("END"))
 
 TYPEDEF.setParseAction(lambda t: make_type_def(t))
+
+def parseString(in_string):
+    return TYPEDEF.parseString(in_string)
