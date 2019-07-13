96379627ad0de9cfeb04d0af4eb18de2519410b0
John Grey <johngrey4296@gmail.com>
2019-07-13 14:58:04 +0100

    updates time parser to new format to simplify pattern construction

--- py_rule/data_structures/time/parsing/parser.py
+++ py_rule/data_structures/time/parsing/parser.py
@@ -4,44 +4,73 @@ A PyParsing parser to create patterns
 # from .pattern import Pattern
 # from .event import Event
 # from .arc import Arc
-from py_rule.data_structures.time.pattern_constructor import CTOR_ACT, construct_pattern
-from py_rule.data_structures.time.utils import TimeVar
+import IPython
+from py_rule.data_structures.time.pattern_constructor import CTOR_ACT, construct_pattern_simple
+from py_rule.fact_trie.parsing import FactParser as FP
+from py_rule.utils import BIND_S, VALUE_TYPE_S, VALUE_S, NAME_S
 from enum import Enum
 from fractions import Fraction
 import pyparsing as pp
 import logging as root_logger
 
 logging = root_logger.getLogger(__name__)
+
+def make_valbind(tokens):
+    data = {BIND_S : False,
+            'opt' : False,
+            VALUE_TYPE_S : NAME_S }
+    value = None
+    if BIND_S in tokens:
+        value = tokens.bind[0][1]
+        data[BIND_S] = True
+    elif VALUE_S in tokens:
+        value = tokens.value[1]
+        data[VALUE_TYPE_S] = tokens.value[0]
+
+    if 'OPT' in tokens:
+        data['opt'] = True
+
+    return (value, data)
+
 #Base Syntax
-s = pp.Suppress
-op = pp.Optional
-opLn = s(op(pp.lineEnd))
-comment = s(pp.dblSlashComment)
+s = FP.s
+op = FP.op
+opLn = FP.opLn
 
 COMMA = s(pp.Literal(','))
 OBRACKET = s(pp.Literal('['))
 CBRACKET = s(pp.Literal(']'))
+OPAR = s(pp.Literal('('))
+CPAR = s(pp.Literal(')'))
 COLON = s(pp.Literal(':'))
-QUESTION = s(pp.Literal('?'))
+QUESTION = s(pp.Literal('?')).setResultsName('OPT')
 LESS = s(pp.Literal('<'))
 MORE = s(pp.Literal('>'))
 TILDE = s(pp.Literal('~'))
 DOLLAR = s(pp.Literal('$'))
 VBAR = s(pp.Literal('|'))
 
-acts = pp.Or([COMMA, COLON, QUESTION, LESS, MORE, TILDE])
+acts = pp.Or([COMMA, COLON, TILDE])
 
-NAME = pp.Word(pp.alphas)
-VAR = DOLLAR + NAME
+VALUE = FP.VALUE
+BIND = FP.BIND
 
-pattern_contents = pp.OneOrMore(pp.Or([VAR, NAME, acts]))
+Time_VALBIND = pp.Or([FP.N(VALUE_S, VALUE), FP.N(BIND_S, BIND)]) + op(QUESTION)
 
-pattern = pp.OneOrMore(acts)
+pattern_contents = pp.OneOrMore(pp.Or([Time_VALBIND, acts]))
 
-pattern_nested = pp.nestedExpr('[', ']', pattern)
+pattern = pp.Forward()
+openers = pp.Or([OPAR, OBRACKET, LESS])
+closers = pp.Or([CPAR, CBRACKET, MORE])
 
+pattern <<= pp.Group( openers + pp.ZeroOrMore( pattern_contents | pattern )  + closers + op(QUESTION))
+
+main_pattern = s(OBRACKET) + pattern + s(CBRACKET)
 
 # Actions
+Time_VALBIND.setParseAction(make_valbind)
+pattern.setParseAction(construct_pattern_simple)
+main_pattern.setParseAction(lambda x: x[0])
 OBRACKET.setParseAction(lambda x: CTOR_ACT.PSTART)
 CBRACKET.setParseAction(lambda x: CTOR_ACT.PEND)
 COMMA.setParseAction(lambda x: CTOR_ACT.PDUAL)
@@ -50,11 +79,6 @@ LESS.setParseAction(lambda x: CTOR_ACT.CSTART)
 MORE.setParseAction(lambda x: CTOR_ACT.CEND)
 TILDE.setParseAction(lambda x: CTOR_ACT.SIL)
 
-VAR.setParseAction(lambda toks: TimeVar(toks[0]))
-
 def parse_string(s):
     """ The primary access point """
-    parsed = pattern_contents.parseString(s)
-    # Stack based constructor:
-    return construct_pattern(parsed[:])
-
+    return main_pattern.parseString(s)[0][0]
