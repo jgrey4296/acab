7139d00399dc83b48ffac9f6b3a079f5222320c9
John Grey <johngrey4296@gmail.com>
2019-07-16 17:01:34 +0100

    adjusted naming of iterator values

--- py_rule/data_structures/time/pattern_iterator.py
+++ py_rule/data_structures/time/pattern_iterator.py
@@ -9,20 +9,20 @@ logging = root_logger.getLogger(__name__)
 class PatternIterator:
     """ Automates retrieval of pattern values  """
 
-    def __init__(self, pattern, just_values=True, rand_s=None):
-        self.pattern = pattern
-        self.denominator = self.pattern.denominator()
-        self.position = 0
-        self.just_values = just_values
-        self.rand_state = rand_s
+    def __init__(self, pattern, just_values=True, rnd_s=None):
+        self._pattern = pattern
+        self._denominator = self._pattern.denominator()
+        self._position = 0
+        self._just_values = just_values
+        self._rnd_state = rnd_s
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        time = f(self.position, self.denominator)
-        self.position += 1
+        time = f(self._position, self._denominator)
+        self._position += 1
         #if at a new cycle, tick the random generator
-        return self.pattern(time, just_values=self.just_values, rand_s=self.rand_state)
+        return self._pattern(time, just_values=self._just_values, rnd_s=self._rnd_state)
 
     #raise StopIteration
