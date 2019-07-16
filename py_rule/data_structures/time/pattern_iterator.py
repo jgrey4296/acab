6b582c7dd44391c4ac56a4ee64785268020a2c4c
John Grey <johngrey4296@gmail.com>
2019-07-16 19:53:29 +0100

    improved iter, adding cycles and count controls

--- py_rule/data_structures/time/pattern_iterator.py
+++ py_rule/data_structures/time/pattern_iterator.py
@@ -1,6 +1,7 @@
 """
 PatternIterator automates pattern output
 """
+import IPython
 from fractions import Fraction as f
 from .utils import time_str
 import logging as root_logger
@@ -9,20 +10,24 @@ logging = root_logger.getLogger(__name__)
 class PatternIterator:
     """ Automates retrieval of pattern values  """
 
-    def __init__(self, pattern, just_values=True, rnd_s=None):
+    def __init__(self, pattern, just_values=True, rnd_s=None, cycles=1, count=None):
         self._pattern = pattern
         self._denominator = self._pattern.denominator()
         self._position = 0
         self._just_values = just_values
         self._rnd_state = rnd_s
+        self._target = count
+        if self._target is None and cycles is not None:
+            self._target = cycles * self._denominator
 
     def __iter__(self):
         return self
 
     def __next__(self):
         time = f(self._position, self._denominator)
-        self._position += 1
+        if self._target is not None and self._position >= self._target:
+            raise StopIteration()
         #if at a new cycle, tick the random generator
-        return self._pattern(time, just_values=self._just_values, rnd_s=self._rnd_state)
-
-    #raise StopIteration
+        result = self._pattern(time, just_values=self._just_values, rnd_s=self._rnd_state)
+        self._position += 1
+        return result
