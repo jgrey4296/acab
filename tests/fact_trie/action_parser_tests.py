bbdd752c8bc3dd69576b45b9731f90e2258fc4fb
jg <johngrey@Johns-Mac-mini.local>
2019-10-23 14:02:22 +0100

    adds a test for action macro parsing

--- tests/fact_trie/action_parser_tests.py
+++ tests/fact_trie/action_parser_tests.py
@@ -17,59 +17,62 @@ ActionBlah()
 
 class Trie_Action_Parser_Tests(unittest.TestCase):
 
-      def setUp(self):
-            return 1
+    def setUp(self):
+        return 1
 
-      def tearDown(self):
-            return 1
+    def tearDown(self):
+        return 1
 
-      #----------
-      #use testcase snippets
-      def test_simple_action_parse(self):
-            result = AP.parseString("+(20, 30, 40)")[0]
-            self.assertIsInstance(result, actions.Action)
-            self.assertEqual(result._op._op_str, '+')
-            self.assertEqual([x[-1]._value for x in result._values], [20, 30, 40])
+    #----------
+    #use testcase snippets
+    def test_simple_action_parse(self):
+        result = AP.parseString("+(20, 30, 40)")[0]
+        self.assertIsInstance(result, actions.Action)
+        self.assertEqual(result._op._op_str, '+')
+        self.assertEqual([x[-1]._value for x in result._values], [20, 30, 40])
 
-      def test_custom_action_parse(self):
-          result = AP.parseString("blah(20, 30, 40)")
-          self.assertEqual(len(result), 1)
-          self.assertIsInstance(result[0], actions.Action)
-          self.assertEqual(result[0]._op._op_str, "blah")
-          self.assertEqual([x[-1]._value for x in result[0]._values], [20, 30, 40])
+    def test_custom_action_parse(self):
+        result = AP.parseString("blah(20, 30, 40)")
+        self.assertEqual(len(result), 1)
+        self.assertIsInstance(result[0], actions.Action)
+        self.assertEqual(result[0]._op._op_str, "blah")
+        self.assertEqual([x[-1]._value for x in result[0]._values], [20, 30, 40])
 
-      def test_string_value(self):
-            result = AP.parseString('+("blah bloo", "blee", "awef")')
-            self.assertEqual(len(result), 1)
-            self.assertIsInstance(result[0], actions.Action)
-            self.assertEqual(result[0]._op._op_str, "+")
-            self.assertEqual([x[-1]._value for x in result[0]._values], ["blah bloo","blee","awef"])
+    def test_string_value(self):
+        result = AP.parseString('+("blah bloo", "blee", "awef")')
+        self.assertEqual(len(result), 1)
+        self.assertIsInstance(result[0], actions.Action)
+        self.assertEqual(result[0]._op._op_str, "+")
+        self.assertEqual([x[-1]._value for x in result[0]._values], ["blah bloo","blee","awef"])
 
-      def test_actions_parse(self):
-            result = AP.parseString('+(2), -(3), @(4)')
-            self.assertEqual(len(result), 3)
-            self.assertTrue(all([isinstance(x, actions.Action) for x in result]))
-            for action, op in zip(result, ["+", "-", "@"]):
-                  self.assertEqual(action._op._op_str, op)
+    def test_actions_parse(self):
+        result = AP.parseString('+(2), -(3), @(4)')
+        self.assertEqual(len(result), 3)
+        self.assertTrue(all([isinstance(x, actions.Action) for x in result]))
+        for action, op in zip(result, ["+", "-", "@"]):
+            self.assertEqual(action._op._op_str, op)
 
-      def test_actions_fact_str(self):
-            result = AP.parseString('+(a.b.c), -(a!b.d), +($x), +($x.a.b)')
-            self.assertEqual(len(result), 4)
-            self.assertTrue(all([isinstance(x, actions.Action) for x in result]))
+    def test_actions_fact_str(self):
+        result = AP.parseString('+(a.b.c), -(a!b.d), +($x), +($x.a.b)')
+        self.assertEqual(len(result), 4)
+        self.assertTrue(all([isinstance(x, actions.Action) for x in result]))
 
-      def test_action_str_equal(self):
-            actions = ["+(2)", "-(3)", "@(4)"]
-            parsed = [AP.parseString(x)[0] for x in actions]
-            zipped = zip(actions, parsed)
-            self.assertTrue(all([x == str(y) for x,y in zipped]))
+    def test_action_str_equal(self):
+        actions = ["+(2)", "-(3)", "@(4)"]
+        parsed = [AP.parseString(x)[0] for x in actions]
+        zipped = zip(actions, parsed)
+        self.assertTrue(all([x == str(y) for x,y in zipped]))
 
-      def test_action_binding_expansion(self):
-            bindings = {"x" : FP.parseString('a.b.c')[0] }
-            action = AP.parseString("+($x)")[0]
-            newAction = action.expand_bindings(bindings)
-            self.assertEqual(str(newAction), "+(a.b.c)")
+    def test_action_binding_expansion(self):
+        bindings = {"x" : FP.parseString('a.b.c')[0] }
+        action = AP.parseString("+($x)")[0]
+        newAction = action.expand_bindings(bindings)
+        self.assertEqual(str(newAction), "+(a.b.c)")
 
-
+    def test_action_macro_definition_empty(self):
+        test_str = "#test():\n +(a.b.c)\nend"
+        definition = AP.action_definition.parseString(test_str)[0]
+        self.assertEqual(definition._name, "test")
 
 if __name__ == "__main__":
       LOGLEVEL = logging.INFO
