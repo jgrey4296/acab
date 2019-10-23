cc22ea5d87e780c2b2cb41ce6c8b4817c649c383
jg <johngrey@Johns-Mac-mini.local>
2019-10-23 10:52:56 +0100

    updates engine logic tests to new EL format

--- tests/engine/engine_logic_tests.py
+++ tests/engine/engine_logic_tests.py
@@ -22,152 +22,152 @@ class Engine_Logic_Tests(unittest.TestCase):
     #----------
     #use testcase snippets
     def test_simple_logic(self):
-        self.e.registerRules(""".a.test.rule:
-        	.a.b.c?
+        self.e.registerRules("""a.test.rule:
+        	a.b.c?
 
-        	+(.a.b.d)
+        	+(a.b.d)
         end""")
-        self.e.add(".a.b.c")
-        self.assertTrue(self.e.query("~.a.b.d?"))
+        self.e.add("a.b.c")
+        self.assertTrue(self.e.query("~a.b.d?"))
         self.e._run_rules()
-        self.assertTrue(self.e.query(".a.b.d?"))
+        self.assertTrue(self.e.query("a.b.d?"))
 
     def test_simple_file_load(self):
         self.e.load_file(self.path("exampleRule1.trie"))
-        self.assertTrue(self.e.query(".a.b.c?, ~.d.e.f?"))
+        self.assertTrue(self.e.query("a.b.c?, ~d.e.f?"))
         self.assertEqual(len(self.e._rules), 1)
         self.e._run_rules()
-        self.assertTrue(self.e.query(".a.b.c?, .d.e.f?"))
+        self.assertTrue(self.e.query("a.b.c?, d.e.f?"))
 
     def test_multi_rule_load_from_single_file(self):
         self.e.load_file(self.path("exampleRules2.trie"))
         self.assertEqual(len(self.e._rules), 2)
-        self.assertTrue(self.e.query(".a.b.c?, .a.other!ex?, ~.d.e.f?, ~.d.e.g?"))
+        self.assertTrue(self.e.query("a.b.c?, a.other!ex?, ~d.e.f?, ~d.e.g?"))
         self.e._run_rules()
-        self.assertTrue(self.e.query(".a.b.c?, .a.other!ex?, .d.e.f?, .d.e.g?"))
+        self.assertTrue(self.e.query("a.b.c?, a.other!ex?, d.e.f?, d.e.g?"))
 
     def test_file_load_with_comments(self):
         self.e.load_file(self.path( "exampleComments.trie"))
         self.assertEqual(len(self.e._rules), 1)
-        self.assertTrue(self.e.query("~.this.should.not.assert?"))
+        self.assertTrue(self.e.query("~this.should.not.assert?"))
 
     def test_file_load_retraction(self):
         self.e.load_file(self.path( "retractionTest.trie"))
         self.assertEqual(len(self.e._rules), 1)
-        self.assertTrue(self.e.query(".a.b.c?"))
+        self.assertTrue(self.e.query("a.b.c?"))
         self.e._run_rules()
-        self.assertTrue(self.e.query("~.a.b.c?"))
+        self.assertTrue(self.e.query("~a.b.c?"))
 
     def test_file_load_multi_clauses(self):
         self.e.load_file(self.path( "multiclause_rule.trie"))
         self.assertEqual(len(self.e._rules), 1)
-        self.assertTrue(self.e.query(".a.b.c?, .a.c!d?"))
+        self.assertTrue(self.e.query("a.b.c?, a.c!d?"))
         self.e._run_rules()
-        self.assertTrue(self.e.query(".a.b.c?, .a.c!d?, .a.b.e?"))
+        self.assertTrue(self.e.query("a.b.c?, a.c!d?, a.b.e?"))
 
     def test_file_load_string_query(self):
         self.e.load_file(self.path("string_query_test.trie"))
         self.assertEqual(len(self.e._rules), 2)
-        self.assertTrue(self.e.query("~.a.b.e?"))
+        self.assertTrue(self.e.query("~a.b.e?"))
         self.e._run_rules()
-        self.assertTrue(self.e.query(".a.b.e?, .a.b.f?"))
+        self.assertTrue(self.e.query("a.b.e?, a.b.f?"))
 
     def test_file_load_string_assert(self):
         self.e.load_file(self.path("string_assert_test.trie"))
-        self.assertTrue(self.e.query('~.a.b."an asserted string"?'))
+        self.assertTrue(self.e.query('~a.b."an asserted string"?'))
         self.e._run_rules()
-        self.assertTrue(self.e.query('.a.b."an asserted string"?'))
+        self.assertTrue(self.e.query('a.b."an asserted string"?'))
 
     def test_file_load_string_retract(self):
         self.e.load_file(self.path("string_retract_test.trie"))
-        self.assertTrue(self.e.query('.a.b."a test string"?'))
+        self.assertTrue(self.e.query('a.b."a test string"?'))
         self.e._run_rules()
-        self.assertTrue(self.e.query('~.a.b."a test string"?'))
+        self.assertTrue(self.e.query('~a.b."a test string"?'))
 
     def test_file_load_transform(self):
         self.e.load_file(self.path("transform_test.trie"))
         self.assertEqual(len(self.e._rules), 1)
-        self.assertTrue(self.e.query('.a.b!5?, .a.c!10?, .a.d!20?'))
+        self.assertTrue(self.e.query('a.b!5?, a.c!10?, a.d!20?'))
         self.e._run_rules()
-        self.assertTrue(self.e.query('.a.b!25?'))
-        self.assertTrue(self.e.query('.a.c!30?'))
-        self.assertTrue(self.e.query('.a.d!40?'))
+        self.assertTrue(self.e.query('a.b!25?'))
+        self.assertTrue(self.e.query('a.c!30?'))
+        self.assertTrue(self.e.query('a.d!40?'))
 
     def test_transform_selection_single(self):
         self.e.load_file(self.path("transform_selection_single.trie"))
-        self.assertTrue('.a.b!5?, .a.c!10?, .a.d!20?')
+        self.assertTrue('a.b!5?, a.c!10?, a.d!20?')
         self.e._run_rules()
-        queried = [True for x in [".a.b!25?",".a.c!30?",".a.d!40?"] if bool(self.e.query(x))]
+        queried = [True for x in ["a.b!25?","a.c!30?","a.d!40?"] if bool(self.e.query(x))]
         self.assertEqual(len(queried), 1)
 
 
     def test_file_load_multi_transform(self):
         self.e.load_file(self.path("multi_transform_test.trie"))
         self.assertEqual(len(self.e._rules), 1)
-        self.assertTrue(self.e.query('.a.b!5?, .a.c!10?, .a.d!20?'))
+        self.assertTrue(self.e.query('a.b!5?, a.c!10?, a.d!20?'))
         self.e._run_rules()
-        self.assertTrue(self.e.query('.a.b!50?'))
-        self.assertTrue(self.e.query('.a.c!60?'))
-        self.assertTrue(self.e.query('.a.d!80?'))
+        self.assertTrue(self.e.query('a.b!50?'))
+        self.assertTrue(self.e.query('a.c!60?'))
+        self.assertTrue(self.e.query('a.d!80?'))
 
     def test_file_exclusion_update(self):
         self.e.load_file(self.path("exclusion_update_test.trie"))
-        self.assertTrue(self.e.query('.a.b!20?, ~.a.b.!10?, ~.a.b!5?'))
+        self.assertTrue(self.e.query('a.b!20?, ~a.b.!10?, ~a.b!5?'))
 
     def test_macro_binding(self):
         self.e.load_file(self.path("macro_binding_test.trie"))
-        self.assertTrue(self.e.query('.a.b.c.d?, .a.b.c.e?'))
+        self.assertTrue(self.e.query('a.b.c.d?, a.b.c.e?'))
 
     def test_macro_binding_empty(self):
         self.e.load_file(self.path("macro_empty_binding_test.trie"))
-        self.assertTrue(self.e.query('~.a.b.c?'))
+        self.assertTrue(self.e.query('~a.b.c?'))
 
     def test_action_macro(self):
         self.e.load_file(self.path("action_macro.trie"))
-        self.assertTrue(self.e.query('.a.b.first?, .a.c.second?'))
+        self.assertTrue(self.e.query('a.b.first?, a.c.second?'))
         self.e._run_rules()
-        self.assertTrue(self.e.query('~.a.b.first?, ~.a.c.second?, .a.b.second?, .a.c.first?'))
+        self.assertTrue(self.e.query('~a.b.first?, ~a.c.second?, a.b.second?, a.c.first?'))
         rule = list(self.e._rules.values())[0]
         self.assertEqual(len(rule._actions), 4)
 
     def test_two_action_macros(self):
         self.e.load_file(self.path("two_action_macro.trie"))
-        self.assertTrue(self.e.query('.a.b.first?, .a.c.second?'))
+        self.assertTrue(self.e.query('a.b.first?, a.c.second?'))
         self.e._run_rules()
-        self.assertTrue(self.e.query('~.a.b.first?, ~.a.c.second?, .a.b.second?, .a.c.first?, .blah?, .bloo?'))
+        self.assertTrue(self.e.query('~a.b.first?, ~a.c.second?, a.b.second?, a.c.first?, .blah?, .bloo?'))
         rule = list(self.e._rules.values())[0]
         self.assertEqual(len(rule._actions), 6)
 
     def test_action_macros_with_values(self):
         self.e.load_file(self.path("valued_action_macro.trie"))
-        self.assertTrue(self.e.query('.a.b.c?'))
+        self.assertTrue(self.e.query('a.b.c?'))
         self.e._run_rules()
-        self.assertTrue(self.e.query('.a.b.c?, .a.b."this is a test"?'))
+        self.assertTrue(self.e.query('a.b.c?, a.b."this is a test"?'))
 
     def test_variable_macro_in_action_macro_test(self):
         self.e.load_file(self.path("variable_macro_in_action_macro_test.trie"))
-        self.assertTrue(self.e.query('.a.b.c?'))
+        self.assertTrue(self.e.query('a.b.c?'))
         self.e._run_rules()
-        self.assertTrue(self.e.query('.a.b.c?, .a.b.blah?'))
+        self.assertTrue(self.e.query('a.b.c?, a.b.blah?'))
 
     def test_rule_selection_running(self):
         self.e.load_file(self.path("selection_of_rules_test.trie"))
-        self.assertTrue(self.e.query('.a.b.c?'))
+        self.assertTrue(self.e.query('a.b.c?'))
         self.e._run_rules(rule_tags=['second'])
-        self.assertTrue(self.e.query('.a.b.c?, ~.blah?, .bloo?'))
+        self.assertTrue(self.e.query('a.b.c?, ~blah?, bloo?'))
         self.e._run_rules(rule_tags=['first'])
-        self.assertTrue(self.e.query('.a.b.c?, .blah?, .bloo?'))
+        self.assertTrue(self.e.query('a.b.c?, blah?, bloo?'))
 
     def rule_load_with_comments(self):
         self.e.load_file(self.path("rule_with_comments.trie"))
         self.assertEqual(len(self.e._rules), 1)
-        rule = self.e._rules['.a.rule']
+        rule = self.e._rules['a.rule']
         self.assertEqual(len(rule._tags), 2)
         self.assertEqual(len(rule._query), 2)
         self.assertEqual(len(rule._transform), 2)
         self.assertEqual(len(rule._actions), 2)
         self.e._run_rules()
-        self.assertTrue(self.e.query('.a.d.c?, .a.d.d?, .a.b!30?'))
+        self.assertTrue(self.e.query('a.d.c?, a.d.d?, a.b!30?'))
 
 
 if __name__ == "__main__":
