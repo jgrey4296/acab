3176126bdb9af1d313065f88ead20a153db42f90
jg <johngrey@Johns-Mac-mini.local>
2019-10-23 10:54:09 +0100

    updates engine tests to new EL format

--- tests/engine/engine_tests.py
+++ tests/engine/engine_tests.py
@@ -24,35 +24,35 @@ class Engine_Tests(unittest.TestCase):
 
     def test_assert(self):
         self.assertEqual(len(self.e._knowledge_base._root), 0)
-        self.e.add('.a.b.c')
+        self.e.add('a.b.c')
         self.assertEqual(len(self.e._knowledge_base._root), 1)
 
     def test_retract(self):
-        self.e.add('.a.b.c')
+        self.e.add('a.b.c')
         self.assertEqual(len(self.e._knowledge_base._root), 1)
-        self.e.retract('.a')
+        self.e.retract('a')
         self.assertEqual(len(self.e._knowledge_base._root), 0)
 
     def test_query(self):
-        self.e.add('.a.b.c')
-        result = self.e.query('.a.b.c?')
+        self.e.add('a.b.c')
+        result = self.e.query('a.b.c?')
         self.assertTrue(bool(result))
 
     def test_query_fail(self):
-        self.assertFalse(self.e.query('.a.b.c?'))
+        self.assertFalse(self.e.query('a.b.c?'))
 
     def test_query_fail_with_asserted_facts(self):
-        self.e.add('.a.b.c, .a.b.d')
-        result = self.e.query('.a.b.c?, .a.b.e?')
-        self.assertFalse(self.e.query('.a.b.c?, .a.b.e?'))
+        self.e.add('a.b.c, a.b.d')
+        result = self.e.query('a.b.c?, a.b.e?')
+        self.assertFalse(self.e.query('a.b.c?, a.b.e?'))
 
     def test_query_with_binds(self):
-        self.e.add('.a.b.c, .a.b.d, .a.d.c')
-        self.assertTrue(self.e.query('.a.b.$x?, .a.d.$x?'))
+        self.e.add('a.b.c, a.b.d, a.d.c')
+        self.assertTrue(self.e.query('a.b.$x?, a.d.$x?'))
 
     def test_query_with_binds_fail(self):
-        self.e.add('.a.b.c, .a.b.d, .a.d.e')
-        self.assertFalse(self.e.query('.a.b.$x?, .a.d.$x?'))
+        self.e.add('a.b.c, a.b.d, a.d.e')
+        self.assertFalse(self.e.query('a.b.$x?, a.d.$x?'))
 
     def test_multi_assert(self):
         self.e.add('.a.b.c, .a.b.d, .a.b.e')
@@ -66,22 +66,22 @@ class Engine_Tests(unittest.TestCase):
         self.assertEqual(len(self.e._knowledge_base._root._reconstruct()), 1)
 
     def test_multi_clause_query(self):
-        self.e.add('.a.b.c, .a.b.d, .a.b.e')
-        result = self.e.query('.a.b.c?, .a.b.d?, .a.b.e?')
+        self.e.add('a.b.c, a.b.d, a.b.e')
+        result = self.e.query('a.b.c?, a.b.d?, a.b.e?')
         self.assertTrue(result)
 
     def test_rule_registration(self):
         self.assertEqual(len(self.e._rules), 0)
-        self.e.registerRules(".a.test.rule:\nend")
+        self.e.registerRules("a.test.rule:\nend")
         self.assertEqual(len(self.e._rules), 1)
-        self.e.registerRules(".a.second.rule:\nend")
+        self.e.registerRules("a.second.rule:\nend")
         self.assertEqual(len(self.e._rules), 2)
 
     def test_rule_registration_overwrite(self):
         self.assertEqual(len(self.e._rules), 0)
-        self.e.registerRules(".a.test.rule:\nend")
+        self.e.registerRules("a.test.rule:\nend")
         self.assertEqual(len(self.e._rules), 1)
-        self.e.registerRules(".a.test.rule:\nend")
+        self.e.registerRules("a.test.rule:\nend")
         self.assertEqual(len(self.e._rules), 1)
 
 
@@ -92,7 +92,7 @@ class Engine_Tests(unittest.TestCase):
         self.assertTrue(True)
 
     def test_run_transform(self):
-        stub_ctx = pyRule.Contexts.initial(None)
+        stub_ctx = Contexts.initial(None)
         stub_ctx[0]['a'] = 2
         stub_ctx[0]['b'] = 4
 
@@ -104,7 +104,7 @@ class Engine_Tests(unittest.TestCase):
         self.assertEqual(result['b'], 8)
 
     def test_run_transform_rebind(self):
-        stub_ctx = pyRule.Contexts.initial(None)
+        stub_ctx = Contexts.initial(None)
         stub_ctx[0]['a'] = 2
         stub_ctx[0]['b'] = 8
 
@@ -117,7 +117,7 @@ class Engine_Tests(unittest.TestCase):
         self.assertEqual(result['w'], 16)
 
     def test_run_unary_transform(self):
-        stub_ctx = pyRule.Contexts.initial(None)
+        stub_ctx = Contexts.initial(None)
         stub_ctx[0]['a'] = 2
         stub_ctx[0]['b'] = -2
         stub_ctx[0]['c'] = 2.53
@@ -130,7 +130,7 @@ class Engine_Tests(unittest.TestCase):
         self.assertEqual(result['c'], 2)
 
     def test_run_unary_transform_rebind(self):
-        stub_ctx = pyRule.Contexts.initial(None)
+        stub_ctx = Contexts.initial(None)
         stub_ctx[0]['a'] = 2
         stub_ctx[0]['b'] = -2
         stub_ctx[0]['c'] = 2.53
@@ -146,7 +146,7 @@ class Engine_Tests(unittest.TestCase):
         self.assertEqual(result['z'], 2)
 
     def test_run_binary_transform(self):
-        stub_ctx = pyRule.Contexts.initial(None)
+        stub_ctx = Contexts.initial(None)
         stub_ctx[0]['a'] = 2
         stub_ctx[0]['b'] = -2
         stub_ctx[0]['c'] = 2.53
@@ -159,7 +159,7 @@ class Engine_Tests(unittest.TestCase):
         self.assertEqual(result['c'], 24.53)
 
     def test_run_binary_transform_rebind(self):
-        stub_ctx = pyRule.Contexts.initial(None)
+        stub_ctx = Contexts.initial(None)
         stub_ctx[0]['a'] = 2
         stub_ctx[0]['b'] = -2
         stub_ctx[0]['c'] = 2.53
@@ -175,7 +175,7 @@ class Engine_Tests(unittest.TestCase):
         self.assertTrue(isclose(result['z'], 4.53))
 
     def test_run_ternary_regex_transform(self):
-        stub_ctx = pyRule.Contexts.initial(None)
+        stub_ctx = Contexts.initial(None)
         stub_ctx[0]['a'] = "blah"
         stub_ctx[0]['b'] = "aaablah"
         stub_ctx[0]['c'] = "awefblahawef"
@@ -189,7 +189,7 @@ class Engine_Tests(unittest.TestCase):
         self.assertEqual(result['c'], 'AAAA')
 
     def test_run_ternary_regex_rebind(self):
-        stub_ctx = pyRule.Contexts.initial(None)
+        stub_ctx = Contexts.initial(None)
         stub_ctx[0]['a'] = "blah"
         stub_ctx[0]['b'] = "aaablah"
         stub_ctx[0]['c'] = "awefblahawef"
@@ -206,7 +206,7 @@ class Engine_Tests(unittest.TestCase):
         self.assertEqual(result['z'], 'AAAA')
 
     def test_run_unary_format(self):
-        stub_ctx = pyRule.Contexts.initial(None)
+        stub_ctx = Contexts.initial(None)
         stub_ctx[0]['a'] = "AAA"
         stub_ctx[0]['b'] = "BBB"
         stub_ctx[0]['c'] = "CCC"
@@ -222,7 +222,7 @@ class Engine_Tests(unittest.TestCase):
         self.assertEqual(result['z'], 'CCC BBB AAA')
 
     def test_run_unary_format_rebind(self):
-        stub_ctx = pyRule.Contexts.initial(None)
+        stub_ctx = Contexts.initial(None)
         stub_ctx[0]['a'] = "AAA"
         stub_ctx[0]['b'] = "BBB"
         stub_ctx[0]['c'] = "CCC"
@@ -239,67 +239,67 @@ class Engine_Tests(unittest.TestCase):
 
 
     def test_run_assert_action(self):
-        actions = AP.parseString("+(.a.b.c)")
-        self.assertFalse(self.e.query(".a.b.c?"))
+        actions = AP.parseString("+(a.b.c)")
+        self.assertFalse(self.e.query("a.b.c?"))
         self.e._run_actions({},actions)
-        self.assertTrue(self.e.query(".a.b.c?"))
+        self.assertTrue(self.e.query("a.b.c?"))
 
     def test_run_retract_action(self):
-        actions = AP.parseString("-(.a.b.c)")
-        self.e.add(".a.b.c")
-        self.assertTrue(self.e.query(".a.b.c?"))
+        actions = AP.parseString("-(a.b.c)")
+        self.e.add("a.b.c")
+        self.assertTrue(self.e.query("a.b.c?"))
         self.e._run_actions({}, actions)
-        self.assertFalse(self.e.query(".a.b.c?"))
-        self.assertTrue(self.e.query("~.a.b.c?"))
+        self.assertFalse(self.e.query("a.b.c?"))
+        self.assertTrue(self.e.query("~a.b.c?"))
 
     def test_run_assert_multi_action(self):
-        actions = AP.parseString("+(.a.b.c), +(.a.b.d)")
-        self.assertFalse(self.e.query(".a.b.c?, .a.b.d?"))
-        self.assertTrue(self.e.query("~.a.b.c?, ~.a.b.d?"))
+        actions = AP.parseString("+(a.b.c), +(a.b.d)")
+        self.assertFalse(self.e.query("a.b.c?, a.b.d?"))
+        self.assertTrue(self.e.query("~a.b.c?, ~a.b.d?"))
         self.e._run_actions({}, actions)
-        self.assertTrue(self.e.query(".a.b.c?, .a.b.d?"))
+        self.assertTrue(self.e.query("a.b.c?, a.b.d?"))
 
     def test_run_mixed_multi_action(self):
-        actions = AP.parseString("+(.a.b.c), -(.a.b.d)")
-        self.e.add(".a.b.d")
-        self.assertTrue(self.e.query("~.a.b.c?, .a.b.d?"))
+        actions = AP.parseString("+(a.b.c), -(a.b.d)")
+        self.e.add("a.b.d")
+        self.assertTrue(self.e.query("~a.b.c?, a.b.d?"))
         self.e._run_actions({}, actions)
-        self.assertTrue(self.e.query(".a.b.c?, ~.a.b.d?"))
+        self.assertTrue(self.e.query("a.b.c?, ~a.b.d?"))
 
     def test_run_bound_assert_action(self):
         data = {"x": "blah"}
-        actions = AP.parseString("+(.a.b.$x)")
-        self.assertTrue(self.e.query("~.a.b.blah?"))
+        actions = AP.parseString("+(a.b.$x)")
+        self.assertTrue(self.e.query("~a.b.blah?"))
         self.e._run_actions(data, actions)
-        self.assertTrue(self.e.query(".a.b.blah?"))
+        self.assertTrue(self.e.query("a.b.blah?"))
 
     def test_run_bound_retract_action(self):
         data = {"blah" : "bloo"}
-        actions = AP.parseString("-(.a.$blah.c)")
-        self.e.add(".a.bloo.c")
-        self.assertTrue(self.e.query(".a.bloo.c?"))
+        actions = AP.parseString("-(a.$blah.c)")
+        self.e.add("a.bloo.c")
+        self.assertTrue(self.e.query("a.bloo.c?"))
         self.e._run_actions(data, actions)
-        self.assertTrue(self.e.query("~.a.bloo.c?, .a.bloo?"))
+        self.assertTrue(self.e.query("~a.bloo.c?, a.bloo?"))
 
     def test_run_mixed_bound_actions(self):
         data = {"blah": "bloo"}
-        actions = AP.parseString("+(.a.$blah), -(.b.$blah)")
-        self.e.add(".b.bloo")
-        self.assertTrue(self.e.query(".b.bloo?"))
+        actions = AP.parseString("+(a.$blah), -(b.$blah)")
+        self.e.add("b.bloo")
+        self.assertTrue(self.e.query("b.bloo?"))
         self.e._run_actions(data, actions)
-        self.assertTrue(self.e.query(".a.bloo?, ~.b.bloo?"))
+        self.assertTrue(self.e.query("a.bloo?, ~b.bloo?"))
 
     def test_register_and_run_entire_rule(self):
-        self.e.registerRules(".test.rule:\n.a.$x?\n\n$x + 20 -> $y\n\n+(.b.$y)\nend")
-        self.e.add(".a.20")
-        self.assertTrue(self.e.query(".a.20?, ~.b.40?"))
+        self.e.registerRules("test.rule:\na.$x?\n\n$x + 20 -> $y\n\n+(b.$y)\nend")
+        self.e.add("a.20")
+        self.assertTrue(self.e.query("a.20?, ~b.40?"))
         self.e._run_rules()
-        self.assertTrue(self.e.query(".a.20?, .b.40?"))
+        self.assertTrue(self.e.query("a.20?, b.40?"))
 
     def test_register_rule_added_to_trie(self):
-        self.assertTrue(self.e.query('~.test.rule?'))
-        self.e.registerRules('.test.rule:\n.a.b.c?\n\n+(.a.b.d)\nend')
-        self.assertTrue(self.e.query('.test.rule?'))
+        self.assertTrue(self.e.query('~test.rule?'))
+        self.e.registerRules('test.rule:\na.b.c?\n\n+(a.b.d)\nend')
+        self.assertTrue(self.e.query('test.rule?'))
 
     # def test_rule_query(self):
     #     self.e.add('.a.b.c')
