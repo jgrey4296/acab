7c39290d66fea7534b12287c239b05cdbbd52bd4
jg <johngrey@Johns-Mac-mini.local>
2019-10-24 13:25:36 +0100

    removed obsolete selection tests

--- tests/fact_trie/transform_parser_tests.py
+++ tests/fact_trie/transform_parser_tests.py
@@ -101,37 +101,15 @@ class Trie_Transform_Parser_Tests(unittest.TestCase):
             result = TP.parseString('_$x')
             self.assertIsInstance(result._components[0]._op, transforms.RoundOp)
 
-
-      def test_selection_transform_all(self):
-            result = TP.select.parseString('select _ - _')[0]
-            self.assertIsInstance(result, transforms.SelectionTransform)
-            self.assertEqual(result._lBound._value, "_")
-            self.assertEqual(result._uBound._value, "_")
-
-      def test_selection_transform_bounded(self):
-            result = TP.select.parseString('select 0 - 2')[0]
-            self.assertIsInstance(result, transforms.SelectionTransform)
-            self.assertEqual(result._lBound._value, 0)
-            self.assertEqual(result._uBound._value, 2)
-
-      def test_selection_transform_variable(self):
-            result = TP.select.parseString('select $x - $y')[0]
-            self.assertIsInstance(result, transforms.SelectionTransform)
-            self.assertEqual(result._lBound._value, 'x')
-            self.assertEqual(result._uBound._value, 'y')
-
       def test_fact_str_equal(self):
-            transforms = ["$x + 20", "$x + 20, $y + 5",
+            transforms = ["$x + 20", "$x + 20\n$y + 5",
                           "$xc - 10", "$x * 100",
                           "$x + 20 -> $y",
                           "$Blah + $bloo -> $BLEE",
                           "-$x", "_$x", "-$x -> $y",
                           "_$x -> $y", "$x ~= /blah/$a",
                           "$x ~= /aw+/$b -> $blah",
-                          "$x + 2d5",
-                          "select 0 - 2",
-                          "select _ - _",
-                          "select $x - $y"
+                          "$x + 2d5"
             ]
             parsed = [TP.parseString(x) for x in transforms]
             zipped = zip(transforms, parsed)
