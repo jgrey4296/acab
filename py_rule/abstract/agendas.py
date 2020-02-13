7b4864eefab6a75c150fc7ca3850384e55003e44
John Grey <johngrey4296@gmail.com>
2020-02-13 21:17:04 +0000

    create stub agenda and pipeline classes

---
+++ py_rule/abstract/agendas.py
@@ -0,0 +1,14 @@
+
+class Agenda:
+    """ Abstract Class of Rule Layer Agendas
+    Takes a set of potential rule activations
+    and applys a transform, filter, or other function on them
+    """
+
+    def __init__(self):
+        return
+
+    # TODO: implement base interface
+    # call
+    # inputs constraints,
+    # output constraints
---
+++ py_rule/abstract/pipeline.py
@@ -0,0 +1,15 @@
+
+class Pipeline:
+    """ Abstract Class to describe a rule engine pipeline
+    Collects together sets of rules,
+    and a sequence of agendas to run on their output
+    """
+
+    def __init__(self):
+        return
+
+
+    # TODO: define interface
+    # call
+    # input constraints
+    # output constraints
