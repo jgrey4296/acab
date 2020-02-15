6d4b1822279178b964675fe3fd928f22c84dd623
John Grey <johngrey4296@gmail.com>
2020-02-15 02:33:14 +0000

    add base error submodule

---
+++ py_rule/error/pyrule_base_exception.py
@@ -0,0 +1,7 @@
+class PyRuleBaseException:
+    """ The base exception class for the PyRule package """
+
+    def __init__(self):
+        return
+
+
---
+++ py_rule/error/pyrule_parse_exception.py
@@ -0,0 +1,9 @@
+from .pyrule_base_exception import PyRuleBaseException
+
+class PyRuleParseException(PyRuleBaseException):
+    """ The base exception for parsing errors """
+
+    def __init__(self):
+        return
+
+
