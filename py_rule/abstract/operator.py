a537261555331ffc86559745f7f86bcbf0e307e0
John Grey <johngrey4296@gmail.com>
2020-02-15 04:48:19 +0000

    add the Operator base class

---
+++ py_rule/abstract/operator.py
@@ -0,0 +1,12 @@
+"""
+The Base Operator Definition
+Used for Comparison, Transform, and Performance Operators
+"""
+class Operator:
+    """ The Base Operator Class """
+
+    def __init__(self):
+        return
+
+    def __call__(self):
+        return
