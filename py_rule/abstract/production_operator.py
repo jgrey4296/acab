3aa1a10a19bd369bcfb013b86b7204347780f55f
John Grey <johngrey4296@gmail.com>
2020-02-16 04:23:05 +0000

    add basic abstract operator class

---
+++ py_rule/abstract/production_operator.py
@@ -0,0 +1,19 @@
+"""
+The Base Operator Definition
+Used for Comparison, Transform, and Performance Operators
+"""
+class ProductionOperator:
+    """ The Base Operator Class """
+
+    def __init__(self):
+        self._op_str = None
+        return
+
+    def __call__(self):
+        return
+
+    def __str__(self):
+        raise Exception("Abstract str method")
+
+    def __repr__(self):
+        raise Exception("Abstract repr method")
