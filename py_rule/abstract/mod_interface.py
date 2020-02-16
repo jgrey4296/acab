c11e99208ac8c0796596f756c1c1cea3c0d1bc04
John Grey <johngrey4296@gmail.com>
2020-02-16 04:28:28 +0000

    flesh out module specification class

--- py_rule/abstract/mod_interface.py
+++ py_rule/abstract/mod_interface.py
@@ -3,16 +3,25 @@ The Module Interface definition that modules need to enact.
 Comes in two parts: The Parser, and the data
 
 """
+from .value import PyRuleValue
 
+class ModuleSpecification:
 
-class ModuleParser:
-
-    def __init__(self, parser):
+    def __init__(self, parser=None, types=None, funcs=None):
         """ A Parser has to provide a parser combinator to
         integrate into the Trie Lanuage.
         The Combinator *must* return a tuple:
         ("typestr", data) """
         self._parser = parser
+        # The value types the module adds
+        assert(all([x in PyRuleValue.__subclasses__()]))
+        self._types = []
+        if types is not None:
+            self._types += types
+        # The functions on the value types the module adds
+        self._functions = []
+        if funcs is not None:
+            self._functions += funcs
 
     def parse_string(self, string):
         """ Takes a String, parses it into Data format,
@@ -23,26 +32,6 @@ class ModuleParser:
     def get_parser(self):
         return self._parser
 
-
-class ModuleData:
-
-    def __init__(self):
-        return
-
-    def __str__(self):
-        """ Data needs to implement a str method that produces
-        output that can be re-parsed """
-        raise Exception("This is an Abstract Method")
-
-    def copy(self):
-        """ Data needs to be able to be copied """
-        raise Exception("This is an Abstract Method")
-
-    def bind(self, bindings):
-        """ Data needs to be able to bind a dictionary
-        of values to internal variables """
-        raise Exception("This is an Abstract Method")
-
-    def var_set(self):
-        """ Data needs to be able to report internal variables """
+    def construct_operators(self):
+        """ Use this to call operator constructors """
         raise Exception("This is an Abstract Method")
