750c128a55c15b15de68645923c4036e7cc18222
John Grey <johngrey4296@gmail.com>
2020-02-16 04:21:15 +0000

    add time module specification

---
+++ py_rule/modules/time/time_module.py
@@ -0,0 +1,19 @@
+from py_rule.abstract.mod_interface import ModuleSpecification
+from .parsing import parser as TP
+
+
+class TimeSpec(ModuleSpecification):
+    """ TimeParser Class, providing entry points
+    for an engine and knowledge base to parse Time Strings
+    """
+
+    def __init__(self):
+        super().__init__(parser=TP.main_pattern,
+                         types=[],
+                         funcs=[])
+
+    def parse_string(self, s):
+        return TP.parseString(s)
+
+    def construct_operators(self):
+        return
