0d3555330ce0a247f6bda4f39aac90859242844a
John Grey <johngrey4296@gmail.com>
2020-02-18 17:17:55 +0000

    add comments and initial structure for layer

--- py_rule/abstract/layer.py
+++ py_rule/abstract/layer.py
@@ -1,10 +1,33 @@
+"""
+Layers can be parsable
+and inserted or removed from pipelines?
 
+should also hold tests to ensure particular results from
+particular inputs, and statistical ranges
+
+a.layer(::Layer):
+    //require modules
+
+    //rule selectors as:
+    #tag_queries?
+    rule.location.queries?
+    rule.input.queries?
+    rule.output.queries?
+
+    + agenda(sort_by: a.query.$x?)
+    + agenda
+    + agenda
+end
+
+"""
 
 class Layer:
     """ The Abstract Layer Class """
 
     def __init__(self):
-        self._rule_selector = None
+        # rule selector as a list of queries?
+        self._rule_selectors = None
         self._agendas = []
-        return
+        self._tests = []
+
 
