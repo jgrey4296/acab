e6d1e688d82960af159a0b3459f2fd657bb81073
John Grey <johngrey4296@gmail.com>
2020-02-18 17:18:12 +0000

    add comments and initial structure for agenda

--- py_rule/abstract/agenda.py
+++ py_rule/abstract/agenda.py
@@ -1,3 +1,13 @@
+""""
+Agendas don't need to be parsable,
+they just need to be registered so
+layers and pipelines can be defined
+"""
+
+
+from enum import Enum
+
+RELATION_E = Enum('Agenda_Relation', 'ONE2ONE ONE2MANY MANY2ONE MANY2MANY')
 
 class Agenda:
     """ Abstract Class of Rule Layer Agendas
@@ -6,12 +16,16 @@ class Agenda:
     """
 
     def __init__(self):
+        # A function type signature
         self._type_signature = None
+        # Whether the agenda expands or constrains proposals
+        self._relation_type = None
+        # Whether proposals are indexed, and what by
+        self._is_indexed = False
+        self._index = None
 
-    # TODO: implement base interface
 
     def __call__(self, proposals, engine, **kwargs):
         """ Take the proposals, transform them in some way,
         then enact them on the engine """
         return
-
