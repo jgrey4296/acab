2f8cd77aaf4c96b961758ddc687be613a383f603
John Grey <johngrey4296@gmail.com>
2020-02-20 02:51:25 +0000

    update imports for testing, implement hotloading, bring typing tests to passing

--- tests/typing/typedef_parser_tests.py
+++ tests/typing/typedef_parser_tests.py
@@ -2,13 +2,24 @@ import unittest
 import logging
 import random
 from test_context import py_rule
-import py_rule.knowledge_bases.trie_kb.parsing.TypeDefParser as TD
+from py_rule.knowledge_bases.trie_kb.parsing import FactParser as FP
+import py_rule.typing.parsing.TypeDefParser as TD
+import py_rule.typing.parsing.TypeParser as TP
 from py_rule.abstract.trie.nodes.trie_node import TrieNode
 from py_rule.abstract.sentence import Sentence
 from py_rule.typing.type_definition import TypeDefinition
 import py_rule.util as util
+from py_rule.typing import util as TU
 
 class Trie_Fact_Parser_Tests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        FP.TYPE_ANNOTATION << TP.TYPEDEC_CORE
+        TP.BASIC_SEN << FP.BASIC_SEN
+        TD.VALBIND << FP.VALBIND
+        TD.BASIC_SEN << FP.BASIC_SEN
+        TD.PARAM_SEN << FP.PARAM_SEN
 
     def setUp(self):
         return 1
@@ -23,20 +34,20 @@ class Trie_Fact_Parser_Tests(unittest.TestCase):
         self.assertIsNotNone(TD.TYPEDEF)
 
     def test_basic_typedef(self):
-        result = TD.parseString('::blah:\n\na.b.c\n\nEND')[0]
+        result = TD.parseString('::blah:\n\na.b.c\n\nend')[0]
         self.assertIsInstance(result, TypeDefinition)
         self.assertEqual(len(result._vars), 0)
         self.assertEqual(result._name._value, "blah")
         self.assertEqual(len(result._structure), 1)
 
     def test_typedef_with_variable(self):
-        result = TD.parseString('::blah($x):\n\na.b.c\n\nEND')[0]
+        result = TD.parseString('::blah($x):\n\na.b.c\n\nend')[0]
         self.assertIsInstance(result, TypeDefinition)
         self.assertEqual(len(result._vars), 1)
         self.assertEqual(result._vars[0]._value, "x")
 
     def test_typedef_with_multi_variables(self):
-        result = TD.parseString('::blah($x, $y):\n\na.b.c\n\nEND')[0]
+        result = TD.parseString('::blah($x, $y):\n\na.b.c\n\nend')[0]
         self.assertIsInstance(result, TypeDefinition)
         self.assertEqual(len(result._vars), 2)
         var_set = set([x._value for x in result._vars])
@@ -44,12 +55,12 @@ class Trie_Fact_Parser_Tests(unittest.TestCase):
         self.assertEqual(var_set, match_set)
 
     def test_typedef_with_structure_types(self):
-        result = TD.parseString('::blah:\n\na.b.c(::bloo)\n\nEND')[0]
-        self.assertEqual(result._structure[0][-1]._data['typedec']._name._value, 'bloo')
+        result = TD.parseString('::blah:\n\na.b.c(::bloo)\n\nend')[0]
+        self.assertEqual(result._structure[0][-1]._data[TU.TYPE_DEC_S]._name._value, 'bloo')
 
     def test_typedef_with_bad_vars(self):
         with self.assertRaises(Exception):
-            result = TD.parseString('::blah(blee):\n\na.b.c\n\nEND')[0]
+            result = TD.parseString('::blah(blee):\n\na.b.c\n\nend')[0]
 
 
 if __name__ == "__main__":
