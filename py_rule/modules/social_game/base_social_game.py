e72d1a002fcf3366a858315edcf57854a36f1c5e
John Grey <johngrey4296@gmail.com>
2020-02-23 12:08:37 +0000

    add notes to empty social_game module

---
+++ py_rule/modules/social_game/base_social_game.py
@@ -0,0 +1,39 @@
+"""
+** Koster Games
+*** Base Game
+    Position
+    Demarcation
+    Decision making
+    Choice
+    Information
+    payoff
+    Scope
+
+*** Parallel (A)Symmetric
+    mediating status,
+    ordering, leaderboards
+*** Direct Opposition
+    Resource consumption
+    Tug of War
+    handicaps
+*** Multifront opposition
+    last man standing
+    Bidding
+*** Information games
+    deception, bluffing
+    3rd party/meta betting
+*** Games masters
+*** Roles and Teams
+    Ritual, gifts,
+    reciprocity, mentoring
+    identity, ostracism
+*** Networks
+    trust, guilds
+    exclusivity, contract
+    election, reputation,
+    influence
+    public goods, commons
+    services
+
+
+"""
