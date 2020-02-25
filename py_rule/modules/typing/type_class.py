50d86d59a3321e5686b7cd8aaf5c8a1727ed9c84
John Grey <johngrey4296@gmail.com>
2020-02-25 11:56:55 +0000

    add type class stub

--- py_rule/modules/typing/type_class.py
+++ py_rule/modules/typing/type_class.py
@@ -1,3 +1,21 @@
 """
 Defines the required functions a type needs to implement
 """
+from .pyrule_type import Type
+
+class TypeClass(Type):
+    """ Definition of a coherent collection of functions """
+
+    def __init__(self):
+        super(TypeClass, self).__init__()
+
+
+
+class TypeClassInstance(Type):
+    """ A Pairing of a type, with a type class it implements """
+
+    def __init__(self):
+        super(TypeClassInstance, self).__init__()
+
+
+
