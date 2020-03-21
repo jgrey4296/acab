478a1aad10b1021f98f51a11b78dbbebc3031161
John Grey <johngrey4296@gmail.com>
2020-03-21 22:20:56 +0000

    improve contexts class to enable multiple appending

--- py_rule/abstract/contexts.py
+++ py_rule/abstract/contexts.py
@@ -11,6 +11,13 @@ class Contexts:
         init._init_alt(start_node)
         return init
 
+    def _init_alt(self, start_node):
+        """ Setup the initial context of no bindings
+        """
+        self._matches = [({}, start_node)]
+
+
+    #--------------------
     def __init__(self):
         # A list of (data,lastNode) tokens
         self._matches = []
@@ -34,25 +41,16 @@ class Contexts:
     def __bool__(self):
         return len(self._matches) > 0
 
-    def _init_alt(self, start_node):
-        """ Setup the initial context of no bindings
-        """
-        self._matches = [({}, start_node)]
-
-    def append(self, data):
+    def append(self, *data):
         """ Add a number of matching possibilities into this set of contexts """
-        assert(len(data) == 2)
-        if data[0] is not None and data[1] is not None:
-            self._matches.append(data)
+        assert(all([isinstance(x, tuple) for x in data]))
+        for x,y in data:
+            assert(x is not None and y is not None)
+            self._matches.append((x,y))
 
     def fail(self):
         """ Remove all contexts, as none are suitable """
         self._matches = []
-
-    def verify_matches(self, targetWMEMatch):
-        """ Ensure all alternatives have bound a certain number
-            of wmes (ie: for each clause in query there needs to be a wme) """
-        self._matches = [x for x in self._matches if len(x[0]) == targetWMEMatch]
 
     def set_all_alts(self, target=None, binding=None):
         """ Duplicate the Contexts, with a specific node as the current leaf """
