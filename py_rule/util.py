3416f4e449755a68581214d7b18312ebfa97a96d
John Grey <johngrey4296@gmail.com>
2020-03-27 00:29:26 +0000

    remove obsolete utility functions

--- py_rule/util.py
+++ py_rule/util.py
@@ -53,17 +53,3 @@ ACTION_HEAD_S    = "α"
 STRUCTURE_S      = "σ"
 FUNC_S           = "λ"
 
-def build_rebind_dict(formal, usage):
-    """ Build a dictionary for action macro expansion,
-    to swap internal formal params for provided usage params """
-    assert(all([BIND_S in x._data for x in formal]))
-    fvals = [x._value for x in formal]
-    return dict(zip(fvals, usage))
-
-def has_equivalent_vars_pred(node):
-    """ A Predicate to use with Trie.get_nodes
-    Finds nodes with multiple vars as children that can be merged """
-    if node._op == EX_OP.ROOT:
-        return False
-    var_children = [x for x in node._children.values() if x._is_var]
-    return len(var_children) > 1
