86d4f50216490e8e38c41ce4848cb558c5770153
John Grey <johngrey4296@gmail.com>
2020-04-06 16:30:52 +0100

    add basic tests for pyrule value

--- py_rule/abstract/__tests/value_tests.py
+++ py_rule/abstract/__tests/value_tests.py
@@ -3,6 +3,8 @@ from os.path import splitext, split
 import unittest
 import logging
 from py_rule.abstract.value import PyRuleValue
+from py_rule.abstract.node import PyRuleNode
+from py_rule.abstract.sentence import Sentence
 
 class PyRuleValueTests(unittest.TestCase):
 
@@ -35,12 +37,48 @@ class PyRuleValueTests(unittest.TestCase):
         var_set = value.var_set()
         self.assertTrue(all([x in var_set['in'] for x in ["a","b","c"]]))
 
-    @unittest.skip("TODO")
     def test_apply_onto(self):
-        return
+        value = PyRuleValue("test")
+        value._tags.add('testval')
+        sen = Sentence([PyRuleNode(x) for x in range(5)])
+        self.assertEqual(sen[-1]._value, 4)
+        self.assertIsInstance(sen[-1]._value, int)
+        value.apply_onto(sen)
+        self.assertIsInstance(sen[-1]._value, PyRuleValue)
+
+    def test_apply_onto_with_tags(self):
+        value = PyRuleValue("test")
+        value._tags.add('testval')
+        sen = Sentence([PyRuleNode(x) for x in range(5)])
+        self.assertEqual(sen[-1]._value, 4)
+        self.assertIsInstance(sen[-1]._value, int)
+        self.assertFalse("testTag" in value._tags)
+        value.apply_onto(sen, tags=["testTag"])
+        self.assertIsInstance(sen[-1]._value, PyRuleValue)
+        self.assertTrue("testTag" in value._tags)
+
+    def test_has_tag(self):
+        value = PyRuleValue("test")
+        value._tags.update(["a"])
+        self.assertTrue(value.has_tag("a"))
+
+    def test_has_tag_fail(self):
+        value = PyRuleValue("test")
+        value._tags.update(["a"])
+        self.assertFalse(value.has_tag("q"))
+
+    def test_has_tag_multi(self):
+        value = PyRuleValue("test")
+        value._tags.update(["a", "b", "c"])
+        self.assertTrue(value.has_tag("a", "b", "c"))
+
+    def test_has_tag_multi_fail(self):
+        value = PyRuleValue("test")
+        value._tags.update(["a", "b", "c"])
+        self.assertFalse(value.has_tag("a", "b", "c", "q"))
 
     @unittest.skip('TODO')
-    def test_has_tag(self):
+    def test_verify(self):
         return
 
 
