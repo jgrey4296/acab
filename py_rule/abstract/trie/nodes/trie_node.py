5b4a8bad0cd1cee5ff65e4238e8c2955bf80885d
John Grey <johngrey4296@gmail.com>
2020-04-09 16:11:34 +0100

    update trie node init

--- py_rule/abstract/trie/nodes/trie_node.py
+++ py_rule/abstract/trie/nodes/trie_node.py
@@ -10,8 +10,8 @@ class TrieNode(PyRuleNode):
         return TrieNode(util.ROOT_S)
 
 
-    def __init__(self, value, data=None, type_str=None):
-        super().__init__(value, data, type_str=type_str)
+    def __init__(self, value, data=None, type_str=None, tags=None, name=None):
+        super().__init__(value, data, type_str=type_str, tags=tags, name=name)
 
     def copy(self):
         newnode = TrieNode(self._value, self._data)
