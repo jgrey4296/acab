ee2161a45ee0f6bcfc235e4185de2dadc06afa8d
John Grey <johngrey4296@gmail.com>
2020-04-10 21:36:00 +0100

    add agenda parse_fn to statment constructor call

--- py_rule/modules/structures/agenda/AgendaParser.py
+++ py_rule/modules/structures/agenda/AgendaParser.py
@@ -4,6 +4,14 @@ Agenda Parser
 Should define an Agenda Instance with its parameters,
 to then be used in a layer
 
+σ::DefaultAgenda:
+	select.$x(::Number)
+end
+
+σ::RankingAgenda:
+	rank.by.$x
+end
+
 eg:
 Σ::primary.agenda(::DefaultAgenda): select.20 end
 Σ::secondary.agenda(::RankingAgenda): rank.by:$x? end
@@ -13,18 +21,14 @@ import logging as root_logger
 import pyparsing as pp
 from py_rule.abstract.parsing import util as PU
 from py_rule.abstract.production_operator import ProductionContainer
-from py_rule.abstract.agenda import Agenda
+from py_rule.abstract.agenda import Agenda, make_agenda
 
 logging = root_logger.getLogger(__name__)
 
-def construct_agenda(toks):
-    #Get the agenda constructor
-
-    #construct
-
-    #add the body
-
-    return ("agenda", the_agenda)
+def construct_agenda_setup_dict(toks):
+    # construct initializer dictionary
+    the_dict = {}
+    return ("agenda_dict", the_dict)
 
 
 HOTLOAD_BASIC_SEN = pp.Forward()
@@ -33,7 +37,8 @@ agenda_body = None
 
 agenda_stmt = PU.STATEMENT_CONSTRUCTOR(PU.AGENDA_HEAD,
                                        HOTLOAD_BASIC_SEN,
-                                       agenda_body)
+                                       agenda_body,
+                                       parse_fn=make_agenda)
 
 parse_point = agenda_stmt
 
