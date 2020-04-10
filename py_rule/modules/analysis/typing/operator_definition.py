4ee323f758158d4697880b4d5715b01949ca2d63
John Grey <johngrey4296@gmail.com>
2020-04-10 21:37:07 +0100

    refine operator_definition slightly

--- py_rule/modules/analysis/typing/operator_definition.py
+++ py_rule/modules/analysis/typing/operator_definition.py
@@ -1,9 +1,11 @@
 from py_rule.util import FUNC_S
 from py_rule.abstract.printing import util as PrU
+from py_rule.util import VALUE_TYPE_S, NAME_S
 
 from .type_definition import TypeDefinition
 from .type_instance import TypeInstance
-from .util import OP_DEF_S
+from .util import OP_DEF_S, TYPE_DEF_S
+
 
 class OperatorDefinition(TypeDefinition):
     """ Defines the type signature of an operator"""
@@ -13,11 +15,15 @@ class OperatorDefinition(TypeDefinition):
         with the binding to a ProductionOperator that is
         syntax sugared, and its inline place"""
         # eg: operator.+.$x(::num).$y(::num).$z(::num).num_plus
-        super().__init__([structure], type_str=OP_DEF_S)
+        if not isinstance(structure, list):
+            structure = [structure]
+        # TODO: unify vars over structure
+        super().__init__(structure, type_str=OP_DEF_S)
         self._func_name = sugar_syntax
 
     def pprint(self, **kwargs):
         return PrU.print_statement(self, is_structured=True, **kwargs)
 
     def build_type_declaration(self):
-        return TypeInstance(self._name, self._path, self._vars[:])
+        just_path = self.path.copy()
+        return TypeInstance(just_path, args=self.vars)
