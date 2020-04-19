17913290716d01f9f5f9d715a73ff85baa6aaa35
John Grey <johngrey4296@gmail.com>
2020-04-19 20:39:24 +0100

    add MODULE_SPEC to typing and time __init__.py's

--- py_rule/modules/analysis/typing/__init__.py
+++ py_rule/modules/analysis/typing/__init__.py
@@ -2,3 +2,6 @@
 The Typing Submodule Provides a TypeChecker,
 
 """
+from py_rule.modules.analysis.typing.typing_module import TypingSpec
+
+MODULE_SPEC = TypingSpec()
---
+++ py_rule/modules/structures/time/__init__.py
@@ -0,0 +1,3 @@
+from py_rule.modules.structures.time.time_module import TimeSpec
+
+MODULE_SPEC = TimeSpec()
