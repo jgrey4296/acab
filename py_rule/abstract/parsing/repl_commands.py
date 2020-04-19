364d2bf0c07e764cc8047dcfdf654225bd281036
John Grey <johngrey4296@gmail.com>
2020-04-19 03:19:18 +0100

    implement more repl_commands

--- py_rule/abstract/parsing/repl_commands.py
+++ py_rule/abstract/parsing/repl_commands.py
@@ -5,6 +5,11 @@ from enum import Enum
 from os.path import split, splitext, exists, expanduser, abspath
 import importlib
 import logging as root_logger
+
+from py_rule.abstract.agenda import Agenda
+from py_rule.agendas import *
+from py_rule.abstract.action import ActionOp
+
 logging = root_logger.getLogger(__name__)
 
 ReplE = Enum("Repl Commands", "INIT LOAD SAVE PRINT RUN PASS STEP ACT DECOMPOSE LISTEN CHECK STATS HELP EXIT MULTILINE MODULE PROMPT")
@@ -33,7 +38,8 @@ register(ReplE.INIT, engine_init)
 
 def engine_module(engine, params):
     logging.info("Loading Module: {}".format(params))
-    # TODO
+    temp_module = importlib.import_module(params[0])
+    engine.load_modules(temp_module.MODULE_SPEC)
     return engine, None
 
 register(ReplE.MODULE, engine_module)
@@ -50,7 +56,6 @@ register(ReplE.LOAD, engine_load)
 def engine_save(engine, params):
     logging.info("Saving State to: {}".format(params))
     filename = abspath(expanduser(params[0]))
-    # TODO
     assert(exists(filename))
     engine.save_file(filename)
     return engine, None
@@ -59,17 +64,24 @@ register(ReplE.SAVE, engine_save)
 
 def engine_print(engine, params):
     logging.info("Printing: {}".format(params))
+    result = []
     # TODO
-    # if kb
+    if "wm" in params:
+        print("WM: {}")
+    elif "module" in params:
+        print("Module: {}")
+    elif "layer" in params:
+        print("Layer: {}")
+    elif "pipeline" in params:
+        print("Pipeline: {}")
 
-    # if bindings
+    elif "bindings" in params:
+        print("Bindings: {}")
 
-    # modules/layers/pipeline
+    else:
+        print("Querying: {}")
 
-    # else sentence/query
-
-    result = None
-    return engine, result
+    return engine, "\n".join(result)
 
 register(ReplE.PRINT, engine_print)
 
@@ -88,10 +100,13 @@ register(ReplE.RUN, engine_run)
 
 def engine_pass(engine, params):
     logging.info("Passing sentences through: {}".format(params))
-    # TODO
     # Determine query / assertion
-    engine.add(*params)
-    return engine, None
+    # FIXME: Simple hack for now
+    if params[0].strip()[-1] == "?":
+        result = engine.query(*params)
+    else:
+        result = engine.add(*params)
+    return engine, result
 
 register(ReplE.PASS, engine_pass)
 
@@ -138,9 +153,9 @@ register(ReplE.LISTEN, engine_listen)
 def engine_type_check(engine, params):
     logging.info("Type Checking: {}".format(params))
     # TODO
-    # If single statement
+    # If single statement, run analysis layer with statement inserted, return types
 
-    # else everything
+    # else everything: run analysis layer
 
     return engine, None
 
@@ -149,25 +164,89 @@ register(ReplE.CHECK, engine_type_check)
 def engine_stats(engine, params):
     logging.info("Getting Stats: {}".format(params))
     # TODO
-    # KB stats
-
+    result = []
     # actions
+    result.append("Action Stats: ")
+    result.append("\t{}".format("\n\t".join([str(x) for x in ActionOp.op_list])))
+
+    # agendas
+    result.append("--------------------")
+    result.append("Agenda Stats: ")
+    result.append("\t{}".format("\n\t".join([str(x) for x in Agenda.agenda_list])))
 
     # rules
+    result.append("--------------------")
+    result.append("Rule Stats: ")
+
+
+    # pipeline
+    result.append("--------------------")
+    result.append("Pipeline Stats: ")
+    result.append(str(engine._pipeline))
 
     # layers
+    if engine._pipeline is not None:
+        result.append("--------------------")
+        result.append("Layer Stats: ")
+        result.append("\t{}".format("\n\t".join([str(x) for x in engine._pipeline._layers])))
 
     # modules
+    result.append("--------------------")
+    result.append("Module Stats: ")
+    result.append("\t{}".format("\n\t".join([x.__name__ for x in engine._loaded_modules])))
 
-    return engine, None
+    # KB stats
+    result.append("--------------------")
+    result.append("KB Stats: ")
+    result.append(str(engine._working_memory))
+
+    # Bindings
+    result.append("--------------------")
+    result.append("Binding Stats: ")
+    # TODO possibly filter out at binds
+    bind_groups = engine._cached_bindings[:]
+    result.append("\t{}".format("\n\t".join([str(x) for x in bind_groups])))
+
+
+    result.append("")
+    return engine, "\n".join(result)
 
 register(ReplE.STATS, engine_stats)
 
 def engine_help(engine, params):
     logging.info("Printing Help")
     # TODO
+    result = []
     # Print commands
-    return engine, None
+    result.append("-------------------- PyRule Help:")
+    result.append("---------- Args:")
+    result.append("--engine                          : Specify a load engine.")
+    result.append("                                    Defaults to py_rule.engines.trie_engine.TrieEngine")
+    result.append("-v {LEVEL} | --verbose {LEVEL}    : Specify Log level. File Level is one lower")
+
+    result.append("")
+    result.append("---------- Commands:")
+    result.append("\tany.valid.sentence     : Assert")
+    result.append("\tany.value.sentence?    : Query")
+
+    result.append("\tquit                   : Quit the interpreter")
+    result.append("\tstats                  : Print interpreter stats")
+
+    result.append("\tinit {a.class}         : Initialise a new engine")
+    result.append("\tmodule {a.module}      : Add a new module to the engine")
+
+    result.append("\tload {file}            : Load a file into the engine")
+    result.append("\tsave {file}            : Save a file to the engine")
+
+    result.append("\tprint                  : TODO print")
+    result.append("\trun                    : TODO run")
+    result.append("\tact                    : TODO act")
+    result.append("\tlisten et al           : TODO listen")
+    result.append("\tcheck                  : TODO check")
+
+    result.append("\thelp                   : Print this help")
+
+    return engine, "\n".join(result)
 
 register(ReplE.HELP, engine_help)
 
