794f9d7070407c80847df2761038cc6f853f30ae
John Grey <johngrey4296@gmail.com>
2020-04-29 01:55:15 +0100

    remove obsolete type_class file, add sum types and type class definition parser

--- py_rule/modules/analysis/typing/parsing/TypeDefParser.py
+++ py_rule/modules/analysis/typing/parsing/TypeDefParser.py
@@ -8,8 +8,6 @@ from . import util as TU
 logging = root_logger.getLogger(__name__)
 
 #Hotloaded definitions:
-## Values / bindings
-HOTLOAD_VALBIND = pp.Forward()
 ## Basic sentence (unable to parse annotations)
 HOTLOAD_BASIC_SEN = pp.Forward()
 ## Param sentence (able to parse annotations)
@@ -17,7 +15,7 @@ HOTLOAD_PARAM_SEN = pp.Forward()
 
 PARAM_SEN_PLURAL = pp.delimitedList(HOTLOAD_PARAM_SEN, delim=PU.DELIM)
 
-def make_type_def(toks):
+def make_record_def(toks):
     type_def = TypeDefinition(toks[:])
     return (type_def.type, type_def)
 
@@ -30,17 +28,31 @@ def make_op_def(toks):
 
     return (op_def.type, op_def)
 
+def make_sum_def(toks):
+    sum_def = TypeDefinition(toks[:])
+    return (type_def.type, type_def)
 
 
-# σ::a.test.type: a.value.$x(::num) end
-TYPEDEF_BODY = PARAM_SEN_PLURAL
-TYPEDEF_BODY.setParseAction(make_type_def)
+# Record Type definition:
+# a.test.type: (::σ)  a.value.$x(::num) end
+RECORD_DEF_BODY = PARAM_SEN_PLURAL
+RECORD_DEF_BODY.setParseAction(make_record_def)
 
-TYPEDEF = PU.STATEMENT_CONSTRUCTOR(PU.STRUCT_HEAD,
-                                   HOTLOAD_BASIC_SEN,
-                                   TYPEDEF_BODY + PU.emptyLine)
+RECORD_TYPE = PU.STATEMENT_CONSTRUCTOR(PU.STRUCT_HEAD,
+                                       HOTLOAD_BASIC_SEN,
+                                       RECORD_DEF_BODY + PU.emptyLine)
 
-# λ::numAdd: $x(::num).$y(::num).$z(::num) => +
+# Sum Type definition
+# ie: first subwords are the subtypes. subtypes are automatic records
+SUM_DEF_BODY = pp.delimitedList(RECORD_TYPE, delim=PU.emptyLine)
+SUM_DEF_BODY.setParseAction(make_sum_def)
+
+SUM_TYPE = PU.STATEMENT_CONSTRUCTOR(PU.SUM_HEAD,
+                                    HOTLOAD_BASIC_SEN,
+                                    SUM_DEF_BODY + PU.emptyLine)
+
+
+# numAdd: (::λ) $x(::num).$y(::num).$z(::num) => +
 OP_DEF_BODY = PU.NG(TYU.STRUCT_S, HOTLOAD_PARAM_SEN) \
     + PU.op(PU.DBLARROW + PU.N(TYU.SYNTAX_BIND_S, PU.OPERATOR_SUGAR))
 OP_DEF_BODY.setParseAction(make_op_def)
@@ -51,12 +63,19 @@ OP_DEF = PU.STATEMENT_CONSTRUCTOR(PU.FUNC_HEAD,
                                   end=pp.lineEnd,
                                   single_line=True)
 
+# Type class constructor:
+TYPE_CLASS_BODY = pp.delimitedList(OP_DEF, delim=PU.emptyLine)
 
-COMBINED_DEFS = pp.Or(TYPEDEF, OP_DEF)
+TYPE_CLASS_DEF = PU.STATEMENT_CONSTRUCTOR(PU.TYPE_CLASS_HEAD,
+                                          HOTLOAD_BASIC_SEN,
+                                          TYPE_CLASS_BODY + PU.emptyLine)
+
+
+COMBINED_DEFS = pp.Or([SUM_TYPE, RECORD_TYPE, OP_DEF])
 
 # NAMING
-TYPEDEF_BODY.setName("TypeDefinitionBody")
-TYPEDEF.setName("TypeDefinition")
+RECORD_DEF_BODY.setName("TypeDefinitionBody")
+RECORD_TYPE.setName("TypeDefinition")
 OP_DEF_BODY.setName("OperatorDefinitionBody")
 OP_DEF.setName("OperatorDefinition")
 
