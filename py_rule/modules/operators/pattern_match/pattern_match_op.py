1e393ce13d14adef334eab31c414287fcb016d51
John Grey <johngrey4296@gmail.com>
2020-05-02 08:38:53 +0100

    add stub for patternmatch operator

--- py_rule/modules/operators/pattern_match/pattern_match_op.py
+++ py_rule/modules/operators/pattern_match/pattern_match_op.py
@@ -2,3 +2,12 @@
 The actual pattern matcher logic as a transform operator
 
 """
+from py_rule.abstract.transform import TransformOp
+
+class PatternMatchOp(TransformOp):
+    def __init__(self):
+        super(PatternMatchOp, self).__init__(-1)
+
+    def __call__(self, node, patterns, data=None, engine=None):
+
+        return None
