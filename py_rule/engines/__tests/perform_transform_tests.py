6473e80d9f11acdfe687d374fc84308ffab35278
John Grey <johngrey4296@gmail.com>
2020-05-14 01:38:03 +0100

    update tests for engine

--- py_rule/engines/__tests/perform_transform_tests.py
+++ py_rule/engines/__tests/perform_transform_tests.py
@@ -13,7 +13,7 @@ from py_rule.working_memory.trie_wm.parsing import TransformParser as TP
 from py_rule.working_memory.trie_wm.parsing import FactParser as FP
 from py_rule.modules.values import numbers as NS
 from py_rule.modules.operators.standard_operators import StandardOperators
-
+from py_rule.abstract.value import PyRuleValue as PV
 
 class TransformTests(unittest.TestCase):
 
@@ -29,155 +29,155 @@ class TransformTests(unittest.TestCase):
     #use testcase snippets
     def test_run_transform(self):
         stub_ctx = Contexts()
-        stub_ctx.append(({'a': 2, 'b': 4}, 'blah'))
+        stub_ctx.append(({'a': PV(2), 'b': PV(4)}, 'blah'))
 
         stub_transform = TP.parseString('$a AddOp 20 -> $y, $b MulOp 2 -> $z')
 
         # result = self.e._run_transform(stub_ctx[0], stub_transform)
         result = stub_transform(stub_ctx[0], None)[0]
         self.assertIsInstance(result, dict)
-        self.assertEqual(result['a'], 2)
-        self.assertEqual(result['b'], 4)
-        self.assertEqual(result['y'], 22)
-        self.assertEqual(result['z'], 8)
+        self.assertEqual(result['a'].value, 2)
+        self.assertEqual(result['b'].value, 4)
+        self.assertEqual(result['y'].value, 22)
+        self.assertEqual(result['z'].value, 8)
 
     def test_run_transform_rebind(self):
         stub_ctx = Contexts()
-        stub_ctx.append(({'a': 2, 'b': 8}, "blah"))
+        stub_ctx.append(({'a': PV(2), 'b': PV(8)}, "blah"))
 
         stub_transform = TP.parseString('$a AddOp 20 -> $q, $b MulOp $a -> $w')
         result = stub_transform(stub_ctx[0], None)[0]
         self.assertIsInstance(result, dict)
-        self.assertEqual(result['a'], 2)
-        self.assertEqual(result['b'], 8)
-        self.assertEqual(result['q'], 22)
-        self.assertEqual(result['w'], 16)
+        self.assertEqual(result['a'].value, 2)
+        self.assertEqual(result['b'].value, 8)
+        self.assertEqual(result['q'].value, 22)
+        self.assertEqual(result['w'].value, 16)
 
     def test_run_unary_transform(self):
         stub_ctx = Contexts()
-        stub_ctx.append(({'a': 2, 'b':-2, 'c': 2.53}, 'blah'))
+        stub_ctx.append(({'a': PV(2), 'b': PV(-2), 'c': PV(2.53)}, 'blah'))
 
         stub_transform = TP.parseString('NegOp $a -> $x, NegOp $b -> $y, RoundOp $c -> $z')
         result = stub_transform(stub_ctx[0], None)[0]
         self.assertIsInstance(result, dict)
-        self.assertEqual(result['a'], 2)
-        self.assertEqual(result['b'], -2)
-        self.assertEqual(result['c'], 2.53)
-        self.assertEqual(result['x'], -2)
-        self.assertEqual(result['y'], 2)
-        self.assertEqual(result['z'], 2)
+        self.assertEqual(result['a'].value, 2)
+        self.assertEqual(result['b'].value, -2)
+        self.assertEqual(result['c'].value, 2.53)
+        self.assertEqual(result['x'].value, -2)
+        self.assertEqual(result['y'].value, 2)
+        self.assertEqual(result['z'].value, 2)
 
     def test_run_unary_transform_rebind(self):
         stub_ctx = Contexts()
-        stub_ctx.append(({'a': 2, 'b':-2, 'c': 2.53}, 'blah'))
+        stub_ctx.append(({'a': PV(2), 'b':PV(-2), 'c': PV(2.53)}, 'blah'))
 
         stub_transform = TP.parseString('NegOp $a -> $x, NegOp $b -> $y, RoundOp $c -> $z')
         result = stub_transform(stub_ctx[0], None)[0]
         self.assertIsInstance(result, dict)
-        self.assertEqual(result['a'], 2)
-        self.assertEqual(result['b'], -2)
-        self.assertEqual(result['c'], 2.53)
-        self.assertEqual(result['x'], -2)
-        self.assertEqual(result['y'], 2)
-        self.assertEqual(result['z'], 2)
+        self.assertEqual(result['a'].value, 2)
+        self.assertEqual(result['b'].value, -2)
+        self.assertEqual(result['c'].value, 2.53)
+        self.assertEqual(result['x'].value, -2)
+        self.assertEqual(result['y'].value, 2)
+        self.assertEqual(result['z'].value, 2)
 
     def test_run_binary_transform(self):
         stub_ctx = Contexts()
-        stub_ctx.append(({'a':2,'b':-2,'c':2.53},"blah"))
+        stub_ctx.append(({'a':PV(2),'b':PV(-2),'c':PV(2.53)},"blah"))
 
         stub_transform = TP.parseString('$a AddOp 20 -> $x, $b SubOp 20 -> $y, $c AddOp $x -> $z')
         result = stub_transform(stub_ctx[0], None)[0]
         self.assertIsInstance(result, dict)
-        self.assertEqual(result['a'], 2)
-        self.assertEqual(result['b'], -2)
-        self.assertEqual(result['c'], 2.53)
-        self.assertEqual(result['x'], 22)
-        self.assertEqual(result['y'], -22)
-        self.assertTrue(isclose(result['z'], 24.53))
+        self.assertEqual(result['a'].value, 2)
+        self.assertEqual(result['b'].value, -2)
+        self.assertEqual(result['c'].value, 2.53)
+        self.assertEqual(result['x'].value, 22)
+        self.assertEqual(result['y'].value, -22)
+        self.assertTrue(isclose(result['z'].value, 24.53))
 
     def test_run_binary_transform_rebind(self):
         stub_ctx = Contexts()
-        stub_ctx.append(({'a': 2, 'b':-2, 'c': 2.53}, 'blah'))
+        stub_ctx.append(({'a': PV(2), 'b':PV(-2), 'c': PV(2.53)}, 'blah'))
 
         stub_transform = TP.parseString('$a AddOp 20 -> $x, $b SubOp 20 -> $y, $c AddOp $a -> $z')
         result = stub_transform(stub_ctx[0], None)[0]
         self.assertIsInstance(result, dict)
-        self.assertEqual(result['a'], 2)
-        self.assertEqual(result['b'], -2)
-        self.assertEqual(result['c'], 2.53)
-        self.assertEqual(result['x'], 22)
-        self.assertEqual(result['y'], -22)
-        self.assertTrue(isclose(result['z'], 4.53))
+        self.assertEqual(result['a'].value, 2)
+        self.assertEqual(result['b'].value, -2)
+        self.assertEqual(result['c'].value, 2.53)
+        self.assertEqual(result['x'].value, 22)
+        self.assertEqual(result['y'].value, -22)
+        self.assertTrue(isclose(result['z'].value, 4.53))
 
     def test_run_ternary_regex_transform(self):
         stub_ctx = Contexts()
-        stub_ctx.append(({'a': "blah",
-                          'b': "aaablah",
-                          'c': "awefblahawef",
-                          'd': 'AAAA'}, "blah"))
+        stub_ctx.append(({'a': PV("blah"),
+                          'b': PV("aaablah"),
+                          'c': PV("awefblahawef"),
+                          'd': PV("AAAA")}, "blah"))
 
         stub_transform = TP.parseString('$a RegexOp /blah/ bloo -> $x, $b RegexOp /aaa\\w+/ $d -> $y, $c RegexOp /awef(\\w+)awef/ $d -> $z')
         result = stub_transform(stub_ctx[0], None)[0]
         self.assertIsInstance(result, dict)
-        self.assertEqual(result['a'], 'blah')
-        self.assertEqual(result['b'], 'aaablah')
-        self.assertEqual(result['c'], 'awefblahawef')
-        self.assertEqual(result['x'], 'bloo')
-        self.assertEqual(result['y'], 'AAAA')
-        self.assertEqual(result['z'], 'AAAA')
+        self.assertEqual(result['a'].value, 'blah')
+        self.assertEqual(result['b'].value, 'aaablah')
+        self.assertEqual(result['c'].value, 'awefblahawef')
+        self.assertEqual(result['x'].value, 'bloo')
+        self.assertEqual(result['y'].value, 'AAAA')
+        self.assertEqual(result['z'].value, 'AAAA')
 
     def test_run_ternary_regex_rebind(self):
         stub_ctx = Contexts()
         stub_ctx.append(({
-            'a': "blah",
-            'b':"aaablah",
-            'c':"awefblahawef",
-            'd':'AAAA'}, "blah"))
+            'a': PV("blah"),
+            'b': PV("aaablah"),
+            'c': PV("awefblahawef"),
+            'd':  PV("AAAA")}, "blah"))
 
         stub_transform = TP.parseString('$a RegexOp /blah/ bloo -> $x, $b RegexOp /aaa\\w+/ $d -> $y, $c RegexOp /awef(\\w+)awef/ $d -> $z')
         result = stub_transform(stub_ctx[0], None)[0]
         self.assertIsInstance(result, dict)
-        self.assertEqual(result['a'], 'blah')
-        self.assertEqual(result['b'], 'aaablah')
-        self.assertEqual(result['c'], 'awefblahawef')
-        self.assertEqual(result['x'], 'bloo')
-        self.assertEqual(result['y'], 'AAAA')
-        self.assertEqual(result['z'], 'AAAA')
+        self.assertEqual(result['a'].value, 'blah')
+        self.assertEqual(result['b'].value, 'aaablah')
+        self.assertEqual(result['c'].value, 'awefblahawef')
+        self.assertEqual(result['x'].value, 'bloo')
+        self.assertEqual(result['y'].value, 'AAAA')
+        self.assertEqual(result['z'].value, 'AAAA')
 
     def test_run_unary_format(self):
         stub_ctx = Contexts()
-        stub_ctx.append(({'a': "AAA",
-                          'b': "BBB",
-                          'c': "CCC",
-                          'x': "{a}",
-                          'y' : "{a} blah {b}",
-                          'z': "{c} {b} {a}"}, "blah"))
+        stub_ctx.append(({'a': PV("AAA"),
+                          'b': PV("BBB"),
+                          'c': PV("CCC"),
+                          'x': PV("{a}"),
+                          'y': PV("{a} blah {b}"),
+                          'z': PV("{c} {b} {a}")}, "blah"))
 
         stub_transform = TP.parseString('FormatOp $x -> $q, FormatOp $y -> $w, FormatOp $z -> $e')
         result = stub_transform(stub_ctx[0], None)[0]
         self.assertIsInstance(result, dict)
-        self.assertEqual(result['x'], '{a}')
-        self.assertEqual(result['y'], '{a} blah {b}')
-        self.assertEqual(result['z'], '{c} {b} {a}')
-        self.assertEqual(result['q'], 'AAA')
-        self.assertEqual(result['w'], 'AAA blah BBB')
-        self.assertEqual(result['e'], 'CCC BBB AAA')
+        self.assertEqual(result['x'].value, '{a}')
+        self.assertEqual(result['y'].value, '{a} blah {b}')
+        self.assertEqual(result['z'].value, '{c} {b} {a}')
+        self.assertEqual(result['q'].value, 'AAA')
+        self.assertEqual(result['w'].value, 'AAA blah BBB')
+        self.assertEqual(result['e'].value, 'CCC BBB AAA')
 
     def test_run_unary_format_rebind(self):
         stub_ctx = Contexts()
-        stub_ctx.append(({'a': "AAA",
-                          'b': "BBB",
-                          'c': "CCC",
-                          'x': "{a}",
-                          'y': "{a} blah {b}",
-                          'z': "{c} {b} {a}"}, "blah"))
+        stub_ctx.append(({'a': PV("AAA"),
+                          'b': PV("BBB"),
+                          'c': PV("CCC"),
+                          'x': PV("{a}"),
+                          'y': PV("{a} blah {b}"),
+                          'z': PV("{c} {b} {a}")}, "blah"))
 
         stub_transform = TP.parseString('FormatOp $x -> $xa, FormatOp $y -> $ya, FormatOp $z -> $za')
         result = stub_transform(stub_ctx[0], None)[0]
         self.assertIsInstance(result, dict)
-        self.assertEqual(result['xa'], 'AAA')
-        self.assertEqual(result['ya'], 'AAA blah BBB')
-        self.assertEqual(result['za'], 'CCC BBB AAA')
+        self.assertEqual(result['xa'].value, 'AAA')
+        self.assertEqual(result['ya'].value, 'AAA blah BBB')
+        self.assertEqual(result['za'].value, 'CCC BBB AAA')
 
 
 
