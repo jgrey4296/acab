83863e0dd659c1c89aed5733f013749f7ee3356f
John Grey <johngrey4296@gmail.com>
2020-06-25 01:15:57 +0100

    refactor type assignment node update method

--- acab/modules/analysis/typing/nodes/type_assignment_node.py
+++ acab/modules/analysis/typing/nodes/type_assignment_node.py
@@ -2,6 +2,7 @@ import logging as root_logger
 
 import acab.error.type_exceptions as te
 from acab.modules.analysis.typing import util
+from acab.modules.analysis.typing.util import TYPE_DEC_S
 from .typed_node import MonoTypedNode
 
 logging = root_logger.getLogger(__name__)
@@ -17,18 +18,20 @@ class TypeAssignmentTrieNode(MonoTypedNode):
         super().__init__(value, _type=_type)
         self._var_node = var_node
 
-    def update(self, node, lookup):
+    def update(self, word, lookup=None):
         """ Post-addition update method.
-        links self to a lookup-trie node if self is a variable """
+        links self to a lookup-trie word if self is a variable """
         logging.debug("Node: {} updating with {}".format(self._value,
-                                                         str(node)))
+                                                         str(word)))
         # apply type if necessary
-        self.type_match_wrapper(node)
-        if not (self.is_var == node.is_var):
+        if TYPE_DEC_S in word._data:
+            self.unify_types(word._data[TYPE_DEC_S])
+
+        if not (self.is_var == word.is_var):
             # complain if var status doesn't match
             raise te.TypeVariableConflictException(self)
 
-        if self.is_var and self._var_node is None:
+        if self.is_var and self._var_node is None and lookup is not None:
             # if var, connect to var type trie
             self._var_node = lookup.add([self._value], [])
             self._var_node.add_node(self)
