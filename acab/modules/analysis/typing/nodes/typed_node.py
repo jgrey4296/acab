4f9399d43499a02ad19f3c9e9fe556edf2e6b5c7
John Grey <johngrey4296@gmail.com>
2020-06-25 01:15:18 +0100

    refactor typed node type_match into new unify_types method

--- acab/modules/analysis/typing/nodes/typed_node.py
+++ acab/modules/analysis/typing/nodes/typed_node.py
@@ -1,5 +1,6 @@
 import logging as root_logger
 
+from acab.abstract.value import AcabValue
 from acab.abstract.node import AcabNode
 from acab.modules.analysis.typing.util import TYPE_DEC_S
 import acab.error.type_exceptions as te
@@ -15,7 +16,7 @@ class MonoTypedNode(AcabNode):
         assert(_type is None or isisntance(_type, TypeInstance))
         super().__init__(value)
         self._type_instance = _type
-
+        self._var_node = None
 
     @property
     def is_var(self):
@@ -25,18 +26,26 @@ class MonoTypedNode(AcabNode):
         return self._type_instance
 
 
-    def type_match_wrapper(self, node):
-        if TYPE_DEC_S not in node._data:
-            return None
-        return self.type_match(node._data[TYPE_DEC_S])
-
-    def type_match(self, _type):
-        assert(isinstance(_type, TypeInstance))
-        result = None
+    def apply_type_instance(self, _type):
         if self._type_instance is None:
+            assert(_type is None or isinstance(_type, TypeInstance)), breakpoint()
             self._type_instance = _type
-            result = self
-        elif self._type_instance != _type:
-            raise te.TypeConflictException(self._type_instance, _type, self.name)
 
-        return result
+    def unify_types(self, _type):
+        assert(_type is None or isinstance(_type, TypeInstance))
+
+        if self.type_instance == _type:
+            return None
+
+        # TODO: unify type instance and type vars separately?
+
+        # TODO: use <
+        if (self.type_instance is not None and _type is not None) and self._type_instance != _type:
+            raise te.TypeConflictException(self, _type, self.name)
+
+        self.apply_type_instance(_type)
+
+        if self._var_node is not None:
+            self._var_node.apply_type_instance(self.type_instance)
+
+        return self
