cbd87f4c30bd24e44dbc0d0d34ac84816267b02e
John Grey <johngrey4296@gmail.com>
2020-06-26 21:44:39 +0100

    add iniital engine mock tests

--- acab/abstract/__tests/engine_tests.py
+++ acab/abstract/__tests/engine_tests.py
@@ -1,8 +1,16 @@
 #https://docs.python.org/3/library/unittest.html
 from os.path import splitext, split
 import unittest
+import unittest.mock as mock
 import logging
 
+from acab.abstract.working_memory import WorkingMemory
+from acab.abstract.engine import Engine
+
+# https://docs.python.org/3/library/unittest.mock.html
+# mock.Mock / MagicMock
+# create_autospec
+# @patch(' ') / with patch.object(...)
 
 class EngineTests(unittest.TestCase):
 
@@ -18,17 +26,25 @@ class EngineTests(unittest.TestCase):
 
     #----------
     #use testcase snippets
-    @unittest.skip("TODO")
-    def test_init(self):
-        pass
+    @mock.patch('acab.abstract.engine.WorkingMemory', autospec=True)
+    def test_init(self, wm_mock):
+        engine = Engine(wm_mock)
+        wm_mock.assert_called_once_with(None)
 
-    @unittest.skip("TODO")
-    def test_load_modules(self):
-        pass
+    @mock.patch('acab.abstract.engine.ProductionOperator', autospec=True)
+    @mock.patch('acab.abstract.engine.WorkingMemory', autospec=True)
+    def test_load_modules(self, wm_mock, op_mock):
+        module_mock = mock.Mock()
+        engine = Engine(wm_mock, modules=[module_mock])
+        op_mock.clear_registrations.assert_called_once()
+        engine._working_memory.add_modules.assert_called_once()
 
-    @unittest.skip("TODO")
-    def test_reload_modules(self):
-        pass
+    @mock.patch('acab.abstract.engine.ProductionOperator', autospec=True)
+    @mock.patch('acab.abstract.engine.WorkingMemory', autospec=True)
+    def test_reload_modules(self, wm_mock, op_mock):
+        engine = Engine(wm_mock)
+        engine.reload_all_modules()
+        engine._working_memory.add_modules.assert_called()
 
     @unittest.skip("TODO")
     def test_load_file(self):
