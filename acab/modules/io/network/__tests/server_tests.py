f96c330114bfa9534cce85a08e73a6238aa2e96a
John Grey <johngrey4296@gmail.com>
2020-07-18 18:56:44 +0100

    add io module stubs

---
+++ acab/modules/io/network/__tests/server_tests.py
@@ -0,0 +1,49 @@
+#https://docs.python.org/3/library/unittest.html
+# https://docs.python.org/3/library/unittest.mock.html
+
+from os.path import splitext, split
+
+import unittest
+import unittest.mock as mock
+
+import logging
+
+
+class ServerTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        return
+
+    def setUp(self):
+        return 1
+
+    def tearDown(self):
+        return 1
+
+    #----------
+    # use testcase snippet
+    # mock.Mock / MagicMock
+    # create_autospec
+    # @patch(' ') / with patch.object(...)
+
+    # TODO: use socket.socketpair for testing
+    # test init
+
+    # test sending
+
+    # test listening
+
+
+
+if __name__ == "__main__":
+    #run python $filename to use this logging setup
+    #using python -m unittest $filename won't
+    LOGLEVEL = logging.INFO
+    logFileName = "log.{}".format(splitext(split(__file__)[1])[0])
+    logging.basicConfig(filename=logFileName, level=LOGLEVEL, filemode='w')
+    console = logging.StreamHandler()
+    console.setLevel(logging.WARN)
+    logging.getLogger().addHandler(console)
+    unittest.main()
+    #reminder: user logging.getLogger().setLevel(logging.NOTSET) for log control
