fdeb4b9ce6085ec6d199d0194db5dd0e4fe5badf
John Grey <johngrey4296@gmail.com>
2020-07-31 13:45:14 +0100

    improve import exception

--- acab/error/acab_import_exception.py
+++ acab/error/acab_import_exception.py
@@ -4,7 +4,10 @@ from .acab_base_exception import AcabBaseException
 class AcabImportException(AcabBaseException):
     """  """
 
-    def __init__(self):
-        return
+    def __init__(self, module_name):
+        self._name = module_name
+
+    def __str__(self):
+        return "Import Failed: {}".format(self._name)
 
 
