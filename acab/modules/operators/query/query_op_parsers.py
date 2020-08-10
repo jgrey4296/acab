718ec44b8bf59eca17b781ef218ba4a77039138a
John Grey <johngrey4296@gmail.com>
2020-08-10 17:39:44 +0100

    fixup! improve hastag construction

--- acab/modules/operators/query/query_op_parsers.py
+++ acab/modules/operators/query/query_op_parsers.py
@@ -4,6 +4,7 @@ from acab import util
 from acab.abstract.parsing import util as PU
 from acab.abstract.query import QueryComponent
 from acab.abstract.value import AcabValue
+from acab.abstract.sentence import Sentence
 
 from . import query_operators as QO
 
@@ -11,7 +12,8 @@ def construct_tag_query(toks):
     assert(util.TAG_S in toks)
     tags = [x[1] for x in toks[util.TAG_S]]
 
-    return (util.CONSTRAINT_S, QueryComponent(QO.HasTag.__name__, param=tags))
+    tag_op_path = Sentence.build([QO.HasTag.__name__])
+    return (util.CONSTRAINT_S, QueryComponent(tag_op_path, param=tags))
 
 
 tagList = PU.N(util.TAG_S, pp.delimitedList(PU.tagName, delim=","))
