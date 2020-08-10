4cd44d385c69bf35feac2bc155002bf6f8a4386f
John Grey <johngrey4296@gmail.com>
2020-08-10 17:26:52 +0100

    remove obsolete typingspec instantiation from typing

--- acab/modules/analysis/typing/__init__.py
+++ acab/modules/analysis/typing/__init__.py
@@ -4,5 +4,3 @@ The Typing Submodule Provides a TypeChecker,
 """
 from acab.modules.analysis.typing.typing_module import TypingSpec
 from .type_checker import TypeChecker
-
-MODULE_SPEC = TypingSpec()
