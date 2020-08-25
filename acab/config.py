0af0c71a289f77d4e8bd32e55995c64fcdcfe15e
John Grey <johngrey4296@gmail.com>
2020-08-25 04:46:14 +0100

    extend and rename AcabConfig's file

--- acab/config.py
+++ acab/config.py
@@ -1,19 +1,24 @@
 """ Cross-module utilities for the rule engines """
 from configparser import ConfigParser, ExtendedInterpolation
-from acab.error.acab_config_exception import AcabConfigException
 from enum import Enum
+import pyparsing as pp
 
-# Provide accessor and setup from config file
+from acab.error.acab_config_exception import AcabConfigException
+
+actions_e = Enum("Config Actions", "STRIPQUOTE KEYWORD LITERAL DICT LIST UNESCAPE")
 
 class AcabConfig:
     """ A Singleton class for the active configuration
     Uses ${SectionName:Key} interpolation in values,
     Turns multi-line values into lists
     """
-
-    actions_e = Enum("Config Actions", "STRIPQUOTE")
+    actions_e = actions_e
     actions = {
-        AcabConfig.actions_e.STRIP : lambda x: x.strip("'")
+        actions_e.STRIPQUOTE : lambda x: x.strip("'"),
+        actions_e.KEYWORD : lambda x: pp.Keyword(x),
+        actions_e.LITERAL : lambda x: pp.Literal(x),
+        actions_e.LIST : lambda x: x.split("\n"),
+        actions_e.UNESCAPE : lambda x: x.encode().decode("unicode_escape")
         }
     instance = None
 
@@ -35,17 +40,25 @@ class AcabConfig:
         for path in paths:
             self.read(path)
 
+    def read_list(self, lst):
+        assert(isinstance(lst, list))
+        for x in lst:
+            self.read(x)
+
     def read(self, path, force=False):
         if force or path not in self._files:
             self._config.read(path)
             self._files.add(path)
 
-    def __call__(self, section, key, action=None):
+    def __call__(self, section, key, *actions, action=None):
         """
         Get a value from the config
         """
-        value = self.config[section][key]
+        value = self._config[section][key]
         if action in AcabConfig.actions:
+            value = AcabConfig.actions[action](value)
+
+        for action in actions:
             value = AcabConfig.actions[action](value)
 
         return value
