a7e8af7c840d10369cd31fdadc54f49662c2d8ae
John Grey <johngrey4296@gmail.com>
2020-08-28 22:38:06 +0100

    extend acab base exception

--- acab/error/acab_base_exception.py
+++ acab/error/acab_base_exception.py
@@ -1,7 +1,9 @@
 class AcabBaseException(Exception):
     """ The base exception class for the Acab package """
 
-    def __init__(self):
-        return
+    def __init__(self, init_str=None):
+        self._str = init_str
 
+    def __str__(self):
+        return "Non-specific Error Raised: {}".format(self._str)
 
