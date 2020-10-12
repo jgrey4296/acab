e2077f7e10dad0c77fbe9fabd59b4e9ebfd6136c
John Grey <johngrey4296@gmail.com>
2020-10-12 22:49:30 +0100

    fixup! fixup! remove more references to PrU

--- acab/working_memory/trie_wm/util.py
+++ acab/working_memory/trie_wm/util.py
@@ -3,8 +3,11 @@ Cross-module utilities for the rule engines
 """
 from enum import Enum
 
+from acab.abstract.data.node import AcabNode
 from acab.config import AcabConfig
-from acab.abstract.printing import util as PrU
+
+import logging as root_logger
+logging = root_logger.getLogger(__name__)
 
 util = AcabConfig.Get()
 
@@ -13,8 +16,9 @@ ROOT_S = util("Data.Struct", "ROOT_S")
 #Trie exclusion operator:
 EXOP = Enum('EXOP', 'DOT EX')
 
-PrU.register_modal({EXOP.DOT : util("WorkingMemory.TrieWM.Parsing", "EXOP.DOT_S"),
-                    EXOP.EX : util("WorkingMemory.TrieWM.Parsing", "EXOP.EX_S")})
+# TODO
+# PrU.register_modal({EXOP.DOT : util("WorkingMemory.TrieWM.Parsing", "EXOP.DOT_S"),
+#                     EXOP.EX : util("WorkingMemory.TrieWM.Parsing", "EXOP.EX_S")})
 
 DEFAULT_NODE_DATA = {
     OPERATOR_S : EXOP[util("WorkingMemory.TrieWM", "DEFAULT_EXOP")]
@@ -24,7 +28,7 @@ DEFAULT_NODE_DATA = {
 # Utility Funtions:
 def node_is_exclusive(node):
     """ Checks for the exclusion operator in this node """
-    return node._data[util.OPERATOR_S] is EXOP.EX
+    return node._data[OPERATOR_S] is EXOP.EX
 
 def node_looks_exclusive(node):
     """ Checks for implicit exclusivity by having 0 or 1 children """
@@ -34,8 +38,8 @@ def exclusion_matches(contexts, a, b, data):
     """ Compare the EXOP of a node, with whether that exop
         is in the children of the other node/parent """
     logging.info("Running exclusion match test: {} {}".format(str(a), str(b)))
-    assert(isinstance(a, TrieNode))
-    assert(isinstance(b, TrieNode))
+    assert(isinstance(a, AcabNode))
+    assert(isinstance(b, AcabNode))
     result = node_is_exclusive(a) and not node_looks_exclusive(b)
     if result:
         logging.info("C: {}, Node: {}, {}".format(node_is_exclusive(a),
