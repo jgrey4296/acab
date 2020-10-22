b626fb6f75d0284ed467bfeeebd5d23836438e03
John Grey <johngrey4296@gmail.com>
2020-10-22 20:53:39 +0100

    refactor type_system

--- acab/abstract/core/type_system.py
+++ acab/abstract/core/type_system.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+# https://mypy.readthedocs.io/en/stable/cheat_sheet_py3.html
+from typing import List, Set, Dict, Tuple, Optional, Any
+from typing import Callable, Iterator, Union, Match
+from typing import Mapping, MutableMapping, Sequence, Iterable
+from typing import cast, ClassVar, TypeVar, Generic
 
 from acab.config import AcabConfig
 
@@ -6,7 +11,17 @@ from acab.abstract.core.value import AcabValue
 from acab.abstract.core.sentence import Sentence
 from acab.abstract.core.type_base import TypeInstance
 
+from acab.abstract.data.struct_semantics import AcabStructureSemantics
+from acab.abstract.printing.print_semantics import AcabPrintSemantics
+
 util = AcabConfig.Get()
+
+TYPE_FMT_S = util("Printing", "TYPE_FMT_S")
+QUERY_HEAD_S = util("Parsing.Statements", "QUERY_HEAD_S")
+TRANSFORM_HEAD_S = util("Parsing.Statements", "TRANSFORM_HEAD_S")
+ACTION_HEAD_S = util("Parsing.Statements", "ACTION_HEAD_S")
+RULE_HEAD_S = util("Parsing.Statements", "RULE_HEAD_S")
+VALUE_TYPE_S = util("Parsing.Structure", "VALUE_TYPE_S")
 
 # Construct the primitive types
 def build_simple_type_system():
@@ -19,10 +34,10 @@ def build_simple_type_system():
     COMPONENT = type_system.add_primitive(path=["component"])
     CONTAINER = type_system.add_primitive(path=["container"])
     
-    QUERY     = type_system.add_primitive(path=["query"], alias=QUERY_HEAD_S)
-    TRANSFORM = type_system.add_primitive(path=["transform"], alias=TRANSFORM_HEAD_S)
-    ACTION    = type_system.add_primitive(path=["action"], alias=ACTION_HEAD_S)
-    RULE      = type_system.add_primitive(path=["rule"], alias=RULE_HEAD_S)
+    QUERY     = type_system.add_primitive(path=["query"])
+    TRANSFORM = type_system.add_primitive(path=["transform"])
+    ACTION    = type_system.add_primitive(path=["action"])
+    RULE      = type_system.add_primitive(path=["rule"])
 
     return type_system
 
@@ -43,7 +58,13 @@ class AcabTypeSystem:
     @staticmethod
     def GET(**kwargs):
         if AcabTypeSystem._active_type_system is None or "OVERRIDE" in kwargs:
-            new_system = AcabTypeSystem(**kwargs)
+            if 'name' in kwargs:
+                name = kwargs['name']
+                del kwargs['name']
+            else:
+                # TODO randomise this
+                name = "default"
+            new_system = AcabTypeSystem(name, **kwargs)
             AcabTypeSystem._all_systems[new_system.name]
             AcabTypeSystem._active_type_system = new_system
 
@@ -51,7 +72,6 @@ class AcabTypeSystem:
 
     def __init__(self, name, primitives=None, instance_constructor=None, checker=None):
         AcabValue._set_type_system(self)
-
         self._name = name
         self._primitives: List[TypeInstance] = {}
         self._all_types = []
@@ -105,10 +125,17 @@ class AcabTypeSystem:
 
     # TODO def STRING REGEX
 
-    def add_primitive(self, path=None, alias=None):
+    @property
+    def CONTAINER(self):
+        if "CONTAINER" in self._primitives:
+            return self._primitives["CONTAINER"]
+
+        return self.BOTTOM
+
+    def add_primitive(self, path=None):
         assert(isinstance(path, list))
         path_sentence = Sentence.build(path)
-        new_instance = self._instance_constructor(path, type_alias_str=alias)
+        new_instance = self._instance_constructor(path)
         self._primitives[path_sentence] = new_instance
         return new_instance
 
@@ -116,7 +143,7 @@ class AcabTypeSystem:
     def add_type_definition(self, **kwargs):
         raise NotImplementedError()
 
-        def get_alias_chars():
+    def get_alias_chars(self):
         sigils = [x[-1]._primitive._type_alias for x in TypeInstance._type_system.primitives]
         sigils_str = "".join([x for x in sigils if x is not None])
         return sigils_str
