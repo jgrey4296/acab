a1b88a31bc2600c1a2768b7290eeb5646a68080c
John Grey <johngrey4296@gmail.com>
2020-10-26 20:55:42 +0000

    update const loading in utils

--- acab/modules/analysis/typing/util.py
+++ acab/modules/analysis/typing/util.py
@@ -3,6 +3,7 @@ import pyparsing as pp
 
 from acab.config import AcabConfig
 
+from acab.abstract.core.type_system import AcabTypeSystem
 from acab.abstract.core.type_base import TypeInstance
 
 from acab.abstract.parsing.util import s
@@ -24,16 +25,17 @@ STRUCT_S      = util("Module.Typing", "STRUCT_S")
 TVAR_S        = util("Module.Typing", "TVAR_S")
 SYNTAX_BIND_S = util("Module.Typing", "SYNTAX_BIND_S")
 
-SUM_HEAD       = s(util("Module.Typing", "SUM_HEAD_S", action=AcabConfig.actions_e.KEYWORD))
-STRUCT_HEAD    = s(util("Module.Typing", "STRUCTURE_S", action=AcabConfig.actions_e.KEYWORD))
-TYPE_CLASS_HEAD = s(util("Module.Typing", "TYPE_CLASS_S", action=AcabConfig.actions_e.KEYWORD))
-FUNC_HEAD      = s(pp.Word(util("Parsing", "FUNC_SYMBOL_S")))
+SUM_HEAD       = s(util("Module.Typing.Symbols", "SUM_HEAD_S", action=AcabConfig.actions_e.KEYWORD))
+STRUCT_HEAD    = s(util("Module.Typing.Symbols", "STRUCTURE_S", action=AcabConfig.actions_e.KEYWORD))
+TYPE_CLASS_HEAD = s(util("Module.Typing.Symbols", "TYPE_CLASS_S", action=AcabConfig.actions_e.KEYWORD))
+FUNC_HEAD      = s(pp.Word(util("Visual.Symbols", "FUNC_SYMBOL_S")))
 
 DELIM_S       = util("Module.Typing", "DELIM_S", action=AcabConfig.actions_e.STRIPQUOTE)
 
-TYPE_DEFINITION = TypeInstance(path=[TYPE_DEF_S], type_alias_str=util("Module.Typing", "STRUCTURE_S"), primitive=True)
-SUM_DEFINITION = TypeInstance(path=[SUM_DEF_S], type_alias_str=util("Module.Typing", "SUM_HEAD_S"), primitive=True)
-OPERATOR_DEFINITION = TypeInstance(path=[OP_DEF_S], type_alias_str=util("Parsing", "FUNC_SYMBOL_S"), primitive=True)
+# TODO make these registrations
+TYPE_DEFINITION = TypeInstance(path=[TYPE_DEF_S])
+SUM_DEFINITION = TypeInstance(path=[SUM_DEF_S])
+OPERATOR_DEFINITION = TypeInstance(path=[OP_DEF_S])
 # TODO TYPE CLASS
 
 STRUCT_HEAD.setName("StructHead")
--- acab/modules/values/numbers/util.py
+++ acab/modules/values/numbers/util.py
@@ -2,12 +2,12 @@ from acab.abstract.core.type_base import TypeInstance
 from acab.config import AcabConfig
 
 util = AcabConfig.Get()
-DECIMAL_S = util("Module.Numbers", "DECIMAL_S")
+DECIMAL_SYMBOL_S = util("Module.Numbers", "DECIMAL_SYMBOL_S")
 
-
-FLOAT_t = TypeInstance(path=["number", "float"], primitive=True)
-INT_t = TypeInstance(path=["number", "int"], primitive=True)
-FRACT_t = TypeInstance(path=["number", "fract"], primitive=True)
+# TODO replace this with type_system registration
+FLOAT_t = TypeInstance(path=["number", "float"])
+INT_t = TypeInstance(path=["number", "int"])
+FRACT_t = TypeInstance(path=["number", "fract"])
 
 
 def _wrap_float(value, opts=None):
