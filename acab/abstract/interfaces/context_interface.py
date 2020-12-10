ddd7937a8d9b4e567c036f7b0f84f7512395c072
John Grey <johngrey4296@gmail.com>
2020-12-10 17:39:07 +0000

    fix typo in context interface stub

--- acab/abstract/interfaces/context_interface.py
+++ acab/abstract/interfaces/context_interface.py
@@ -1,42 +1,47 @@
 #!/usr/bin/env python3
 
 import abc
+# https://mypy.readthedocs.io/en/stable/cheat_sheet_py3.html
+from typing import List, Set, Dict, Tuple, Optional, Any
+from typing import Callable, Iterator, Union, Match
+from typing import Mapping, MutableMapping, Sequence, Iterable
+from typing import cast, ClassVar, TypeVar, Generic
 
-class ContextInterface(metaclass=abc.ABCMetaClass):
+class ContextInterface(metaclass=abc.ABCMeta):
     """  """
 
     @abc.abstractmethod
-    def append(self) -> :
+    def append(self) -> Any:
         pass
 
     @abc.abstractmethod
-    def fail(self) -> :
+    def fail(self) -> Any:
         pass
 
     @abc.abstractmethod
-    def clear(self) -> :
+    def clear(self) -> Any:
         pass
 
     @abc.abstractmethod
-    def collapse(self) -> :
+    def collapse(self) -> Any:
         pass
 
     @abc.abstractmethod
-    def group_by_type(self) -> :
+    def group_by_type(self) -> Any:
         pass
 
     @abc.abstractmethod
-    def promote_failures(self) -> :
+    def promote_failures(self) -> Any:
         pass
 
     @abc.abstractmethod
-    def demote_failures(self) -> :
+    def demote_failures(self) -> Any:
         pass
 
     @abc.abstractmethod
-    def force_node_position(self) -> :
+    def force_node_position(self) -> Any:
         pass
 
     @abc.abstractmethod
-    def rebind_across_contexts(self) -> :
+    def rebind_across_contexts(self) -> Any:
         pass
