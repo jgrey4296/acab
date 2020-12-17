308133de719823cce480bb7b341c87d8cc756363
John Grey <johngrey4296@gmail.com>
2020-12-17 22:49:57 +0000

    remove obsolete stubs

--- acab/engines/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Engines are the highest level interface in acab.
-They package a working memory up with plumbing to
-load rules, setup IO, and structure high level agent loops.
-
-
-"""
--- acab/working_memory/hypergraph_wm/hg_edge.py
+++
@@ -1,9 +0,0 @@
-from .hg_node import HGNode
-
-
-class HGEdge(HGNode):
-    """ An Edge in a HyperGraph """
-
-    def __init__(self):
-        super().__init__()
-        self._node_sets = {}
