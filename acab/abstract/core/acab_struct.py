3fc591258a97724c0db2bcf8179f81d17563bde1
John Grey <johngrey4296@gmail.com>
2020-12-24 23:33:47 +0000

    rename structure -> AcabStruct to stop pyline name conflict

---
+++ acab/abstract/core/acab_struct.py
@@ -0,0 +1,17 @@
+
+from acab.abstract.interfaces.data_interfaces import StructureInterface
+
+class AcabStruct(StructureInterface):
+    """ A collection of nodes """
+
+    def __init__(self):
+        pass
+
+    def add(self, sen):
+        pass
+
+    def query(self, sen):
+        pass
+
+    def contain(self, sen):
+        pass
