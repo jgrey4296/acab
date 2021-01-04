85a7756c330557328d3788bdbb002e1c58219a08
John Grey <johngrey4296@gmail.com>
2021-01-04 00:07:48 +0000

    add differentiate general and debug listeners

--- acab/modules/semantics/listener_mixin.py
+++ acab/modules/semantics/listener_mixin.py
@@ -1,59 +1,28 @@
-#!/usr/bin/env python3
+# https://mypy.readthedocs.io/en/stable/cheat_sheet_py3.html
+from typing import List, Set, Dict, Tuple, Optional, Any
+from typing import Callable, Iterator, Union, Match
+from typing import Mapping, MutableMapping, Sequence, Iterable
+from typing import cast, ClassVar, TypeVar, Generic
+
+from dataclasses import dataclass, field
 import acab.abstract.interfaces.semantic_interfaces as SI
 
+Sentence = 'Sentence'
+
+ListenerForm = Union[SI.SemanticHandler, Callable]
+
+@dataclass
 class ListenerMixin(SI.SemanticMixin):
-    """ TODO formerly SubSemantics
-    Listeners for triggers
+    """ Maps aribitrary strings to semantics
+    rather than mapping values or nodes
+
+
     """
-    """ The core used to *debug* WM action, using listeners """
-    _listeners : Set[Any] = field(init=False, default_factory=set)
-    _listeners_threshold : Fraction = field(init=False, default=Fraction(1,2))
-    # TODO use these to enable breakpoint context:
-    _production_stack : List['ProductionAbstraction'] = field(init=False, default_factory=list)
 
-    def clear_listeners(self):
-        self._listeners = set()
+    handlers : Dict[str, ListenerForm] = field(default_factory=dict)
 
-    def register_listeners(self, words):
-        self._listeners.update(words)
-
-    def unregister_listeners(self, words):
-        self._listeners.difference_update(words)
-
-    def set_listener_threshold(self, a, b):
-        self._listener_threshold = Fraction(a,b)
-
-    def score_listener(self, words):
-        simple_words = [str(x) if not x.is_var else "$_" for x in words]
-        num_in_listener_bag = sum([1 if x in self._listeners else 0 for x in simple_words])
-        sentence_fraction = Fraction(num_in_listener_bag, len(simple_words))
-        if sentence_fraction >= self._listener_threshold:
-            return True
-
-        return False
-
-    def breakpoint(self):
-        # TODO: add more listener options: pre, on and post
-        breakpoint()
-
-
-
-
-    def add_listeners(self, *words):
-        """ Add basic data breakpoints """
-        self._working_memory.register_listeners(words)
-
-    def remove_listeners(self, *words):
-        """ Remove data breakpoints """
-        self._working_memory.unregister_listeners(words)
-
-    def set_listener_threshold(self, a, b):
-        """ Specify the number of word matches
-        are needed to trigger the breakpoint """
-        self._working_memory.set_listener_threshold(a, b)
-
-    def get_listeners(self):
-        return self._working_memory._listeners
-
-    def get_listener_threshold(self):
-        return self._working_memory._listener_threshold
+    def call(self, instruction: Sentence) -> Optional[ListenerForm]:
+        """
+        Take an instruction and return a suitable handler
+        """
+        pass
