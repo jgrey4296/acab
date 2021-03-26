762e9fec3ccba3e186a1aa0f7f6671b712482c57
John Grey <johngrey4296@gmail.com>
2021-03-26 23:38:28 +0000

    rework acabstruct

--- acab/abstract/core/acab_struct.py
+++ acab/abstract/core/acab_struct.py
@@ -1,5 +1,10 @@
-
+from acab.abstract.config.config import AcabConfig
 from acab.abstract.interfaces.data_interfaces import StructureInterface
+from acab.abstract.core.node import AcabNode
+from acab.abstract.core.values import AcabValue
+
+config = AcabConfig.Get()
+ROOT = AcabValue(name=config.value("Data", "ROOT"))
 
 class AcabStruct(StructureInterface):
     """ A collection of nodes """
@@ -7,11 +12,17 @@ class AcabStruct(StructureInterface):
     def __init__(self):
         pass
 
-    def add(self, sen):
-        pass
+class BasicNodeStruct(AcabStruct):
 
-    def query(self, sen):
-        pass
+    @staticmethod
+    def build_default():
+        return BasicNodeStruct(AcabNode.Root())
 
-    def contain(self, sen):
-        pass
+    def __init__(self, root):
+        self.root = root
+
+class NPArrayStruct(AcabStruct):
+    pass
+
+class NXGraphStruct(AcabStruct):
+    pass
