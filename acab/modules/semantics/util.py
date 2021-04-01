185ac56f315a562dd22d22ca9719c61346b1a771
John Grey <johngrey4296@gmail.com>
2021-04-01 17:13:32 +0100

    add filter candidates to semantics util

--- acab/modules/semantics/util.py
+++ acab/modules/semantics/util.py
@@ -32,3 +32,45 @@ def _get_params(self, params, bound_context):
         else:
             output.append(x.value)
     return output
+
+def _filter_candidates(self, target_pattern, candidates, match_func):
+    """ Filter candidates using match_func to compare
+    against this data_structure
+
+    Where a Match = [(PatternNode, MatchNode)]
+    Return [Match]
+
+    match_func : Node -> [Node] -> [Node]
+    """
+    # TODO check this
+    assert(isinstance(target_pattern, AcabStruct))
+
+    if isinstance(candidates, AcabStruct):
+        candidates = candidates.root
+
+    if not isinstance(candidates, AcabNode):
+        raise AcabBaseException()
+
+    final_matches = []
+    pattern_nodes = list(candidates.children.values())
+    # (current pattern position, available choices, match state)
+    queue = [(word, pattern_nodes, []) for word in target_pattern.root.children.values()]
+
+    while bool(queue):
+        current, available_nodes, match_state = queue.pop(0)
+
+        matching_nodes = match_func(current, available_nodes)
+        for node in matching_nodes:
+            next_match_state = match_state + [(current, node)]
+
+            if bool(current):
+                next_available = list(node.children.values())
+                next_patterns = list(current.children.values())
+                queue += [
+                    (word, next_available, next_match_state)
+                    for word in next_patterns
+                ]
+            else:
+                final_matches.append(next_match_state)
+
+    return final_matches
