afd9fa8d5b6a4550d361686c2e58a31393fa4c05
John Grey <johngrey4296@gmail.com>
2021-04-12 20:57:45 +0100

    add semantic exceptions

--- acab/error/acab_semantic_exception.py
+++ acab/error/acab_semantic_exception.py
@@ -18,3 +18,37 @@ class AcabOperatorMissingException(AcabSemanticException):
 
     def __str__(self):
         return "Semantic Failure: {} at: {}".format(self._info, str(self._data))
+
+class AcabSemanticTestFailure(AcabSemanticException):
+    """ Raised by ConstraintCollection when a test fails,
+    for ContextContainer to handle
+    """
+
+    def __init__(self, info, fail_clause):
+        self._info = info
+        self._data = fail_clause
+
+    def __str__(self):
+        return "Semantic Failure: {} at: {}".format(self._info, str(self._data))
+
+class AcabSemanticQueryContextDepletionFailure(AcabSemanticException):
+    """ Raised by Dependent Semantics to signal the current
+    ContextInstance can't progress any further """
+
+    def __init__(self, info, fail_clause):
+        self._info = info
+        self._data = fail_clause
+
+    def __str__(self):
+        return "Semantic Failure: {} at: {}".format(self._info, str(self._data))
+
+
+class AcabSemanticIndependentFailure(AcabSemanticException):
+    """ Signals failures in the node semantics """
+
+    def __init__(self, info, fail_clause):
+        self._info = info
+        self._data = fail_clause
+
+    def __str__(self):
+        return "Semantic Failure: {} at: {}".format(self._info, str(self._data))
