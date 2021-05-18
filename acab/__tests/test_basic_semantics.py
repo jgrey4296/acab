3d51257c89a86370a65836b27da7f34964913a4c
John Grey <johngrey4296@gmail.com>
2021-05-18 22:22:19 +0100

    import semantic hints and change BIND -> BIND_V

--- acab/__tests/test_basic_semantics.py
+++ acab/__tests/test_basic_semantics.py
@@ -13,15 +13,16 @@ import unittest.mock as mock
 from os.path import split, splitext
 
 import acab
-config       = acab.setup()
+config = acab.setup()
 
 import acab.modules.semantics.abstractions as ASem
 from acab.abstract.core.acab_struct import BasicNodeStruct
 from acab.abstract.core.node import AcabNode
-from acab.abstract.core.production_abstractions import (ProductionOperator,
-                                                        ActionOperator,
+from acab.abstract.core.production_abstractions import (ActionOperator,
                                                         ProductionComponent,
-                                                        ProductionContainer)
+                                                        ProductionContainer,
+                                                        ProductionOperator,
+                                                        ProductionStructure)
 from acab.abstract.core.values import AcabValue, Sentence
 from acab.abstract.interfaces.semantic_interfaces import (AbstractionSemantics,
                                                           SemanticSystem)
@@ -32,20 +33,32 @@ from acab.modules.operators.transform.transform_operators import RegexOp
 from acab.modules.semantics.context_container import (ConstraintCollection,
                                                       ContextContainer,
                                                       ContextInstance)
-from acab.modules.semantics.system import BasicSemanticSystem
 from acab.modules.semantics.dependent import BreadthTrieSemantics
 from acab.modules.semantics.independent import (BasicNodeSemantics,
                                                 ExclusionNodeSemantics)
+from acab.modules.semantics.system import BasicSemanticSystem
 from acab.modules.semantics.util import SemanticOperatorWrapDecorator
 
 EXOP         = config.value("MODAL", "exop")
 EXOP_enum    = config.modal_enums[EXOP]
 
 NEGATION_V   = config.value("Value.Structure", "NEGATION")
-BIND         = config.value("Value.Structure", "BIND")
+BIND_V       = config.value("Value.Structure", "BIND")
 CONSTRAINT_V = config.value("Value.Structure", "CONSTRAINT")
+QUERY_V      = config.value("Parse.Structure", "QUERY")
+TRANSFORM_V  = config.value("Parse.Structure", "TRANSFORM")
+ACTION_V     = config.value("Parse.Structure", "ACTION")
 
 SEMANTIC_HINT_V = config.value("Value.Structure", "SEMANTIC_HINT")
+
+QUERY_SEM_HINT     = Sentence.build([config.value("SEMANTICS", "QUERY")])
+ACTION_SEM_HINT    = Sentence.build([config.value("SEMANTICS", "ACTION")])
+TRANSFORM_SEM_HINT = Sentence.build([config.value("SEMANTICS", "TRANSFORM")])
+RULE_SEM_HINT      = Sentence.build([config.value("SEMANTICS", "RULE")])
+AGENDA_SEM_HINT    = Sentence.build([config.value("SEMANTICS", "AGENDA")])
+LAYER_SEM_HINT     = Sentence.build([config.value("SEMANTICS", "LAYER")])
+PIPELINE_SEM_HINT  = Sentence.build([config.value("SEMANTICS", "PIPELINE")])
+
 # TODO test verify
 
 class IndependentSemanticTests(unittest.TestCase):
@@ -289,7 +302,7 @@ class TrieSemanticTests(unittest.TestCase):
         ctx_container = ContextContainer.build()
         # Construct query sentence
         query_sen = Sentence.build(["a", "test", "x"])
-        query_sen[-1].data[BIND] = True
+        query_sen[-1].data[BIND_V] = True
         # Run query
         trie_sem.query(trie_struct, query_sen, ctxs=ctx_container)
         self.assertEqual(len(ctx_container), 2)
@@ -310,8 +323,8 @@ class TrieSemanticTests(unittest.TestCase):
         ctx_container = ContextContainer.build()
         # Construct query sentence
         query_sen = Sentence.build(["a", "x", "x"])
-        query_sen[-2].data[BIND] = True
-        query_sen[-1].data[BIND] = True
+        query_sen[-2].data[BIND_V] = True
+        query_sen[-1].data[BIND_V] = True
         # Run query
         trie_sem.query(trie_struct, query_sen, ctxs=ctx_container)
         self.assertEqual(len(ctx_container), 1)
@@ -334,7 +347,7 @@ class TrieSemanticTests(unittest.TestCase):
         ctx_container = ContextContainer.build(op_ctx)
         # Construct query sentence
         query_sen = Sentence.build(["a", "test", "x"])
-        query_sen[-1].data[BIND] = True
+        query_sen[-1].data[BIND_V] = True
         # Test for equality to "sentence"
         the_test = ProductionComponent("alpha test",
                                        op_loc_path,
@@ -362,13 +375,13 @@ class TrieSemanticTests(unittest.TestCase):
         ctx_container = ContextContainer.build(op_ctx)
         # Construct query sentence
         query_sen = Sentence.build(["a", "test", "x"])
-        query_sen[-1].data[BIND] = True
+        query_sen[-1].data[BIND_V] = True
         # Second Query sentence
         query_sen2 = Sentence.build(["a", "different", "y"])
-        query_sen2[-1].data[BIND] = True
+        query_sen2[-1].data[BIND_V] = True
         # Test for equality to "sentence"
         test_var = AcabValue.safe_make("x",
-                                       data={BIND: True})
+                                       data={BIND_V: True})
         the_test = ProductionComponent("beta test",
                                        op_loc_path,
                                        [test_var])
@@ -400,13 +413,13 @@ class TrieSemanticTests(unittest.TestCase):
         ctx_container = ContextContainer.build(op_ctx)
         # Construct query sentence
         query_sen = Sentence.build(["a", "test", "x"])
-        query_sen[-1].data[BIND] = True
+        query_sen[-1].data[BIND_V] = True
         # Second Query sentence
         query_sen2 = Sentence.build(["a", "different", "y"])
-        query_sen2[-1].data[BIND] = True
+        query_sen2[-1].data[BIND_V] = True
         # Test for equality to "sentence"
         test_var = AcabValue.safe_make("x",
-                                       data={BIND: True})
+                                       data={BIND_V: True})
         the_test = ProductionComponent("callable test",
                                        op_loc_path,
                                        [test_var])
@@ -447,7 +460,7 @@ class AbstractionSemanticTests(unittest.TestCase):
         })
         ctx_container.push(updated_ctx)
         # Build Transform
-        rebind_target = AcabValue.safe_make("y", data={BIND: True})
+        rebind_target = AcabValue.safe_make("y", data={BIND_V: True})
         clause = ProductionComponent("transform test",
                                      op_loc_path,
                                      ["$x", "es", "ES"],
@@ -564,7 +577,7 @@ class AbstractionSemanticTests(unittest.TestCase):
         })
         ctx_container.push(updated_ctx)
         # Build Container
-        rebind_target    = AcabValue.safe_make("y", data={BIND: True})
+        rebind_target    = AcabValue.safe_make("y", data={BIND_V: True})
         transform_clause = ProductionComponent("transform test",
                                                trans_op_loc_path,
                                                ["$x"],
