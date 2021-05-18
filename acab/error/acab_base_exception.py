6b9424e64fa1db1660e5016ad22ab1e91fe086b1
John Grey <johngrey4296@gmail.com>
2021-05-18 22:19:44 +0100

    extend the base exception to take *args

--- acab/error/acab_base_exception.py
+++ acab/error/acab_base_exception.py
@@ -1,9 +1,10 @@
 class AcabBaseException(Exception):
     """ The base exception class for the Acab package """
 
-    def __init__(self, init_str=None):
+    def __init__(self, init_str=None, *args):
         self._str = init_str
+        self._args = args
 
     def __str__(self):
-        return "Non-specific Error Raised: {}".format(self._str)
-
+        return "Non-specific Error Raised: {} : {}".format(self._str,
+                                                           self._args)
