2f5372f3bdb5dd5fd56657ed1573c1a71e0f4df4
John Grey <johngrey4296@gmail.com>
2021-06-08 12:26:26 +0100

    remove obsolete working memory code, refactor bootstrap parser

--- acab/modules/working_memory/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Knowledge Bases are the core component of acab.
-The provide a core Meta-DSL with associated semantics.
-
-to implemented later:
-hypergraph, numpy, rdf focused WM's to supplement the Trie based Exclusion/Modal WM
-
-"""
