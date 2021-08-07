84ea8f37e45df31b935c4c1d4e5fc2b46fd1f52e
John Grey <johngrey4296@gmail.com>
2021-08-07 06:23:14 +0100

    [repl] refactor parser

--- acab/modules/repl/ReplParser.py
+++ acab/modules/repl/ReplParser.py
@@ -2,233 +2,85 @@
 The parser for the REPL
 
 """
-import pyparsing as pp
 import logging as root_logger
 
+import pyparsing as pp
 from acab.abstract.config.config import AcabConfig
-from acab.abstract.parsing import util as PU
+from acab.abstract.parsing import consts as PU
 
 logging = root_logger.getLogger(__name__)
-HOTLOAD_COMMANDS = pp.Forward()
 config = AcabConfig.Get()
 
-MULTI_LINE_REGEX = config("REPL", "MULTI_LINE_REGEX")
+rst = pp.restOfLine()
 
+# multi line ##################################################################
+MULTI_LINE_START = config.prepare("Module.REPL", "MULTI_LINE_START")()
+MULTI_LINE_END   = config.prepare("Module.REPL", "MULTI_LINE_END")()
 
-def build_slice(toks):
-    result = None
-    first  = 0
-    second = 0
-    if 'first' in toks:
-        first = toks['first']
-        result = first
+multi_line_start = pp.Regex(MULTI_LINE_START)
+multi_line_end = pp.Regex(MULTI_LINE_END)
 
-    if 'second' in toks:
-        second = toks['second'][0]
-        result = slice(first, second)
+multi_line_start.setParseAction(lambda s,l,t: "multi")
+multi_line_end.setParseAction(lambda s,l,t: "multi")
+# ##############################################################
+# ie: sugar parser:
+precmd_parser = pp.Suppress(pp.LineStart()) + pp.Or([multi_line_start,
+                                                     multi_line_end,
+                                                     rst])
 
-    return result
+# step kws ####################################################################
+back_kw     = pp.Keyword("back")
+rule_kw     = pp.Keyword("rule")
+layer_kw    = pp.Keyword("layer")
+pipe_kw     = pp.Keyword("pipe")
+pipeline_kw = pp.Keyword("pipeline")
+step_parser = pp.Or([back_kw,
+                     rule_kw + rst,
+                     layer_kw,
+                     pipe_kw,
+                     pipeline_kw])
 
-def build_multiline(toks):
-    param = True
-    if toks[0][1] == "}":
-        param = False
-
-    return build_command(RE.MULTILINE, params=[param])
-
-
-# Keywords
-load_kw      = pp.Keyword('load')
-save_kw      = pp.Keyword('save')
+# print kws ###################################################################
 wm_kw        = pp.Keyword("wm")
 bootstrap_kw = pp.Keyword("bootstrap")
-pipeline_kw  = pp.Keyword("pipeline")
-action_kw    = pp.Keyword("action")
-operator_kw  = pp.Keyword("operator")
-agenda_kw    = pp.Keyword("agenda")
-binding_kw   = pp.Keyword("binding")
-break_kw     = pp.Keyword("break")
-keyword_kw   = pp.Keyword("keyword")
-module_kw    = pp.Or([pp.Keyword(x) for x in ['module', 'mod']])
+module_kw    = pp.Keyword("module")
+bind_kw      = pp.Keyword("binding")
 
-for_kw       = PU.s(pp.Keyword('for'))
-act_kw       = PU.s(pp.Keyword('act'))
-all_kw       = PU.s(pp.Keyword('all'))
-back_kw      = PU.s(pp.Keyword('back'))
-check_kw     = PU.s(pp.Keyword('check'))
-decompose_kw = PU.s(pp.Keyword('decompose'))
-exit_kw      = PU.s(pp.Or([pp.Keyword(x) for x in ["exit", "quit"]]))
-help_kw      = PU.s(pp.Keyword('help'))
-init_kw      = PU.s(pp.Keyword('init'))
-layer_kw     = PU.s(pp.Keyword('layer'))
-listen_kw    = PU.s(pp.Keyword('listen'))
-listener_kw  = PU.s(pp.Keyword('listener'))
-listeners_kw = PU.s(pp.Keyword('listeners'))
-threshold_kw = PU.s(pp.Keyword("threshold"))
-print_kw     = PU.s(pp.Keyword('print'))
-remove_kw    = PU.s(pp.Keyword('remove'))
-rule_kw      = PU.s(pp.Keyword('rule'))
-run_kw       = PU.s(pp.Keyword('run'))
-stats_kw     = PU.s(pp.Keyword('stats'))
-step_kw      = PU.s(pp.Keyword('step'))
-prompt_kw    = PU.s(pp.Keyword('prompt'))
-from_kw      = PU.s(pp.Keyword('from'))
-echo_kw      = PU.s(pp.Keyword('echo'))
+# TODO also handle bind without kw?
+printer_parser = pp.Or([wm_kw,
+                        bootstrap_kw,
+                        module_kw + rst,
+                        layer_kw + rst,
+                        pipe_kw,
+                        pipeline_kw,
+                        bind_kw + rst])
+# stat kws ####################################################################
+agenda_kw = pp.Keyword("agenda")
 
-# Default: Instructions to pass to an Engine
-empty_line = pp.lineStart + pp.lineEnd
-rest_of_line = PU.op(PU.s(pp.White())) + pp.restOfLine
+stats_parser = pp.Or([wm_kw,
+                      pipe_kw,
+                      pipeline_kw,
+                      layer_kw,
+                      agenda_kw,
+                      rule_kw,
+                      module_kw,
+                      bind_kw])
 
-multi_line = pp.Regex(MULTI_LINE_REGEX)
-multi_line_pop = pp.Literal(':pop')
+# listener ####################################################################
 
-# TODO
-number = pp.Word(pp.nums)
-slice_p = PU.s(pp.Literal('[')) + \
-    PU.op(number).setResultsName('first') + \
-    PU.op(PU.s(pp.Literal(':')) + number).setResultsName('second') + \
-    PU.s(pp.Literal(']'))
+# add/remove/list/threshold
 
-param_p = pp.Or([number, slice_p])
-
-# assertions / retractions / query
-# eg: a.test.statement
-# eg2: a.test.query?
-base_statement = rest_of_line.copy()
-nop_line = rest_of_line.copy()
-
-# run rule/layer/pipeline (select appropriate method by its type)
-# treat string as query
-run_something = run_kw + PU.op(PU.BIND + from_kw + rest_of_line)
-# print trie / query results / selected type
-print_alts = pp.Or([wm_kw, bootstrap_kw, layer_kw, module_kw, pipeline_kw, binding_kw])
-
-print_state   = print_kw + print_alts + PU.op(param_p)
-
-# Instructions to modify engine
-# eg: load ~/test/file.trie
-state_io_cmds = pp.Or([save_kw, load_kw])
-file_path = rest_of_line
-# save / load state
-state_io = state_io_cmds + file_path
-
-# initialise
-# eg: init acab.engines.trie_engine.TrieEngine
-reinit = init_kw + pp.Optional(rest_of_line)
-
-# step forward or back
-# eg: step rule a.test.rule?
-step = step_kw + pp.Optional(pp.Or([back_kw,
-                                    rule_kw + rest_of_line,
-                                    layer_kw + rest_of_line]))
-
-# Instructions to load a module
-# load module
-# eg: load acab.modules.values.numbers
-load_mod = PU.s(module_kw) + rest_of_line
-
-# Misc Instructions
-# perform an action manually
-# eg: act print(a.test.print)
-manual_act = act_kw + rest_of_line
-
-# Decompose rule/layer/pipeline into bindings
-# eg: decompose a.test.$rule?
-decompose = decompose_kw + rest_of_line
-
-# Pause on assertion/retraction/rule/layer/pipeline/action
-# eg: listen for a.test.assertion
-listen_for = listen_kw + for_kw  + rest_of_line
-listeners = listeners_kw
-listener_remove = remove_kw + listener_kw + rest_of_line
-listen_threshold = listen_kw + threshold_kw + rest_of_line
-
-exit_cmd = exit_kw
-
-# Help Instructions
-help_cmd = help_kw
-# Type Check all loaded / this string
-type_check = check_kw + pp.Optional(rest_of_line)
-# Print stats
-# TODO: add control over stats
-stat_words = pp.Or([operator_kw, agenda_kw, rule_kw, pipeline_kw,
-                    layer_kw, module_kw, wm_kw, binding_kw, keyword_kw])
-stats = stats_kw + pp.ZeroOrMore(stat_words)
-
-# Set prompt
-prompt_cmd = prompt_kw + rest_of_line
-
-break_cmd = break_kw
-
-# Actions
-slice_p.setParseAction      (build_slice)
-number.setParseAction       (lambda s, l, toks: int (toks[0]))
-rest_of_line.setParseAction (lambda s, l, toks: toks[0])
-save_kw.setParseAction      (lambda s, l, toks: RE.SAVE)
-load_kw.setParseAction      (lambda s, l, toks: RE.LOAD)
-multi_line.setParseAction   (build_multiline)
-multi_line_pop.setParseAction(lambda s, l, toks: (RE.POP))
-empty_line.setParseAction   (lambda s, l, toks: build_command(RE.NOP, params=[]))
+listen_parser = pp.Or([])
 
 
-break_cmd.setParseAction       (lambda s, l, toks: build_command(RE.BREAK))
-state_io.setParseAction        (lambda s, l, toks: build_command(toks[0], params=toks[1:]))
-base_statement.setParseAction  (lambda s, l, toks: build_command(RE.PASS, params=toks[:]))
-nop_line.setParseAction        (lambda s, l, toks: build_command(RE.NOP, params=toks[:]))
-decompose.setParseAction       (lambda s, l, toks: build_command(RE.DECOMPOSE, params=toks[:]))
-listen_for.setParseAction      (lambda s, l, toks: build_command(RE.LISTEN, type="add", params=toks[:]))
-listener_remove.setParseAction (lambda s, l, toks: build_command(RE.LISTEN, type="remove", params=toks[:]))
-listeners.setParseAction       (lambda s, l, toks: build_command(RE.LISTEN, type="list", params=toks[:]))
-listen_threshold.setParseAction(lambda s, l, toks: build_command(RE.LISTEN, type="threshold", params=toks[:]))
-load_mod.setParseAction        (lambda s, l, toks: build_command(RE.MODULE, params=toks[:]))
-manual_act.setParseAction      (lambda s, l, toks: build_command(RE.ACT, params=toks[:]))
-print_state.setParseAction     (lambda s, l, toks: build_command(RE.PRINT, params=toks[:]))
-reinit.setParseAction          (lambda s, l, toks: build_command(RE.INIT, params=toks[:]))
-run_something.setParseAction   (lambda s, l, toks: build_command(RE.RUN, params=toks[:]))
-step.setParseAction            (lambda s, l, toks: build_command(RE.STEP, params=toks[:]))
-type_check.setParseAction      (lambda s, l, toks: build_command(RE.CHECK, params=toks[:]))
-prompt_cmd.setParseAction      (lambda s, l, toks: build_command(RE.PROMPT, params=toks[:]))
-exit_cmd.setParseAction        (lambda s, l, toks: build_command(RE.EXIT))
-help_cmd.setParseAction        (lambda s, l, toks: build_command(RE.HELP, params=[]))
-stats.setParseAction           (lambda s, l, toks: build_command(RE.STATS, params=toks[:]))
-echo_kw.setParseAction         (lambda s, l, toks: build_command(RE.ECHO, params=[]))
+# misc ########################################################################
+# number = pp.Word(pp.nums)
+# slice_p = PU.s(pp.Literal('[')) + \
+#     PU.op(number).setResultsName('first') + \
+#     PU.op(PU.s(pp.Literal(':')) + number).setResultsName('second') + \
+#     PU.s(pp.Literal(']'))
 
-# Names
-main_commands = PU.s(pp.Literal(':')) + pp.Or([run_something,
-                                               listeners,
-                                               listen_threshold,
-                                               listen_for,
-                                               listener_remove,
-                                               print_state,
-                                               state_io,
-                                               reinit,
-                                               step,
-                                               load_mod,
-                                               manual_act,
-                                               listen_for,
-                                               type_check,
-                                               stats,
-                                               help_cmd,
-                                               exit_cmd,
-                                               prompt_cmd,
-                                               echo_kw,
-                                               break_cmd,
-                                               HOTLOAD_COMMANDS])
+# param_p = pp.Or([number, slice_p])
 
-
-
-parse_point = pp.MatchFirst([multi_line,
-                             main_commands,
-                             empty_line,
-                             base_statement])
-
-alt_parse_point = pp.MatchFirst([multi_line,
-                                 nop_line])
-
-def parseString(in_string, in_multi_line=False):
-    if not in_multi_line:
-        result = parse_point.parseString(in_string)[0]
-    else:
-        logging.info("In ML: {}".format(in_multi_line))
-        result = alt_parse_point.parseString(in_string)[0]
-    return result
+# # print trie / query results / selected type
+# print_alts = pp.Or([wm_kw, bootstrap_kw, layer_kw, module_kw, pipeline_kw, binding_kw])
