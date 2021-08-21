6f2886d5f11f24083d8276d49198e52bd45c65d7
John Grey <johngrey4296@gmail.com>
2021-08-21 21:27:07 +0100

    [fix] ctxcon is false when it has failed ctxins

--- acab/modules/semantics/context_container.py
+++ acab/modules/semantics/context_container.py
@@ -127,7 +127,7 @@ class ContextContainer(CtxInt.ContextContainer_i):
         return container
 
     def __bool__(self):
-        return bool(self._active)
+        return not bool(self._failed)
 
     def fail(self, instance: CtxIns, word: Value, node: Node):
         """ Record a failure, the query sentence that failed,
