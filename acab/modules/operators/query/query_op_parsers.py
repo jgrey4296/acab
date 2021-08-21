7c9820963cf2afde077c91a192f8412c093c9ae3
John Grey <johngrey4296@gmail.com>
2021-08-21 21:20:03 +0100

    [fix] query operator parser use

--- acab/modules/operators/query/query_op_parsers.py
+++ acab/modules/operators/query/query_op_parsers.py
@@ -1,4 +1,5 @@
 import pyparsing as pp
+import re
 
 from acab.abstract.parsing import parsers as PU
 from acab.abstract.config.config import AcabConfig
@@ -7,6 +8,7 @@ from acab.abstract.core.values import AcabValue
 from acab.abstract.core.values import Sentence
 
 from acab.abstract.core.production_abstractions import ProductionComponent
+from acab.abstract.engine.util import prep_op_path
 
 from . import query_operators as QO
 
@@ -14,12 +16,12 @@ config       = AcabConfig.Get()
 TAG_S        = config.prepare("Value.Structure", "TAG")()
 CONSTRAINT_S = config.prepare("Value.Structure", "CONSTRAINT")()
 
+tag_op_path = Sentence.build(prep_op_path(__package__, QO.HasTag.__name__))
 
 def construct_tag_query(toks):
     assert(TAG_S in toks)
     tags = [x[1] for x in toks[TAG_S]]
 
-    tag_op_path = Sentence.build([QO.HasTag.__name__])
     value_tags = [AcabValue(x) for x in tags]
     return (CONSTRAINT_S, ProductionComponent(value=tag_op_path, params=value_tags))
 
