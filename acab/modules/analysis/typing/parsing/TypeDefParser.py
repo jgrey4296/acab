503fd60fce5933e0cc5db5ba8a313708598f5b76
John Grey <johngrey4296@gmail.com>
2021-09-08 18:08:34 +0100

    [refactor] typing parser

--- acab/modules/analysis/typing/parsing/TypeDefParser.py
+++ acab/modules/analysis/typing/parsing/TypeDefParser.py
@@ -8,10 +8,6 @@ from acab.abstract.parsing import parsers as PU
 from acab.abstract.parsing.consts import (DBLARROW, DELIM, NG, N,
                                           component_gap, emptyLine, op)
 from acab.modules.analysis.typing import util as TYU
-from acab.modules.analysis.typing.values.operator_definition import \
-    OperatorDefinition
-from acab.modules.analysis.typing.values.type_definition import (
-    SumTypeDefinition, TypeDefinition)
 
 from acab.abstract.parsing.indented_block import IndentedBlock
 
@@ -27,26 +23,23 @@ HOTLOAD_BASIC_SEN = pp.Forward()
 ## Param sentence (able to parse annotations)
 HOTLOAD_PARAM_SEN = pp.Forward()
 
-# The simplest type, has no body. useful for defining strings and other
-# primitives
-SIMPLE_BODY = pp.empty
-SIMPLE_BODY.setParseAction(TU.make_record_def)
-
-SIMPLE_DEF = PU.STATEMENT_CONSTRUCTOR(HOTLOAD_BASIC_SEN, SIMPLE_BODY)
+# the simplest type
+SIMPLE_DEF = pp.Literal("τ::").suppress() + HOTLOAD_BASIC_SEN("type_sen")
+SIMPLE_DEF.setParseAction(TU.make_simple_def)
 
 # Record Type definition:
 # a.test.type: (::σ)  a.value.$x(::num) end
 RECORD_DEF_BODY = IndentedBlock(HOTLOAD_PARAM_SEN)
 RECORD_DEF_BODY.setParseAction(TU.make_record_def)
 
-RECORD_TYPE = PU.STATEMENT_CONSTRUCTOR(HOTLOAD_BASIC_SEN, RECORD_DEF_BODY)
+RECORD_TYPE = pp.Literal("τ::").suppress() + PU.STATEMENT_CONSTRUCTOR(HOTLOAD_BASIC_SEN, RECORD_DEF_BODY)
 
 # Sum Type definition
 # ie: first subwords are the subtypes. subtypes are automatic records
 SUM_DEF_BODY = IndentedBlock(pp.Or([RECORD_TYPE, SIMPLE_DEF]))
 SUM_DEF_BODY.setParseAction(TU.make_sum_def)
 
-SUM_TYPE = PU.STATEMENT_CONSTRUCTOR(HOTLOAD_BASIC_SEN, SUM_DEF_BODY )
+SUM_TYPE = pp.Literal("Σ::").suppress() + PU.STATEMENT_CONSTRUCTOR(HOTLOAD_BASIC_SEN, SUM_DEF_BODY )
 
 # numAdd: (::λ) $x(::num).$y(::num).$z(::num) => +
 # TODO: enable alias paths, not just sugar
@@ -54,15 +47,13 @@ OP_DEF_BODY = NG(TYU.STRUCT_S, HOTLOAD_PARAM_SEN) \
     + op(DBLARROW + N(TYU.SYNTAX_BIND_S, PU.OPERATOR_SUGAR))
 OP_DEF_BODY.setParseAction(TU.make_op_def)
 
-OP_DEF = PU.STATEMENT_CONSTRUCTOR(HOTLOAD_BASIC_SEN,
-                                  OP_DEF_BODY,
-                                  end=pp.lineEnd,
-                                  single_line=True)
+OP_DEF = pp.Literal("λ::").suppress() + PU.STATEMENT_CONSTRUCTOR(HOTLOAD_BASIC_SEN, OP_DEF_BODY, end=pp.lineEnd, single_line=True)
 
 # Type class constructor:
 TYPE_CLASS_BODY = IndentedBlock(OP_DEF)
+TYPE_CLASS_BODY.setParseAction(TU.make_type_class)
 
-TYPE_CLASS_DEF = PU.STATEMENT_CONSTRUCTOR(HOTLOAD_BASIC_SEN, TYPE_CLASS_BODY)
+TYPE_CLASS_DEF = pp.Literal("γ::").suppress() + PU.STATEMENT_CONSTRUCTOR(HOTLOAD_BASIC_SEN, TYPE_CLASS_BODY)
 
 COMBINED_DEFS = pp.Or([SUM_TYPE, RECORD_TYPE, OP_DEF, SIMPLE_DEF])
 
--- acab/modules/analysis/typing/parsing/util.py
+++ acab/modules/analysis/typing/parsing/util.py
@@ -7,31 +7,37 @@ from typing import (Any, Callable, ClassVar, Dict, Generic, Iterable, Iterator,
 
 from acab.abstract.config.config import AcabConfig
 from acab.abstract.core.values import Sentence
-from acab.modules.analysis.typing.values.operator_definition import \
-    OperatorDefinition
-from acab.modules.analysis.typing.values.type_definition import (
-    SumTypeDefinition, TypeDefinition)
 from acab.modules.analysis.typing import util as TYU
+from acab.modules.analysis.typing.values.type_definition import (
+    OperatorDefinition, SumTypeDefinition, TypeDefinition, TypeClass)
+
+from acab.abstract.parsing.annotation import ValueAnnotation
 
 config          = AcabConfig.Get()
 TYPE_INSTANCE_S = config.prepare("Value.Structure", "TYPE_INSTANCE")()
 
+def make_simple_def(toks):
+    type_def = TypeDefinition(None, [])
+    type_sen = toks['type_sen'].attach_statement(type_def)
+
+    return type_sen
+
 def make_record_def(toks):
-    type_def = TypeDefinition(toks[:])
-    return (TYPE_INSTANCE_S, type_def)
+    type_def = TypeDefinition(None, toks[:])
+    return type_def
 
 def make_op_def(toks):
     syntax_bind = None
     if TYU.SYNTAX_BIND_S in toks:
         syntax_bind = toks[TYU.SYNTAX_BIND_S][0]
 
-    op_def = OperatorDefinition(toks[TYU.STRUCT_S][0], sugar_syntax=syntax_bind)
+    op_def = OperatorDefinition(None, [toks[TYU.STRUCT_S][0]], sugar_syntax=syntax_bind)
 
-    return (TYPE_INSTANCE_S, op_def)
+    return op_def
 
 def make_sum_def(toks):
-    sum_def = SumTypeDefinition(toks[:])
-    return (TYPE_INSTANCE_S, sum_def)
+    sum_def = SumTypeDefinition(None, toks[:])
+    return sum_def
 
 
 def make_type_dec(toks):
@@ -42,4 +48,9 @@ def make_type_dec(toks):
     args = []
     if TYU.ARG_S in toks:
         args = [x[1] if isinstance(x, tuple) else x for x in toks[TYU.ARG_S][:]]
-    return (TYPE_INSTANCE_S, Sentence.build(path, params=args))
+
+    return ValueAnnotation(TYU.TYPE_INSTANCE_S,
+                           Sentence.build(path, params=args))
+
+def make_type_class(toks):
+    return TypeClass(None, toks[:])
