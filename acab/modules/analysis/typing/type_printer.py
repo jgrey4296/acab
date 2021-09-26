a85df89f78670896451c942a460157d7bbf5740a
John Grey <johngrey4296@gmail.com>
2021-09-26 07:36:34 +0100

    [printing] add type printer

---
+++ acab/modules/analysis/typing/type_printer.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+from dataclasses import dataclass, field
+from typing import (Any, Callable, ClassVar, Dict, Generic, Iterable, Iterator,
+                    List, Mapping, Match, MutableMapping, Optional, Sequence,
+                    Set, Tuple, TypeVar, Union, cast)
+from itertools import zip_longest, filterfalse, starmap
+
+import acab.abstract.core.default_structure as DS
+import acab.abstract.interfaces.value as VI
+from acab.abstract.config.config import GET, AcabConfig, ConfigSpec
+from acab.abstract.core.values import Sentence, AcabStatement
+from acab.abstract.interfaces.printing import PrintSemantics_i
+from acab.abstract.printing import default_symbols as DSYM
+from acab.abstract.printing import wrappers as PW
+
+
+config = GET()
+
+@dataclass
+class TypeAwareValuePrinter(PrintSemantics_i):
+
+    signal : str = field(default="_:TYPE_INSTANCE")
+
+    def __call__(self, value, top=None, data=None):
+        return_list = []
+        type_str = str(value.data[DS.TYPE_INSTANCE])
+        if type_str == "_:ATOM":
+            return []
+
+        return_list.append("::")
+        return_list.append(value.data[DS.TYPE_INSTANCE])
+
+        return return_list
