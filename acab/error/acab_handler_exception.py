9f30cb65355b18a37c79fdf37bf0b45f60e9832f
John Grey <johngrey4296@gmail.com>
2021-09-26 07:36:00 +0100

    [exception handling] add handler exception

---
+++ acab/error/acab_handler_exception.py
@@ -0,0 +1,16 @@
+from typing import List, Set, Dict, Tuple, Optional, Any
+from typing import Callable, Iterator, Union, Match
+from typing import Mapping, MutableMapping, Sequence, Iterable
+from typing import cast, ClassVar, TypeVar, Generic
+
+from dataclasses import dataclass, field, InitVar
+
+from .acab_base_exception import AcabBaseException
+
+@dataclass
+class AcabHandlerException(AcabBaseException):
+
+    msg : str = field(init=False, default="Handler Failure: {}")
+
+    def __str__(self):
+        return self.msg.format(self.detail)
