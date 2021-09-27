e554d9f97edee197b0ec025c7bb2f7e6f0b1dfdd
John Grey <johngrey4296@gmail.com>
2021-09-27 18:48:14 +0100

    [error handling] use more specific exceptions

--- acab/abstract/decorators/dsl.py
+++ acab/abstract/decorators/dsl.py
@@ -1,10 +1,10 @@
-from acab.error.acab_base_exception import AcabBaseException
+from acab.error.acab_parse_exception import AcabParseException
 
 def EnsureDSLInitialised(method):
     """ Utility Decorator for DSL Builder's, raising error if not initialised """
     def fn(self, *args, **kwargs):
         if not self._parsers_initialised:
-            raise AcabBaseException("DSL Not Initialised")
+            raise AcabParseException("DSL Not Initialised")
 
         return method(self, *args, **kwargs)
 
