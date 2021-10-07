006dc42d2954fa70eec0f9ae0d43015c0232cdad
John Grey <johngrey4296@gmail.com>
2021-10-07 01:13:31 +0100

    [semantics] implement DFSWalk semantics for query

---
+++ acab/modules/operators/dfs/__tests/test_walk_semantics.py
@@ -0,0 +1,194 @@
+#https://docs.python.org/3/library/unittest.html
+# https://docs.python.org/3/library/unittest.mock.html
+
+from os.path import splitext, split
+
+import unittest
+import unittest.mock as mock
+
+import logging as root_logger
+logging = root_logger.getLogger(__name__)
+
+
+import acab
+config = acab.setup()
+
+from acab.modules.operators.dfs.walk_semantics import WalkTrieSemantics
+from acab.modules.semantics.basic_system import BasicSemanticSystem
+from acab.modules.semantics.independent import ExclusionNodeSemantics
+from acab.modules.semantics.abstractions import QueryPlusAbstraction
+from acab.modules.engines.configured import exlo
+from acab.abstract.core.production_abstractions import ProductionComponent, ProductionContainer
+from acab.abstract.core.values import Sentence, AcabValue
+
+BIND          = config.prepare("Value.Structure", "BIND")()
+QUERY         = config.prepare("Value.Structure", "QUERY")()
+SEM_HINT      = config.prepare("Value.Structure", "SEMANTIC_HINT")()
+TYPE_INSTANCE = config.prepare("Value.Structure", "TYPE_INSTANCE")()
+AT_BIND       = config.prepare("Value.Structure", "AT_BIND")()
+default_modules = config.prepare("Module.REPL", "MODULES")().split("\n")
+
+class TestWalkSemantics(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        root_logger.getLogger('').setLevel(root_logger.WARNING)
+        LOG_FILE_NAME = "log.{}".format(splitext(split(__file__)[1])[0])
+
+        file_h = root_logger.FileHandler(LOG_FILE_NAME, mode='w')
+        file_h.setLevel(root_logger.DEBUG)
+
+        console = root_logger.StreamHandler()
+        console.setLevel(root_logger.WARNING)
+
+        logging = root_logger.getLogger(__name__)
+        logging.setLevel(root_logger.DEBUG)
+        logging.addHandler(console)
+        logging.addHandler(file_h)
+
+        cls.eng = exlo()
+        cls.eng.load_modules(*default_modules)
+        query_plus = QueryPlusAbstraction().as_handler("_:QUERY")
+        cls.eng.semantics._register_handler(query_plus)
+        walker = WalkTrieSemantics().as_handler("_:WALK")
+        cls.eng.semantics._register_handler(walker)
+
+    def tearDown(self):
+        self.eng("~a")
+
+    def test_query_walk_only_below_start(self):
+        """
+        @x ᛦ $y(::target)?
+        """
+        self.eng("a.b.c.test.sub.blah(::target)")
+        self.eng("a.b.d")
+        self.eng("a.b.e.something(::target)")
+
+        # Setup a ctx bonud to 'c'
+        ctxs       = self.eng("a.b.$x.test?")
+
+        # build a walk instruction
+        source_var = AcabValue.safe_make("x", data={BIND: AT_BIND})
+        test_var   = AcabValue.safe_make("y", data={BIND: True,
+                                                    TYPE_INSTANCE: Sentence.build(["target"]),
+                                                    QUERY : True})
+        query_sen = Sentence.build([source_var, test_var],
+                                   data={SEM_HINT : "_:WALK"})
+        query = ProductionContainer(value=[query_sen],
+                                    data={SEM_HINT : "_:QUERY"})
+
+        # call walk
+
+        result = self.eng(query, ctxset=ctxs)
+        # check results
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0].x, "c")
+        self.assertEqual(result[0].y, "blah")
+
+    def test_query_walk_multi_start(self):
+        """
+        @x ᛦ $y(::target)?
+        """
+        self.eng("a.b.c.test.sub.blah(::target)")
+        self.eng("a.b.d")
+        self.eng("a.b.e.test.something(::target)")
+
+        # Setup a ctx bonud to 'c'
+        ctxs       = self.eng("a.b.$x.test?")
+
+        # build a walk instruction
+        source_var = AcabValue.safe_make("x", data={BIND: AT_BIND})
+        test_var   = AcabValue.safe_make("y", data={BIND: True,
+                                                    TYPE_INSTANCE: Sentence.build(["target"]),
+                                                    QUERY : True})
+        query_sen = Sentence.build([source_var, test_var],
+                                   data={SEM_HINT : "_:WALK"})
+        query = ProductionContainer(value=[query_sen],
+                                    data={SEM_HINT : "_:QUERY"})
+
+        # call walk
+        result = self.eng(query, ctxset=ctxs)
+        # check results
+        self.assertEqual(len(result), 2)
+        self.assertEqual(result[0].x, "c")
+        self.assertEqual(result[0].y, "blah")
+        self.assertEqual(result[1].x, "e")
+        self.assertEqual(result[1].y, "something")
+
+    def test_query_walk_no_matches(self):
+        """
+        @x ᛦ $y(::target)?
+        """
+        self.eng("a.b.c.test.sub.blah(::nothing)")
+        self.eng("a.b.d")
+        self.eng("a.b.e.test.something(::nothing)")
+
+        # Setup a ctx bonud to 'c'
+        ctxs       = self.eng("a.b.$x.test?")
+
+        # build a walk instruction
+        source_var = AcabValue.safe_make("x", data={BIND: AT_BIND})
+        test_var   = AcabValue.safe_make("y", data={BIND: True,
+                                                    TYPE_INSTANCE: Sentence.build(["target"]),
+                                                    QUERY : True})
+        query_sen = Sentence.build([source_var, test_var],
+                                   data={SEM_HINT : "_:WALK"})
+        query = ProductionContainer(value=[query_sen],
+                                    data={SEM_HINT : "_:QUERY"})
+
+        # call walk
+        result = self.eng(query, ctxset=ctxs)
+        # check results
+        self.assertEqual(len(result), 0)
+
+    def test_query_walk_from_root(self):
+        """
+        ᛦ $y(::target)?
+        """
+        self.eng("a.b.c.test.sub.blah(::target)")
+        self.eng("a.b.d")
+        self.eng("a.b.e.test.something(::target)")
+
+        # build a walk instruction
+        test_var   = AcabValue.safe_make("y", data={BIND: True,
+                                                    TYPE_INSTANCE: Sentence.build(["target"]),
+                                                    QUERY : True})
+        query_sen = Sentence.build([test_var], data={SEM_HINT: "_:WALK"})
+        query = ProductionContainer(value=[query_sen],
+                                    data={SEM_HINT : "_:QUERY"})
+
+        # call walk
+        result = self.eng(query)
+        # check results
+        self.assertEqual(len(result), 2)
+
+    def test_query_walk_multi_patterns(self):
+        """
+        @x ᛦ $y(::target) $z(::other)?
+        """
+        self.eng("a.b.c.test.sub.blah(::target)")
+        self.eng("a.b.d")
+        self.eng("a.b.e.test.something(::other)")
+
+        # Setup a ctx bonud to 'c'
+        ctxs       = self.eng("a.b.$x.test?")
+
+        # build a walk instruction
+        source_var = Sentence.build([AcabValue.safe_make("x", data={BIND: AT_BIND})])
+        test_var   = AcabValue.safe_make("y", data={BIND: True,
+                                                    TYPE_INSTANCE: Sentence.build(["target"]),
+                                                    QUERY : True})
+
+        test_var2  = AcabValue.safe_make("z", data={BIND: True,
+                                                    TYPE_INSTANCE: Sentence.build(["other"]),
+                                                    QUERY : True})
+
+        query_sen = Sentence.build([source_var, test_var, test_var2],
+                                   data={SEM_HINT : "_:WALK"})
+        query = ProductionContainer(value=[query_sen],
+                                    data={SEM_HINT : "_:QUERY"})
+
+        # call walk
+        result = self.eng(query, ctxset=ctxs)
+        # check results
+        self.assertEqual(len(result), 2)
---
+++ acab/modules/operators/dfs/context_walk_manager.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python3
+
+#!/usr/bin/env python3
+
+import logging as root_logger
+from typing import (Any, Callable, ClassVar, Dict, Generic, Iterable, Iterator,
+                    List, Mapping, Match, MutableMapping, Optional, Sequence,
+                    Set, Tuple, TypeVar, Union, cast)
+
+logging = root_logger.getLogger(__name__)
+
+from dataclasses import FrozenInstanceError, InitVar, dataclass, field, replace
+from enum import Enum
+from uuid import UUID, uuid1
+
+import acab.abstract.interfaces.context as CtxInt
+import acab.error.acab_semantic_exception as ASErr
+from acab.abstract.config import GET
+from acab.abstract.core.production_abstractions import (ProductionComponent,
+                                                        ProductionContainer)
+from acab.abstract.interfaces.value import Sentence_i
+from acab.error.acab_semantic_exception import AcabSemanticException
+from acab.modules.context.constraints import ConstraintCollection
+
+config = GET()
+
+CONSTRAINT_S     = config.prepare("Value.Structure", "CONSTRAINT")()
+NEGATION_S       = config.prepare("Value.Structure", "NEGATION")()
+
+CtxIns           = CtxInt.ContextInstance_i
+CtxSet           = CtxInt.ContextSet_i
+Constraints      = 'ConstraintCollection'
+ProdComp         = ProductionComponent
+ProdCon          = ProductionContainer
+Operator         = 'ProductionOperator'
+Value            = 'AcabValue'
+Statement        = 'AcabStatement'
+Sen              = Sentence_i
+Node             = 'AcabNode'
+ModuleComponents = "ModuleComponents"
+NamedCtxSet      = "NamedCtxSet"
+
+DELAYED_E = Enum("Delayed Instruction Set", "ACTIVE FAIL DEACTIVATE CLEAR MERGE")
+
+
+@dataclass
+class ContextWalkManager:
+    """ Shared State of a data structure walk, between different ctx insts """
+
+    walk_spec     : Sen                = field()
+    root_node     : Node               = field()
+    ctxs          : CtxSet             = field()
+
+    collect_vars  : Set[str]                   = field(init=False, default_factory=set)
+    constraints   : List[ConstraintCollection] = field(init=False, default_factory=list)
+
+    _current_inst       : CtxIns               = field(init=False, default=None)
+
+    def __post_init__(self):
+        sen = self.walk_spec
+        start = 0
+        if sen[0].is_at_var:
+            start = 1
+        constraints = [ConstraintCollection.build(x, operators=self.ctxs._operators) for x in sen[start:]]
+        self.constraints.extend(constraints)
+
+    def __enter__(self):
+        # set all instances to start at node, unless start_word is an at_binding,
+        # in which case get the bound node
+        # handle negated behaviour
+        active_list : List[CtxIns] = self.ctxs.active_list()
+
+        if not self.walk_spec[0].is_at_var:
+            [x.set_current_node(self.root_node) for x in active_list]
+        else:
+            assert(self.walk_spec[0].is_at_var)
+            root_word = self.walk_spec[0]
+            [x.set_current_binding(root_word) for x in active_list]
+
+        return self
+
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        # collect bindings as necessary
+        self.collect()
+        # TODO handle exception
+
+
+    def __iter__(self):
+        return iter(self.constraints)
+
+
+
+    @property
+    def active(self):
+        active_ctxs = self.ctxs.active_list(clear=True)
+        for ctx in active_ctxs:
+            self._current_inst = ctx
+            yield [ctx._current]
+
+
+    def test_and_update(self, results:List[Node]):
+        self.test(results)
+
+    def test(self, possible: List[Node]):
+        """
+        run a word's tests on available nodes, with an instance.
+        bind successes and return them
+
+        constraints can be provided, or extracted from the test word
+
+        """
+
+        # [ctxs.test(ctxIns, accessible, x) for x in tests]
+
+        logging.debug(f"{repr(self)}: Testing/Extending on {len(possible)} : {possible}")
+
+        for constraints in self.constraints:
+            successes   = []
+            # Collect all nodes that pass tests
+            for node in possible:
+                try:
+                    constraints.test(node, self._current_inst)
+                    successes.append(node)
+                except ASErr.AcabSemanticTestFailure as err:
+                    logging.debug(f"Tests failed on {node.value}:\n\t{err}")
+                    self.ctxs.fail(self._current_inst, constraints.source, node, self.walk_spec)
+
+            # Handle successes
+            # success, so copy and extend ctx instance
+            bound_ctxs = self._current_inst.bind(constraints.source,
+                                                 successes,
+                                                 sub_binds=constraints["sub_struct_binds"])
+            self.ctxs.push(bound_ctxs)
+
+        return bound_ctxs
+
+    def collect(self):
+        """
+        Context collecton specific vars.
+        Flattens many contexts into one, with specified variables
+        now as lists accumulated from across the contexts.
+
+        Semantics of collect:
+        0[ctxs]0 -> fail
+        1[ctxs]n -> 1[α]1
+        where
+        α : ctx = ctxs[0] ∪ { β : ctx[β] for ctx in ctxs[1:] }
+        β : var to collect
+
+
+        """
+        if not bool(self.collect_vars):
+            return
+
+        # select instances with bindings
+        # Merge into single new instance
+        # replace
+        raise NotImplementedError()
