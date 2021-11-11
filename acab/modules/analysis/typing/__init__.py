889af6ce974f97a0f36812e426d0bfc2106db754
John Grey <johngrey4296@gmail.com>
2021-11-11 07:46:41 +0000

    [refactor] add guard to typing __init__
    controls imports to allow module to be imported in acab,
    while also allowing test discovery to not error from trying to use
    config
    prior to setup

--- acab/modules/analysis/typing/__init__.py
+++ acab/modules/analysis/typing/__init__.py
@@ -2,3 +2,8 @@
 The Typing Submodule Provides a TypeChecker,
 
 """
+from acab.core.config.config import AcabConfig
+
+if AcabConfig.instance is not None:
+    from .type_printer import *
+    from .typing_dsl import *
