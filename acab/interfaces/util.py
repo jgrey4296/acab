b9bc29e8dfc19ac837916bfa09f16eb9f5da1316
John Grey <johngrey4296@gmail.com>
2021-12-17 15:47:44 +0000

    [interface] add AcabReducible interface

---
+++ acab/interfaces/util.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from typing import List, Set, Dict, Tuple, Optional, Any
+from typing import Callable, Iterator, Union, Match
+from typing import Mapping, MutableMapping, Sequence, Iterable
+from typing import cast, ClassVar, TypeVar, Generic
+
+import abc
+from acab import types as AT
+
+
+class AcabReducible(metaclass=abc.ABCMeta):
+    """ Designates a class as being able to reduce down
+    to a list of sentences """
+
+    @abc.abstractmethod
+    def to_sentences(self) -> List[AT.Sentence]:
+        """ Convert to sentences for printing """
+        pass
