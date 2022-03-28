94722309c57cf58e339ba9435079fa96ffa4ed24
John Grey <johngrey4296@gmail.com>
2022-03-28 15:42:48 +0100

    [update] __init__.setup

--- acab/__init__.py
+++ acab/__init__.py
@@ -8,29 +8,48 @@ communicate with unity.
 
 util provides standard enums, strings and some utility functions
 """
+import logging as logmod
 from typing import Callable, Type, TypeAlias
+
 import acab.interfaces.type_aliases as types
 from acab.core.config.config import GET
-import logging as root_logger
-logging = root_logger.getLogger(__name__)
+from acab.core.util.log_formatter import AcabLogFormatter
+
+logging = logmod.getLogger(__name__)
 
 __all__ = ['types', 'setup', 'GET']
 
 _Value_A : TypeAlias = types.Value
 _Sen_A   : TypeAlias = types.Sentence
 
-test : str = 20
 
-def setup(location=None, basic_exc=False, custom_factory_fns:None|tuple[Type[_Value_A], Type[_Sen_A]]=None):
+def setup(location=None,
+          rich_exc=False,
+          format_logs=True,
+          ) -> types.Config:
     """
-    A Utility to easily setup the config system,
-    allowing the rest to load.
+    A Utility to easily setup the config singleton,
+    allowing the rest of acab to load.
+
+    Arguments:
+    location    :str  : the path to config file(s) to use
+    rich_exc    :bool : True for rich.traceback, false for normal python exceptions
+    format_logs :bool : True for AcabLogFormatter installation
     """
     #pylint: disable=import-outside-toplevel
+
+    from os.path import join, split
+
+    if format_logs:
+        stream_handler = logmod.StreamHandler()
+        stream_handler.setLevel(logmod.INFO - 1)
+        stream_handler.setFormatter(AcabLogFormatter())
+        logging.addHandler(stream_handler)
+        logging.propagate = False
+
+    import acab.core.config.structure
     from acab.core.config.config import AcabConfig
     from acab.core.config.modal import modal_config
-    import acab.core.config.structure
-    from os.path import split, join
 
     if location is None:
         base = split(__file__)[0]
@@ -38,13 +57,18 @@ def setup(location=None, basic_exc=False, custom_factory_fns:None|tuple[Type[_Va
 
     config = AcabConfig(location, hooks=[modal_config])
 
+
+    if not rich_exc:
+        try:
+            from rich.traceback import install
+            install(show_locals=True)
+        except ImportError:
+            logging.debug("Rich Module not found, using default exception handler")
+
     from acab.interfaces.value import ValueFactory_i
-    if not custom_factory_fns:
-        from acab.core.data.value import AcabValue
-        from acab.core.data.sentence import Sentence
-        ValueFactory_i.set(AcabValue, Sentence)
-    else:
-        ValueFactory_i.set(*custom_factory_fns)
+    from acab.core.data.sentence import Sentence
+    from acab.core.data.value import AcabValue
+    ValueFactory_i.set(AcabValue, Sentence)
 
     return config
 
