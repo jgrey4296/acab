2bf884ca911c28268146160f9d3a5bf62d357d45
John Grey <johngrey4296@gmail.com>
2022-04-05 17:30:10 +0100

    [fix] typos

--- acab/modules/parsing/exlo/exlo_dsl.py
+++ acab/modules/parsing/exlo/exlo_dsl.py
@@ -29,39 +29,42 @@ DSL_Fragment = ppDSL.DSL_Fragment
 DSL_Spec     = ppDSL.PyParse_Spec
 DSL_Handler  = ppDSL.PyParse_Handler
 
-EXLO_Parser = DSL_Fragment(specs=[DSL_Spec("action.statement"         , struct=AP.HOTLOAD_ACTION_STATEMENTS, flags=[DSL_Spec.flag_e.COLLECT]),
-                                  DSL_Spec("disallowed.words"         , struct=FP.HOTLOAD_BAD_HEADS,         flags=[DSL_Spec.flag_e.COLLECT]),
-                                  DSL_Spec("operators.action"         , struct=AP.HOTLOAD_OPERATORS,         flags=[DSL_Spec.flag_e.COLLECT]),
-                                  DSL_Spec("operators.query"          , struct=QP.HOTLOAD_QUERY_OP,          flags=[DSL_Spec.flag_e.COLLECT]),
-                                  DSL_Spec("operators.transform"      , struct=TP.HOTLOAD_TRANS_OP,          flags=[DSL_Spec.flag_e.COLLECT]),
-                                  DSL_Spec("query.statement"          , struct=QP.HOTLOAD_QUERY_SEN,         flags=[DSL_Spec.flag_e.COLLECT]),
-                                  DSL_Spec("sentence.annotation.end"  , struct=AP.HOTLOAD_SEN_ENDS, flags=[DSL_Spec.flag_e.COLLECT]),
-                                  DSL_Spec("sentence.annotation.head" , struct=AP.HOTLOAD_SEN_HEADS, flags=[DSL_Spec.flag_e.COLLECT]),
-                                  DSL_Spec("sentence.annotation.post" , struct=AP.HOTLOAD_SEN_POSTS, flags=[DSL_Spec.flag_e.COLLECT]),
-                                  DSL_Spec("sentence.ends"            , struct=FP.HOTLOAD_SEN_ENDS,          flags=[DSL_Spec.flag_e.COLLECT]),
-                                  DSL_Spec("statement"                , struct=TotalP.HOTLOAD_STATEMENTS,    flags=[DSL_Spec.flag_e.COLLECT]),
-                                  DSL_Spec("transform.statement"      , struct=TP.HOTLOAD_TRANS_STATEMENTS,  flags=[DSL_Spec.flag_e.COLLECT]),
-                                  DSL_Spec("word.annotation"          , struct=FP.HOTLOAD_ANNOTATIONS,       flags=[DSL_Spec.flag_e.COLLECT])],
+Link_Signal_To = lambda x, y: DSL_Spec(x, struct=y, flags=[DSL_Spec.flag_e.COLLECT])
+
+EXLO_Parser = DSL_Fragment(specs=[Link_Signal_To("action.statement"         , AP.HOTLOAD_ACTION_STATEMENTS),
+                                  Link_Signal_To("disallowed.words"         , FP.HOTLOAD_BAD_HEADS),
+                                  Link_Signal_To("operators.action"         , AP.HOTLOAD_OPERATORS),
+                                  Link_Signal_To("operators.query"          , QP.HOTLOAD_QUERY_OP),
+                                  Link_Signal_To("operators.transform"      , TP.HOTLOAD_TRANS_OP),
+                                  Link_Signal_To("query.statement"          , QP.HOTLOAD_QUERY_SEN),
+                                  Link_Signal_To("sentence.annotation.end"  , FP.HOTLOAD_SEN_ENDS),
+                                  Link_Signal_To("sentence.annotation.head" , FP.HOTLOAD_SEN_HEADS),
+                                  Link_Signal_To("sentence.annotation.post" , FP.HOTLOAD_SEN_POSTS),
+                                  Link_Signal_To("sentence.ends"            , FP.HOTLOAD_SEN_ENDS),
+                                  Link_Signal_To("statement"                , TotalP.HOTLOAD_STATEMENTS),
+                                  Link_Signal_To("transform.statement"      , TP.HOTLOAD_TRANS_STATEMENTS),
+                                  Link_Signal_To("word.annotation"          , FP.HOTLOAD_ANNOTATIONS)
+                                  ],
                            handlers=[
-                               DSL_Handler("sentence"            , FP.SENTENCE),
-                               DSL_Handler("sentence.ends"       , FP.SEN_MACRO),
-                               DSL_Handler("sentence.annotation.head", FP.op_head_annotation),
-                               DSL_Handler("sentence.operator"   , FP.op_sentence),
-                               DSL_Handler("sentence.plural"     , FP.SEN_PLURAL),
-                               DSL_Handler("word.constrained"    , FP.SEN_NO_MODAL),
+                               DSL_Handler("sentence"                 , FP.SENTENCE),
+                               DSL_Handler("sentence.ends"            , FP.SEN_MACRO),
+                               DSL_Handler("sentence.annotation.head" , FP.op_head_annotation),
+                               DSL_Handler("sentence.operator"        , FP.op_sentence),
+                               DSL_Handler("sentence.plural"          , FP.SEN_PLURAL),
+                               DSL_Handler("word.constrained"         , FP.SEN_NO_MODAL),
                                # Query
-                               DSL_Handler("operators.query"     , PU.OPERATOR_SUGAR),
-                               DSL_Handler("sentence.annotation.post", QP.query_sen_post_annotation)
-                               DSL_Handler("sentence.ends"       , QP.query_statement),
-                               DSL_Handler("word.annotation"     , QP.word_query_constraint),
+                               DSL_Handler("operators.query"          , PU.OPERATOR_SUGAR),
+                               DSL_Handler("sentence.annotation.post" , QP.query_sen_post_annotation),
+                               DSL_Handler("sentence.ends"            , QP.query_statement),
+                               DSL_Handler("word.annotation"          , QP.word_query_constraint),
                                # Transform
-                               DSL_Handler("operators.transform" , PU.OPERATOR_SUGAR),
-                               DSL_Handler("sentence.ends"       , TP.transform_statement),
+                               DSL_Handler("operators.transform"      , PU.OPERATOR_SUGAR),
+                               DSL_Handler("sentence.ends"            , TP.transform_statement),
                                # Action
-                               DSL_Handler("operators.action"    , PU.OPERATOR_SUGAR),
-                               DSL_Handler("sentence.ends"       , AP.action_definition),
+                               DSL_Handler("operators.action"         , PU.OPERATOR_SUGAR),
+                               DSL_Handler("sentence.ends"            , AP.action_definition),
                                # Rule
-                               DSL_Handler("sentence.ends"       , RP.rule),
+                               DSL_Handler("sentence.ends"            , RP.rule),
                                # Total
-                               DSL_Handler(DEFAULT_HANDLER_SIGNAL, TotalP.parse_point)]
+                               DSL_Handler(DEFAULT_HANDLER_SIGNAL     , TotalP.parse_point)]
                            )
