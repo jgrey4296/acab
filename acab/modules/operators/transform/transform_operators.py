1563a3e3f54aee7c3d08ef8029d9de87587b7448
John Grey <johngrey4296@gmail.com>
2022-06-20 17:36:56 +0100

    [refactor] transform regex operator

--- acab/modules/operators/transform/transform_operators.py
+++ acab/modules/operators/transform/transform_operators.py
@@ -17,8 +17,6 @@ config = AcabConfig()
 @OperatorSugar(config.attr.Operator.Sugar.REGEX_TRANSFORM)
 class RegexOp(ProductionOperator):
 
-    @OperatorArgUnWrap
-    @OperatorResultWrap
     def __call__(self, value, pattern, replacement, *, data=None, ctx=None):
         """ Substitute value pattern with value value from passed in data
         value : the replacement
@@ -26,7 +24,17 @@ class RegexOp(ProductionOperator):
 
         sub(pattern, replacement, string, count, flags)
         """
-        return sub(pattern, replacement, value)
+        pattern     = pattern[0].value
+        replacement = replacement[0].value
+        match value:
+            case VI.Sentence_i():
+                words = [sub(pattern, replacement, x.value) for x in value.words]
+                return value.copy(words)
+            case VI.Value_i():
+                subbed = sub(pattern, replacement, value.value)
+                return value.copy(value=subbed, name=subbed)
+            case _:
+                raise TypeError("Unrecognized type to RegexOp", value)
 
 @OperatorSugar(config.attr.Operator.Sugar.FORMAT)
 class FormatOp(ProductionOperator):
